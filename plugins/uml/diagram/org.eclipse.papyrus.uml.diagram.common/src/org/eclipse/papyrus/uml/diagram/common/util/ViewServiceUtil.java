5c5b67464e3afa7d54784a8343c853e3c01edac7
Benoit Maggi <benoit.maggi@cea.fr>
2015-01-05 16:36:19 +0100

    Bug 456688 - [All Diagrams] CommonDragAndDropPolicy should be moved in infra.gmfdiag   - move utility class in gmfdiag   - update dependancies in all drag&dropeditpolicy
    Change-Id: I226c6b208f68d8b832355f7c25bdde9d0d096d49
    Signed-off-by: Benoit Maggi <benoit.maggi@cea.fr>

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.common/src/org/eclipse/papyrus/uml/diagram/common/util/ViewServiceUtil.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.common/src/org/eclipse/papyrus/uml/diagram/common/util/ViewServiceUtil.java
@@ -13,49 +13,15 @@
  *****************************************************************************/
 package org.eclipse.papyrus.uml.diagram.common.util;
 
-import org.eclipse.gmf.runtime.diagram.core.util.ViewUtil;
-import org.eclipse.gmf.runtime.diagram.ui.commands.CreateCommand;
-import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
-import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
-import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewRequest.ViewDescriptor;
-import org.eclipse.gmf.runtime.emf.core.util.EObjectAdapter;
-import org.eclipse.gmf.runtime.notation.Diagram;
-import org.eclipse.gmf.runtime.notation.View;
-import org.eclipse.papyrus.infra.core.editor.IMultiDiagramEditor;
-import org.eclipse.ui.IEditorPart;
 
 /**
  * This class provides method to force the load of the view service see bug
  * 302555
+ * @deprecated since 1.1.0
+ * use org.eclipse.papyrus.infra.gmfdiag.common.utils.ViewServiceUtil
  */
-public class ViewServiceUtil {
+@Deprecated
+public class ViewServiceUtil extends org.eclipse.papyrus.infra.gmfdiag.common.utils.ViewServiceUtil {
 
-	/**
-	 * To load a service, we build a dummy command and we test if it can be
-	 * executed
-	 *
-	 */
-	public static void forceLoad() {
-		IEditorPart activeEditor = MDTUtil.getActiveEditor();
-		if (activeEditor != null) {
-			if (activeEditor instanceof IMultiDiagramEditor) {
-				Diagram diagram = (Diagram) ((IMultiDiagramEditor) activeEditor).getAdapter(Diagram.class);
-				if (diagram != null) {
-					String diagramSemanticHint = diagram.getType();
-					DiagramEditPart host = (DiagramEditPart) ((IMultiDiagramEditor) activeEditor).getAdapter(DiagramEditPart.class);
-
-					// When we don't have the semanticHint, the command can't be
-					// executed, if the ViewService is not started
-					// The goal of this class is to launch the view service
-					ViewDescriptor descriptor = new ViewDescriptor(new EObjectAdapter(diagram), Diagram.class, diagramSemanticHint, ViewUtil.APPEND, false, ((IGraphicalEditPart) host).getDiagramPreferencesHint());
-					CreateCommand dummyCommand = new CreateCommand(((IGraphicalEditPart) host).getEditingDomain(), descriptor, ((View) host.getModel()));
-
-					// this action force the load of the ViewService
-					dummyCommand.canExecute();
-				}
-			}
-		}
-
-	}
 
 }
