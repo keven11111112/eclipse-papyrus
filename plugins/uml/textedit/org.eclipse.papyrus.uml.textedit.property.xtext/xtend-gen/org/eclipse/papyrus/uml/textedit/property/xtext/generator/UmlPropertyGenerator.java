536350b7e9d650feb7aa883d128e814b10f04ca3
Camille Letavernier <camille.letavernier@cea.fr>
2015-01-21 12:36:23 +0100

    [Releng] Update version numbers to 1.0.2
    - Fix remaining Generators

--- plugins/uml/textedit/org.eclipse.papyrus.uml.textedit.property.xtext/xtend-gen/org/eclipse/papyrus/uml/textedit/property/xtext/generator/UmlPropertyGenerator.java
+++ plugins/uml/textedit/org.eclipse.papyrus.uml.textedit.property.xtext/xtend-gen/org/eclipse/papyrus/uml/textedit/property/xtext/generator/UmlPropertyGenerator.java
@@ -12,8 +12,12 @@
  */
 package org.eclipse.papyrus.uml.textedit.property.xtext.generator;
 
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.generator.IFileSystemAccess;
+import org.eclipse.xtext.generator.IGenerator;
+
 @SuppressWarnings("all")
-public class UmlPropertyGenerator /* implements IGenerator  */{
-  public void doGenerate(final /* Resource */Object resource, final /* IFileSystemAccess */Object fsa) {
+public class UmlPropertyGenerator implements IGenerator {
+  public void doGenerate(final Resource resource, final IFileSystemAccess fsa) {
   }
 }
--- plugins/uml/textedit/org.eclipse.papyrus.uml.textedit.state.xtext/xtend-gen/org/eclipse/papyrus/uml/textedit/state/xtext/generator/UmlStateGenerator.java
+++ plugins/uml/textedit/org.eclipse.papyrus.uml.textedit.state.xtext/xtend-gen/org/eclipse/papyrus/uml/textedit/state/xtext/generator/UmlStateGenerator.java
@@ -3,8 +3,12 @@
  */
 package org.eclipse.papyrus.uml.textedit.state.xtext.generator;
 
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.generator.IFileSystemAccess;
+import org.eclipse.xtext.generator.IGenerator;
+
 @SuppressWarnings("all")
-public class UmlStateGenerator /* implements IGenerator  */{
-  public void doGenerate(final /* Resource */Object resource, final /* IFileSystemAccess */Object fsa) {
+public class UmlStateGenerator implements IGenerator {
+  public void doGenerate(final Resource resource, final IFileSystemAccess fsa) {
   }
 }
