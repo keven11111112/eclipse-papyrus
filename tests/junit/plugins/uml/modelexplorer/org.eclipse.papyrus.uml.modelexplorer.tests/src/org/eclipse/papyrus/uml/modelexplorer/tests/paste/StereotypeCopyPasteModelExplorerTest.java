bcf3c737a2f35f9e97b6d6c70be4faefe7fbf96f
Gabriel Pascual <gabriel.pascual@all4tec.net>
2015-02-02 10:15:55 +0100

    438511: Copy/paste should copy property values for applied stereotype https://bugs.eclipse.org/bugs/show_bug.cgi?id=438511
      - Fix the command for duplication of applied stereotype to get legacy
    properties values
      - Fix associated unit test
    
    Change-Id: I0d156d1a8be356749e07ddb83cb0f89f6181db92
    Signed-off-by: Gabriel Pascual <gabriel.pascual@all4tec.net>
    (cherry picked from commit 83739b65fbf2b4758cd328b0142acaa439e1c6c0)

--- tests/junit/plugins/uml/modelexplorer/org.eclipse.papyrus.uml.modelexplorer.tests/src/org/eclipse/papyrus/uml/modelexplorer/tests/paste/StereotypeCopyPasteModelExplorerTest.java
+++ tests/junit/plugins/uml/modelexplorer/org.eclipse.papyrus.uml.modelexplorer.tests/src/org/eclipse/papyrus/uml/modelexplorer/tests/paste/StereotypeCopyPasteModelExplorerTest.java
@@ -9,6 +9,7 @@
  *
  * Contributors:
  *  Benoit Maggi (CEA LIST) benoit.maggi@cea.fr - Initial API and implementation
+ *  Gabriel Pascual (ALL4TEC gabriel.pascual@all4tec.net - bug 438511
  *
  *****************************************************************************/
 package org.eclipse.papyrus.uml.modelexplorer.tests.paste;
@@ -20,68 +21,50 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.papyrus.junit.framework.classification.tests.AbstractPapyrusTest;
 import org.eclipse.papyrus.junit.utils.HandlerUtils;
-import org.eclipse.papyrus.junit.utils.tests.AbstractEditorTest;
-import org.eclipse.papyrus.uml.modelexplorer.tests.Activator;
+import org.eclipse.papyrus.junit.utils.rules.PapyrusEditorFixture;
+import org.eclipse.papyrus.junit.utils.rules.PluginResource;
 import org.eclipse.papyrus.uml.tools.utils.NamedElementUtil;
+import org.eclipse.papyrus.views.modelexplorer.ModelExplorerView;
 import org.eclipse.ui.ISelectionService;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.uml2.uml.Model;
 import org.eclipse.uml2.uml.Stereotype;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 /**
- * Sample test for cut in model explorer
+ * Test for copy/past of stereotype in model explorer.
  * 
- * @author bmaggi
+ * @author Benoit Maggi
  */
-public class StereotypeCopyPasteModelExplorerTest extends AbstractEditorTest {
+@PluginResource("resources/stereotypeCopyPasteMETest/ProfileApplication.di")
+public class StereotypeCopyPasteModelExplorerTest extends AbstractPapyrusTest {
 
 	public static final String COPY_COMMAND_ID = "org.eclipse.ui.edit.copy"; //$NON-NLS-1$
 
 	public static final String PASTE_COMMAND_ID = "org.eclipse.ui.edit.paste"; //$NON-NLS-1$
 
-	public static final String RESOURCES_PATH = "resources/"; //$NON-NLS-1$
+	public final static String STEREOTYPED_ELEMENT_NAME = "theFirst"; //$NON-NLS-1$
 
-	public static final String MODEL_NAME = "modelWithStereotype"; //$NON-NLS-1$
-
-	public static final String PROJECT_NAME = "modelWithStereotype"; //$NON-NLS-1$
-
-	public final static String CLASS1_NAME = "Class1"; //$NON-NLS-1$
+	@Rule
+	public final PapyrusEditorFixture editorFixture = new PapyrusEditorFixture();
 
 	// information on the stereotype
-	public final static String REQUIREMENT = "SysML::Requirements::Requirement"; //$NON-NLS-1$
+	public final static String STEREOTYPE_QN = "profile::First"; //$NON-NLS-1$
 
-	public final static String REQUIREMENT_TEXT = "text"; //$NON-NLS-1$	
-	
-	public final static String REQUIREMENT_TEXT_VALUE = "description"; //$NON-NLS-1$
-	
-	public final static String REQUIREMENT_ID = "id"; //$NON-NLS-1$
-	
-	public final static String REQUIREMENT_ID_VALUE = "1"; //$NON-NLS-1$
-	
-	
-	
-	@Before
-	public void initModelForCopyPasteTest() {
-		try {
-			initModel(PROJECT_NAME, MODEL_NAME, Activator.getDefault().getBundle()); //$NON-NLS-1$ //$NON-NLS-2$
-		} catch (Exception e) {
-			Assert.fail(e.getMessage());
-		}
-	}
+	public final static String PARENT_STEREOTYPE_PROPERTY = "parentProperty"; //$NON-NLS-1$	
 
-	@Override
-	protected String getSourcePath() {
-		return RESOURCES_PATH;
-	}
+	public final static String PARENT_STEREOTYPE_PROPERTY_VALUE = "child"; //$NON-NLS-1$
 
-	
+	public final static String STEREOTYPE_PROPERTY = "childProperty"; //$NON-NLS-1$
 
-	
+	public final static Double STEREOTYPE_PROPERTY_VALUE = 2.3; //$NON-NLS-1$
+
+
 	/**
 	 * Simple copy paste of a class1 with <<requirememt>> stereotype
 	 * 
@@ -89,35 +72,34 @@ public class StereotypeCopyPasteModelExplorerTest extends AbstractEditorTest {
 	 */
 	@Test
 	public void copyPasteofClass1Test() throws Exception {
-		//get the rootModel
-		Assert.assertNotNull("RootModel is null", getRootUMLModel()); //$NON-NLS-1$
-		//get all semantic element that will handled
-		Model model = (Model)getRootUMLModel();
+		// get the rootModel
+		Assert.assertNotNull("RootModel is null", editorFixture.getModel()); //$NON-NLS-1$
+		// get all semantic element that will handled
+		Model model = (Model) editorFixture.getModel();
 
 		IWorkbenchWindow activeWorkbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 		ISelectionService selectionService = activeWorkbenchWindow.getSelectionService();
-		modelExplorerView = getModelExplorerView();
+		ModelExplorerView modelExplorerView = editorFixture.getModelExplorerView();
 		modelExplorerView.setFocus();
 		List<Object> elements = new ArrayList<Object>();
-		elements.add(getRootUMLModel());
+		elements.add(model);
 		modelExplorerView.revealSemanticElement(elements);
 
-		org.eclipse.uml2.uml.Class class1 = (org.eclipse.uml2.uml.Class)model.getPackagedElement(CLASS1_NAME);
-		
+		org.eclipse.uml2.uml.Class class1 = (org.eclipse.uml2.uml.Class) model.getPackagedElement(STEREOTYPED_ELEMENT_NAME);
+
 		elements.clear();
 		elements.add(class1);
 		modelExplorerView.revealSemanticElement(elements);
-		Object class1TreeObject = ((IStructuredSelection)selectionService.getSelection()).getFirstElement();
+		Object class1TreeObject = ((IStructuredSelection) selectionService.getSelection()).getFirstElement();
 		Assert.assertNotNull("Class1 TreeElement is null", class1TreeObject); //$NON-NLS-1$
 
 		IHandler copyHandler = HandlerUtils.getActiveHandlerFor(COPY_COMMAND_ID);
 		Assert.assertTrue("Copy not available", copyHandler.isEnabled()); //$NON-NLS-1$
 		copyHandler.execute(new ExecutionEvent());
 
-		// select root
-
+		// select roo
 		elements.clear();
-		elements.add(getRootUMLModel());
+		elements.add(model);
 		modelExplorerView.revealSemanticElement(elements);
 
 		// paste
@@ -125,16 +107,18 @@ public class StereotypeCopyPasteModelExplorerTest extends AbstractEditorTest {
 		Assert.assertTrue("Paste not available", pasteHandler.isEnabled()); //$NON-NLS-1$
 		pasteHandler.execute(new ExecutionEvent());
 
-		// check that there is a Class1 
-		org.eclipse.uml2.uml.Class copyOfClass1 = (org.eclipse.uml2.uml.Class)model.getPackagedElement(NamedElementUtil.COPY_OF + "_" + CLASS1_NAME + "_1");
-		Assert.assertNotNull("The copy is missing", copyOfClass1); //$NON-NLS-1$
-		
-		Stereotype stereotype = copyOfClass1.getAppliedStereotype(REQUIREMENT);
-		EObject stereotypeApplication = copyOfClass1.getStereotypeApplication(stereotype);	
-		Assert.assertEquals("Wrong copied id", stereotypeApplication.eGet(stereotypeApplication.eClass().getEStructuralFeature(REQUIREMENT_ID)), REQUIREMENT_ID_VALUE);
-		Assert.assertEquals("Wrong copied text", stereotypeApplication.eGet(stereotypeApplication.eClass().getEStructuralFeature(REQUIREMENT_TEXT)), REQUIREMENT_TEXT_VALUE);
-		
+		// check that there is a Class1
+		org.eclipse.uml2.uml.Class copyOfElement = (org.eclipse.uml2.uml.Class) model.getPackagedElement(NamedElementUtil.COPY_OF + "_" + STEREOTYPED_ELEMENT_NAME + "_1");
+		Assert.assertNotNull("The copy is missing", copyOfElement); //$NON-NLS-1$
+
+		Stereotype stereotype = copyOfElement.getAppliedStereotype(STEREOTYPE_QN);
+		EObject stereotypeApplication = copyOfElement.getStereotypeApplication(stereotype);
+		Assert.assertEquals("Wrong copied property " + STEREOTYPE_PROPERTY, stereotypeApplication.eGet(stereotypeApplication.eClass().getEStructuralFeature(STEREOTYPE_PROPERTY)), STEREOTYPE_PROPERTY_VALUE);
+		Assert.assertEquals("Wrong copied property " + PARENT_STEREOTYPE_PROPERTY, stereotypeApplication.eGet(stereotypeApplication.eClass().getEStructuralFeature(PARENT_STEREOTYPE_PROPERTY)), PARENT_STEREOTYPE_PROPERTY_VALUE);
+
 	}
+
+
 
 
 
