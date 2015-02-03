69b295d02866782747b41808eda107406dd38136
Francois Le Fevre - CEA <francois.le-fevre@cea.fr>
2015-02-03 16:51:59 +0100

    Bug 459042 : [Releng]: emfgen plugin do not respect naming convention
    changing package name
    changing artifact groupId
    changing maven mojo annotation
    
    Change-Id: I7054178d3f598edf4f0ca0a6cec7f8d40b82f187
    Signed-off-by: Francois Le Fevre - CEA <francois.le-fevre@cea.fr>

---
+++ releng/emfgen/emfgen-maven-plugin/src/main/java/org/eclipse/papyrus/emfgen/maven/EmfGenMojo.java
@@ -0,0 +1,140 @@
+package org.eclipse.papyrus.emfgen.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.eclipse.papyrus.emfgen.core.EmfGenerator;
+import org.eclipse.papyrus.emfgen.core.EmfGeneratorAppOptions;
+/**
+ *  @author CEA Tech - Francois Le Fevre
+ * 2014
+**/
+@Mojo( name = "emfgen", defaultPhase = LifecyclePhase.INSTALL )
+public class EmfGenMojo extends AbstractMojo
+{
+
+    public void execute() throws MojoExecutionException
+    {
+    	getLog().info("Welcome to EmfGen");
+    	getLog().info("dealing with "+genModel+" output: "+ outputDirectory);
+    	EmfGeneratorAppOptions emfGeneratorAppOptions = new EmfGeneratorAppOptions(genModel,ecoreModel, outputDirectory,isRelocate, isGenModel, isGenEdit,isGenEditor,isGenTests);
+		EmfGenerator myEmfGenerator = new EmfGenerator();
+		myEmfGenerator.execute(emfGeneratorAppOptions.getEmfModel(), emfGeneratorAppOptions.getEmfGeneratorOptions());
+    }
+    
+    /**
+     * Output directory
+     * */
+    @Parameter
+    private String outputDirectory = null;
+
+    /**
+     * Ecore path
+     */
+    @Parameter
+    private String ecoreModel = null;
+    
+    /**
+     * genModel path
+     * @parameter property="${genModel}"
+     * @required
+     */
+    private String genModel = null;
+    
+    /**
+     * isRelocate
+     */
+    @Parameter
+    private Boolean isRelocate = null;
+    
+    /**
+     * isGenModel
+     */
+    @Parameter
+    private Boolean isGenModel = null;
+    
+    /**
+     * isGenEdit
+     */
+    @Parameter
+    private Boolean isGenEdit = null;
+    
+    /**
+     * isGenEditor
+     */
+    @Parameter
+    private Boolean isGenEditor = null;
+    
+    /**
+     * isGenTests
+     */
+    @Parameter
+    private Boolean isGenTests = null;
+
+	public String getOutputDirectory() {
+		return outputDirectory;
+	}
+
+	public String getEcoreModel() {
+		return ecoreModel;
+	}
+
+	public String getGenModel() {
+		return genModel;
+	}
+
+	public void setOutputDirectory(String outputDirectory) {
+		this.outputDirectory = outputDirectory;
+	}
+
+	public void setEcoreModel(String ecoreModel) {
+		this.ecoreModel = ecoreModel;
+	}
+
+	public void setGenModel(String genModel) {
+		this.genModel = genModel;
+	}
+
+	public Boolean getIsGenModel() {
+		return isGenModel;
+	}
+
+	public void setIsGenModel(Boolean isGenModel) {
+		this.isGenModel = isGenModel;
+	}
+
+	public Boolean getIsGenEdit() {
+		return isGenEdit;
+	}
+
+	public void setIsGenEdit(Boolean isGenEdit) {
+		this.isGenEdit = isGenEdit;
+	}
+
+	public Boolean getIsGenEditor() {
+		return isGenEditor;
+	}
+
+	public void setIsGenEditor(Boolean isGenEditor) {
+		this.isGenEditor = isGenEditor;
+	}
+
+	public Boolean getIsGenTests() {
+		return isGenTests;
+	}
+
+	public void setIsGenTests(Boolean isGenTests) {
+		this.isGenTests = isGenTests;
+	}
+
+	public Boolean getIsRelocate() {
+		return isRelocate;
+	}
+
+	public void setIsRelocate(Boolean isRelocate) {
+		this.isRelocate = isRelocate;
+	}
+}
