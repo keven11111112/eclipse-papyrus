c89fa60d018cc673e324c6de0c7f3cf8c6927d25
Nicolas FAUVERGUE <nicolas.fauvergue@all4tec.net>
2015-02-11 18:18:45 +0100

    458260: [Table 2] Formatting of typed elements in 'reference' cells https://bugs.eclipse.org/bugs/show_bug.cgi?id=458260
    Change-Id: Ie36dd81e8e5b61dbbb7a6ff329a5a3be7da330ed
    Signed-off-by: Nicolas FAUVERGUE <nicolas.fauvergue@all4tec.net>

--- plugins/infra/nattable/org.eclipse.papyrus.infra.nattable/src/org/eclipse/papyrus/infra/nattable/provider/GenericCellLabelProvider.java
+++ plugins/infra/nattable/org.eclipse.papyrus.infra.nattable/src/org/eclipse/papyrus/infra/nattable/provider/GenericCellLabelProvider.java
@@ -9,6 +9,7 @@
  *
  * Contributors:
  *  Vincent Lorenzo (CEA LIST) vincent.lorenzo@cea.fr - Initial API and implementation
+ *  Nicolas FAUVERGUE (ALL4TEC) nicolas.fauvergue@all4tec.net - Bug 458260
  *
  *****************************************************************************/
 package org.eclipse.papyrus.infra.nattable.provider;
@@ -61,31 +62,91 @@ public class GenericCellLabelProvider extends AbstractNattableCellLabelProvider 
 	public String getText(Object element) {
 		final ILabelProviderCellContextElementWrapper contextElement = (ILabelProviderCellContextElementWrapper) element;
 		final IConfigRegistry registry = ((ILabelProviderContextElementWrapper) element).getConfigRegistry();
-		Object value = contextElement.getObject();
-		final StringBuffer label = new StringBuffer();
+		final Object value = contextElement.getObject();
 		final LabelProviderService service = getLabelProviderService(registry);
+		return getText(service, value);
+	}
+	
+	/**
+	 * Get the text for the value.
+	 * 
+	 * @param service The label provider service.
+	 * @param value The value to parse.
+	 * @return The string representing the object.
+	 */
+	protected String getText(LabelProviderService service, Object value){
+		final StringBuffer label = new StringBuffer();
 		if (value instanceof Collection<?>) {
-			Iterator<?> iter = ((Collection<?>) value).iterator();
-			label.append(Constants.BEGIN_OF_COLLECTION);
-			int i = 1;
-			while (iter.hasNext()) {
-				if (i > MAX_DISPLAYED_ELEMENTS) {
-					label.append(Constants.BIG_COLLECTION);
-					break;
-				}
-
-				Object current = iter.next();
-				label.append(service.getLabelProvider(current).getText(current));
-				if (iter.hasNext()) {
-					label.append(Constants.SEPARATOR);
-				}
-				i++;
-			}
-
-			label.append(Constants.END_OF_COLLECTION);
+			label.append(getCollectionText(service, (Collection<?>)value));
 		} else {
 			label.append(service.getLabelProvider(value).getText(value));
 		}
 		return label.toString();
+	}
+	
+	/**
+	 * This allow to manage if the hooks of collection will be displayed or not.
+	 * 
+	 * @return <code>false</code>
+	 */
+	private boolean isDisplayCollectionHooks(){
+		// This allow to manage if the hooks of collection will be displayed or not
+		// This will be managed by preference later
+		return false;
+	}
+	
+	/**
+	 * This allow to manage if the big collection display will be added.
+	 * 
+	 * @return <code>false</code>
+	 */
+	private boolean isDisplayBigCollection(){
+		// This allow to manage if the big collection display will be added or not
+		// This will be managed by preference later
+		return false;
+	}
+	
+	/**
+	 * Get the text for the collection object.
+	 * 
+	 * @param service The label provider service.
+	 * @param value The value to parse.
+	 * @return The string representing the object.
+	 */
+	protected String getCollectionText(LabelProviderService service, Collection<?> value){
+		final StringBuffer label = new StringBuffer();
+		Iterator<?> iter = ((Collection<?>) value).iterator();
+		if(isDisplayCollectionHooks())
+			label.append(Constants.BEGIN_OF_COLLECTION);
+		int i = 1;
+		while (iter.hasNext()) {
+			if (i > MAX_DISPLAYED_ELEMENTS) {
+				if(isDisplayBigCollection())
+					label.append(Constants.BIG_COLLECTION);
+				break;
+			}
+
+			Object current = iter.next();
+			label.append(getElementText(service, current));
+			if (iter.hasNext()) {
+				label.append(Constants.SEPARATOR);
+			}
+			i++;
+		}
+
+		if(isDisplayCollectionHooks())
+			label.append(Constants.END_OF_COLLECTION);
+		return label.toString();
+	}
+	
+	/**
+	 * Get the text for the element.
+	 * 
+	 * @param service The label provider service.
+	 * @param value The value to parse.
+	 * @return The string representing the object.
+	 */
+	protected String getElementText(LabelProviderService service, Object value){
+		return service.getLabelProvider(value).getText(value);
 	}
 }
