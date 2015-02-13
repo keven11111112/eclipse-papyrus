f3e8a9db27f252723305c9f9ae95c00f6c51143b
Ansgar Radermacher <ansgar.radermacher@cea.fr>
2015-02-13 11:41:24 +0100

    399864 - [CDT/StateMachine] Papyrus shall enable to see code on transitions

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.statemachine/custom-src/org/eclipse/papyrus/uml/diagram/statemachine/custom/preferences/CSSOptionsConstants.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.statemachine/custom-src/org/eclipse/papyrus/uml/diagram/statemachine/custom/preferences/CSSOptionsConstants.java
@@ -12,21 +12,21 @@
 package org.eclipse.papyrus.uml.diagram.statemachine.custom.preferences;
 
 /**
- * Constants for Transition preferences (use same constants as for CSS options)
+ * Values to use in CSS styles for transition options (how to show code attached to a transition)
  */
-public class PreferenceConstants {
+public class CSSOptionsConstants {
 	/**
 	 * integer: Maximum length of displayed bodies.
 	 */
-	public final static String BODY_CUT_LENGTH = CSSOptionsConstants.BODY_CUT_LENGTH;
+	public final static String BODY_CUT_LENGTH = "bodyCutLength"; //$NON-NLS-1$
 
 	/**
 	 * Boolean: if true, indicate presence of parameters (attributes) by adding (...) to call or signal event.
 	 */
-	public final static String INDICATE_PARAMETERS = CSSOptionsConstants.INDICATE_PARAMETERS;
+	public final static String INDICATE_PARAMETERS = "indicateParameters"; //$NON-NLS-1$
 
 	/**
 	 * Boolean: if true, add a line-break to the transition label before the effect
 	 */
-	public final static String LINEBREAK_BEFORE_EFFECT = CSSOptionsConstants.LINEBREAK_BEFORE_EFFECT;
+	public final static String LINEBREAK_BEFORE_EFFECT = "lineBreakBeforeEffect"; //$NON-NLS-1$
 }
