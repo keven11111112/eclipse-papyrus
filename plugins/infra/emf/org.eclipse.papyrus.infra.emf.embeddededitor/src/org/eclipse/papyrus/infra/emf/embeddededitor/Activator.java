377dcc49bb00df376e1e104ec604b6954e3c1940
Florian Noyrit <florian.noyrit@cea.fr>
2015-03-13 19:55:18 +0100

    Fix some failing tests due to migration of composite diagram to ElementTypesConfiguration

--- plugins/infra/emf/org.eclipse.papyrus.infra.emf.embeddededitor/src/org/eclipse/papyrus/infra/emf/embeddededitor/Activator.java
+++ plugins/infra/emf/org.eclipse.papyrus.infra.emf.embeddededitor/src/org/eclipse/papyrus/infra/emf/embeddededitor/Activator.java
@@ -21,7 +21,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.papyrus.editor.emf"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.eclipse.papyrus.infra.emf.embeddededitor"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
@@ -36,7 +36,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
 	@Override
@@ -48,7 +48,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	@Override
