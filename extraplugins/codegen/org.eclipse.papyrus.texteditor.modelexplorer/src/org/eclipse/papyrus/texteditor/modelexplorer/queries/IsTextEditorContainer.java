ed2b36fe5347eaaf13fadeab0bbccb8fb29acc16
Ansgar Radermacher <ansgar.radermacher@cea.fr>
2015-03-30 10:49:00 +0200

    435352 - [Property view] Attribute selection takes way too long

--- extraplugins/codegen/org.eclipse.papyrus.texteditor.modelexplorer/src/org/eclipse/papyrus/texteditor/modelexplorer/queries/IsTextEditorContainer.java
+++ extraplugins/codegen/org.eclipse.papyrus.texteditor.modelexplorer/src/org/eclipse/papyrus/texteditor/modelexplorer/queries/IsTextEditorContainer.java
@@ -14,15 +14,11 @@
  *****************************************************************************/
 package org.eclipse.papyrus.texteditor.modelexplorer.queries;
 
-import java.util.Iterator;
-
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.papyrus.emf.facet.efacet.core.IFacetManager;
 import org.eclipse.papyrus.emf.facet.efacet.core.exception.DerivedTypedElementException;
 import org.eclipse.papyrus.emf.facet.query.java.core.IJavaQuery2;
 import org.eclipse.papyrus.emf.facet.query.java.core.IParameterValueList2;
-import org.eclipse.papyrus.infra.viewpoints.policy.NotationUtils;
-import org.eclipse.papyrus.texteditor.model.texteditormodel.TextEditorModel;
 import org.eclipse.papyrus.views.modelexplorer.queries.AbstractEditorContainerQuery;
 
 /** Returns true if the element contains a Table */
@@ -34,18 +30,6 @@ public class IsTextEditorContainer extends AbstractEditorContainerQuery implemen
 	@Override
 	public Boolean evaluate(EObject context, IParameterValueList2 parameterValues, IFacetManager manager)
 			throws DerivedTypedElementException {
-		Iterator<EObject> roots = NotationUtils.getNotationRoots(context);
-		if (roots == null) {
-			return false;
-		}
-		while (roots.hasNext()) {
-			EObject inner = roots.next();
-			if (inner instanceof TextEditorModel) {
-				if (((TextEditorModel) inner).getEditedObject() == context) {
-					return true;
-				}
-			}
-		}
 		return false;
 	}
 }
