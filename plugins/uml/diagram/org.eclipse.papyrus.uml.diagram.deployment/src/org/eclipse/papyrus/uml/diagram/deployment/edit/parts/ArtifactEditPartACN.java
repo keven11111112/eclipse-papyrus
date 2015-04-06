ab4053291e14dd97570e895f60bc96cfc8f87dc9
ashatilov <montages.as@gmail.com>
2015-04-06 13:37:11 +0300

    Bug 463769 - Stereotype compartment can not be restored
     - Deployment diagram contribution
    
    Change-Id: I29b837530e49461c8f8b85e0d7015b9eff23dbcc
    Signed-off-by: ashatilov <montages.as@gmail.com>
    Reviewed-on: https://git.eclipse.org/r/45316
    Tested-by: Hudson CI
    Reviewed-by: Michael Golubev <golubev@montages.com>
    Reviewed-by: Camille Letavernier <camille.letavernier@cea.fr>

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.deployment/src/org/eclipse/papyrus/uml/diagram/deployment/edit/parts/ArtifactEditPartACN.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.deployment/src/org/eclipse/papyrus/uml/diagram/deployment/edit/parts/ArtifactEditPartACN.java
@@ -32,6 +32,7 @@ import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
 import org.eclipse.gmf.runtime.diagram.ui.figures.IBorderItemLocator;
 import org.eclipse.gmf.runtime.draw2d.ui.figures.ConstrainedToolbarLayout;
 import org.eclipse.gmf.runtime.gef.ui.figures.NodeFigure;
+import org.eclipse.gmf.runtime.notation.BasicCompartment;
 import org.eclipse.gmf.runtime.notation.Edge;
 import org.eclipse.gmf.runtime.notation.NotationPackage;
 import org.eclipse.gmf.runtime.notation.View;
@@ -157,7 +158,8 @@ public class ArtifactEditPartACN extends RoundedCompartmentEditPart {
 		if (NotationPackage.eINSTANCE.getView_Visible().equals(event.getFeature())) {
 			Object notifier = event.getNotifier();
 			List<?> modelChildren = ((View) getModel()).getChildren();
-			if (!(notifier instanceof Edge)) {
+			if (false == notifier instanceof Edge
+					&& false == notifier instanceof BasicCompartment) {
 				if (modelChildren.contains(event.getNotifier())) {
 					return;
 				}
