634aecf6bf27078e97bc3427f7c0f4b87e2f56b6
Remi Schnekenburger <remi.schnekenburger@cea.fr>
2015-05-06 17:30:33 +0200

    466602: [Activity Diagram] Tests error on the activity diagram tests due to wrong initialization of the containment feature. https://bugs.eclipse.org/bugs/show_bug.cgi?id=466602
    - force the containment feature for object nodes

--- plugins/uml/org.eclipse.papyrus.uml.service.types/src/org/eclipse/papyrus/uml/service/types/helper/ActivityHelper.java
+++ plugins/uml/org.eclipse.papyrus.uml.service.types/src/org/eclipse/papyrus/uml/service/types/helper/ActivityHelper.java
@@ -30,10 +30,40 @@ public class ActivityHelper extends ElementEditHelper {
 	
 	@Override
 	protected ICommand getCreateCommand(CreateElementRequest req) {
-		CreateElementRequest createRequest = isStructuredNode(req.getElementType()) ? createStructuredNodeRequest(req) : req;
-		return super.getCreateCommand(createRequest);
+		CreateElementRequest request;
+		boolean isObjectNode = isObjectNode(req.getElementType());
+		if (isObjectNode) {
+			request = createObjectNodeRequest(req);
+		} else {
+			boolean isStructureNode = isStructuredNode(req.getElementType());
+			if (isStructureNode) {
+				request = createStructuredNodeRequest(req);
+			} else {
+				request = req;
+			}
+		}
+		return super.getCreateCommand(request);
 	}
 	
+	/**
+	 * @param elementType
+	 * @return
+	 */
+	private CreateElementRequest createObjectNodeRequest(CreateElementRequest baseReq) {
+		CreateElementRequest req = new CreateElementRequest(baseReq.getEditingDomain(), baseReq.getContainer(), baseReq.getElementType());
+		req.addParameters(baseReq.getParameters());
+		req.setContainmentFeature(UMLPackage.eINSTANCE.getActivity_OwnedNode());
+		return req;
+	}
+
+	/**
+	 * @param elementType
+	 * @return
+	 */
+	protected boolean isObjectNode(IElementType type) {
+		return type.getEClass() != null && UMLPackage.eINSTANCE.getObjectNode().isSuperTypeOf(type.getEClass());
+	}
+
 	protected CreateElementRequest createStructuredNodeRequest(CreateElementRequest baseReq) {
 		CreateElementRequest req = new CreateElementRequest(baseReq.getEditingDomain(), baseReq.getContainer(), baseReq.getElementType());
 		req.addParameters(baseReq.getParameters());
