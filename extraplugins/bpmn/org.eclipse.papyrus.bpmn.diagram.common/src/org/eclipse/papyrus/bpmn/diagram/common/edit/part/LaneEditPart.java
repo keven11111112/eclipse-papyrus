b660b7d240fcf4993d2555df0ab54f79d30331d8
Juan Cadavid <juan.cadavid@cea.fr>
2015-07-16 17:23:00 +0200

    Papyrus BPMN customization
    Change-Id: Ifccfedbbd4f30f166e1b39136ec1822a63f4f33c
    Signed-off-by: Juan Cadavid <juan.cadavid@cea.fr>

---
+++ extraplugins/bpmn/org.eclipse.papyrus.bpmn.diagram.common/src/org/eclipse/papyrus/bpmn/diagram/common/edit/part/LaneEditPart.java
@@ -0,0 +1,14 @@
+package org.eclipse.papyrus.bpmn.diagram.common.edit.part;
+
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.papyrus.uml.diagram.activity.edit.part.CustomActivityPartitionEditPart;
+import org.eclipse.papyrus.uml.diagram.activity.figures.ActivityPartitionFigure;
+
+
+public class LaneEditPart extends CustomActivityPartitionEditPart {
+
+	public LaneEditPart(View view) {
+		super(view);
+	}
+
+}
---
+++ extraplugins/bpmn/org.eclipse.papyrus.bpmn.diagram.common/src/org/eclipse/papyrus/bpmn/diagram/common/providers/CustomBpmnUMLEditPartProvider.java
@@ -0,0 +1,12 @@
+package org.eclipse.papyrus.bpmn.diagram.common.providers;
+
+import org.eclipse.papyrus.uml.diagram.activity.edit.part.CustomUMLEditPartFactory;
+import org.eclipse.papyrus.uml.diagram.activity.providers.CustomUMLEditPartProvider;
+
+public class CustomBpmnUMLEditPartProvider extends CustomUMLEditPartProvider{
+	public CustomBpmnUMLEditPartProvider() {
+		super();
+		setFactory(new CustomBpmnUMLEditPartFactory());
+		setAllowCaching(true);
+	}
+}
