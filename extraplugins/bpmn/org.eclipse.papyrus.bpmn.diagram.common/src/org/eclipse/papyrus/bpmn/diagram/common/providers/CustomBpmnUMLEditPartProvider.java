524300c0c362fa7594e9ff59c4cf3e58a65b1969
Juan Cadavid <juan.cadavid@cea.fr>
2015-07-16 17:23:00 +0200

    Papyrus BPMN customization Bundle info corrections
    Signed-off-by: Juan Cadavid <juan.cadavid@cea.fr>

--- extraplugins/bpmn/org.eclipse.papyrus.bpmn.diagram.common/src/org/eclipse/papyrus/bpmn/diagram/common/providers/CustomBpmnUMLEditPartProvider.java
+++ extraplugins/bpmn/org.eclipse.papyrus.bpmn.diagram.common/src/org/eclipse/papyrus/bpmn/diagram/common/providers/CustomBpmnUMLEditPartProvider.java
@@ -1,9 +1,5 @@
 package org.eclipse.papyrus.bpmn.diagram.common.providers;
 
-<<<<<<< Upstream, based on origin/master
-import org.eclipse.papyrus.uml.diagram.activity.edit.part.CustomUMLEditPartFactory;
-=======
->>>>>>> c612c25 Papyrus BPMN customization Bundle info corrections
 import org.eclipse.papyrus.uml.diagram.activity.providers.CustomUMLEditPartProvider;
 
 public class CustomBpmnUMLEditPartProvider extends CustomUMLEditPartProvider{
