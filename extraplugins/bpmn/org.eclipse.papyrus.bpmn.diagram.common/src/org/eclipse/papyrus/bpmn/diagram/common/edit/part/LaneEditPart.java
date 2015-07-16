f324eea9e9d4391f40ad5f6a632857e384416fb9
Juan Cadavid <juan.cadavid@cea.fr>
2015-07-16 17:23:00 +0200

    Papyrus BPMN customization Bundle info corrections
    Signed-off-by: Juan Cadavid <juan.cadavid@cea.fr>

--- extraplugins/bpmn/org.eclipse.papyrus.bpmn.diagram.common/src/org/eclipse/papyrus/bpmn/diagram/common/edit/part/LaneEditPart.java
+++ extraplugins/bpmn/org.eclipse.papyrus.bpmn.diagram.common/src/org/eclipse/papyrus/bpmn/diagram/common/edit/part/LaneEditPart.java
@@ -2,7 +2,6 @@ package org.eclipse.papyrus.bpmn.diagram.common.edit.part;
 
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.papyrus.uml.diagram.activity.edit.part.CustomActivityPartitionEditPart;
-import org.eclipse.papyrus.uml.diagram.activity.figures.ActivityPartitionFigure;
 
 
 public class LaneEditPart extends CustomActivityPartitionEditPart {
