8d0b38ea11bd162e146c547f7a195138d6ab3f2e
Ed Seidewitz <ed-s@modeldriven.com>
2015-07-20 20:57:01 -0400

    Bug 472232 - Improved handling of default values.
    - Updated Alf2UML to save default value text in body of
    OpaqueExpression.
    - Updated UML2Alf to re-parse default value from body text.
    
    Change-Id: I4b073fa77ca132867be8c42c8a765fae80d9ff6b
    Signed-off-by: Ed Seidewitz <ed-s@modeldriven.com>

--- plugins/uml/alf/org.eclipse.papyrus.uml.alf.to.fuml/src/org/eclipse/papyrus/uml/alf/to/fuml/qvt/QVTLibrary.java
+++ plugins/uml/alf/org.eclipse.papyrus.uml.alf.to.fuml/src/org/eclipse/papyrus/uml/alf/to/fuml/qvt/QVTLibrary.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2013, 2014 CEA LIST.
+ * Copyright (c) 2013-2015 CEA LIST.
  *    
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,18 +7,24 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *  IJI - Initial implementation
- *  MDS - Updated for Luna
+ *  Ed Seidewitz (IJI/MDS)
  * 
  *****************************************************************************/
 
 package org.eclipse.papyrus.uml.alf.to.fuml.qvt;
 
+import java.io.StringReader;
+
 import org.eclipse.m2m.qvt.oml.blackbox.java.Module;
 import org.eclipse.m2m.qvt.oml.blackbox.java.Operation;
 import org.eclipse.m2m.qvt.oml.blackbox.java.Operation.Kind;
 import org.eclipse.papyrus.uml.alf.AlfStandaloneSetup;
+import org.eclipse.papyrus.uml.alf.Expression;
 import org.eclipse.papyrus.uml.alf.SyntaxElement;
+import org.eclipse.papyrus.uml.alf.services.AlfGrammarAccess;
+import org.eclipse.xtext.IGrammarAccess;
+import org.eclipse.xtext.parser.IParseResult;
+import org.eclipse.xtext.parser.IParser;
 import org.eclipse.xtext.resource.SaveOptions;
 import org.eclipse.xtext.serializer.ISerializer;
 
@@ -37,6 +43,15 @@ public class QVTLibrary {
 		ISerializer serializer = injector.getInstance(ISerializer.class);
 		SaveOptions options = SaveOptions.newBuilder().format().noValidation().getOptions();
 		return serializer.serialize(element, options);
+	}
+	
+	@Operation(contextual = true, kind = Kind.QUERY)
+	public static Expression parseExpression(String text) {
+		Injector injector = new AlfStandaloneSetup().createInjectorAndDoEMFRegistration();
+		IParser parser = injector.getInstance(IParser.class);
+		AlfGrammarAccess grammar = (AlfGrammarAccess) injector.getInstance(IGrammarAccess.class);
+		IParseResult result = parser.parse(grammar.getExpressionRule(), new StringReader(text));
+		return result.hasSyntaxErrors()? null: (Expression)result.getRootASTElement();
 	}
 
 }
