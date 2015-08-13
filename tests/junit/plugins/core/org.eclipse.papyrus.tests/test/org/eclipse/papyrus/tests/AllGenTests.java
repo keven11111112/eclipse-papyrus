4db203b5800ef16b163c6968f777cd409eb1a11b
Christian W. Damus <give.a.damus@gmail.com>
2015-08-13 11:02:27 -0400

    Bug 464647: [Tests] Generate JUnit tests for diagram view synch https://bugs.eclipse.org/bugs/show_bug.cgi?id=464647
    Add a profile profile for execution of generated tests, splitting the org.eclipse.papyrus.tests.AllGenTests suite into sub-suites and invoking them serially using Maven's facility for multiple executions of a plug-in.

--- tests/junit/plugins/core/org.eclipse.papyrus.tests/test/org/eclipse/papyrus/tests/AllGenTests.java
+++ tests/junit/plugins/core/org.eclipse.papyrus.tests/test/org/eclipse/papyrus/tests/AllGenTests.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2010, 2014 CEA LIST and others.
+ * Copyright (c) 2010, 2015 CEA LIST; Christian W. Damus, and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,6 +8,7 @@
  *
  * Contributors:
  *  Juan Cadavid - juan.cadavid@cea.fr
+ *  Christian W. Damus - bug 464647
  *
  *****************************************************************************/
 package org.eclipse.papyrus.tests;
@@ -20,6 +21,8 @@ import org.eclipse.papyrus.junit.framework.runner.FragmentTestSuiteClass;
 import org.eclipse.papyrus.junit.framework.runner.ITestSuiteClass;
 import org.eclipse.papyrus.junit.framework.runner.SuiteSpot;
 import org.junit.runner.RunWith;
+
+import com.google.common.collect.ImmutableList;
 
 
 /**
@@ -42,11 +45,51 @@ public class AllGenTests {
 		suiteClasses.add(new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.communication.tests", "org.eclipse.papyrus.uml.diagram.communication.test.AllGenTests"));
 		suiteClasses.add(new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.component.tests", "org.eclipse.papyrus.uml.diagram.component.test.AllGenTests"));
 		suiteClasses.add(new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.composite.tests", "org.eclipse.papyrus.uml.diagram.composite.test.AllGenTests"));
+		suiteClasses.add(new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.deployment.tests", "org.eclipse.papyrus.uml.diagram.deployment.test.AllGenTests"));
+		suiteClasses.add(new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.profile.tests", "org.eclipse.papyrus.uml.diagram.profile.tests.AllGenTests"));
+		suiteClasses.add(new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.statemachine.tests", "org.eclipse.papyrus.uml.diagram.profile.tests.AllGenTests"));
+		suiteClasses.add(new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.sequence.tests", "org.eclipse.papyrus.uml.diagram.profile.tests.AllGenTests"));
 		suiteClasses.add(new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.timing.tests", "org.eclipse.papyrus.uml.diagram.timing.test.AllGenTests"));
 		suiteClasses.add(new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.usecase.tests", "org.eclipse.papyrus.uml.diagram.usecase.test.AllGenTests"));
-		suiteClasses.add(new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.deployment.tests", "org.eclipse.papyrus.uml.diagram.deployment.test.AllGenTests"));
-		suiteClasses.add(new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.profile.testss", "org.eclipse.papyrus.uml.diagram.profile.tests.AllGenTests"));
 		// end
 	}
 
+	//
+	// The tests broken out into sub-suites for serial execution, to avoid OOMEs
+	//
+
+	@RunWith(AllTestsRunner.class)
+	public static class Suite1 {
+		@SuiteSpot
+		public static final List<? extends ITestSuiteClass> suiteClasses = ImmutableList.of( //
+				new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.clazz.tests", "org.eclipse.papyrus.uml.diagram.clazz.test.AllGenTests"),
+				new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.profile.tests", "org.eclipse.papyrus.uml.diagram.profile.tests.AllGenTests"),
+				new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.usecase.tests", "org.eclipse.papyrus.uml.diagram.usecase.test.AllGenTests"));
+	}
+
+	@RunWith(AllTestsRunner.class)
+	public static class Suite2 {
+		@SuiteSpot
+		public static final List<? extends ITestSuiteClass> suiteClasses = ImmutableList.of( //
+				new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.component.tests", "org.eclipse.papyrus.uml.diagram.component.test.AllGenTests"),
+				new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.composite.tests", "org.eclipse.papyrus.uml.diagram.composite.test.AllGenTests"),
+				new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.deployment.tests", "org.eclipse.papyrus.uml.diagram.deployment.test.AllGenTests"));
+	}
+
+	@RunWith(AllTestsRunner.class)
+	public static class Suite3 {
+		@SuiteSpot
+		public static final List<? extends ITestSuiteClass> suiteClasses = ImmutableList.of( //
+				new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.activity.tests", "org.eclipse.papyrus.uml.diagram.activity.test.AllGenTests"),
+				new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.statemachine.tests", "org.eclipse.papyrus.uml.diagram.profile.tests.AllGenTests"));
+	}
+
+	@RunWith(AllTestsRunner.class)
+	public static class Suite4 {
+		@SuiteSpot
+		public static final List<? extends ITestSuiteClass> suiteClasses = ImmutableList.of( //
+				new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.communication.tests", "org.eclipse.papyrus.uml.diagram.communication.test.AllGenTests"),
+				new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.sequence.tests", "org.eclipse.papyrus.uml.diagram.profile.tests.AllGenTests"),
+				new FragmentTestSuiteClass("org.eclipse.papyrus.uml.diagram.timing.tests", "org.eclipse.papyrus.uml.diagram.timing.test.AllGenTests"));
+	}
 }
