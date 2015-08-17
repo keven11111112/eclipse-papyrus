08d170c2b865c3e49ddbc5b6cc67809d26c5a1c1
Florian Noyrit <florian.noyrit@cea.fr>
2015-08-17 20:24:56 +0200

    Bug 474637 : [Releng] Fix RCP issues on MacOs
    Default workspace location set to @user.home/workspace-papyrus
    Initial contrib of a script to repackage RCP: sign for MacOS app and
    Windows. 
    
    Signed-off-by: Florian Noyrit <florian.noyrit@cea.fr>

---
+++ releng/toolkit/server/repackageRcp.sh
@@ -0,0 +1,102 @@
+#--------------------------------------------------------------------------------
+# Copyright (c) 2015 CEA LIST.
+#
+#    
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#	 CEA LIST
+#--------------------------------------------------------------------------------
+
+rcpProductDir=${WORKSPACE}/releng/rcp/org.eclipse.papyrus.rcp.product/target/products/
+rcpProductTempDir=${WORKSPACE}/releng/rcp/products
+product-id=org.eclipse.papyrus.rcp.product
+TMP=${rcpProductDir}/tmp
+papyrusMacApp=Papyrus.app
+papyrusWinApp=papyrus.exe
+papyrusVersion=mars
+
+mkdir $rcpProductTempDir
+
+cd $rcpProductDir
+
+#Proceed with Windows
+for f in *win32*; do
+	echo "Repackaging $f"
+
+	if [[ $f == *x86_64* ]]; then
+		bitness=64
+	else
+		bitness=32
+	fi
+
+	rm -rf $TMP
+	mkdir $TMP
+	cd $TMP
+	
+	unzip -qq $rcpProductDir/$f
+
+	cd Papyrus
+
+	echo "  Signing $papyrusWinApp"
+	curl -o signed.exe -F filedata=@$papyrusWinApp http://build.eclipse.org:31338/winsign.php
+	mv signed.exe $papyrusWinApp
+
+	cd ..
+
+	zip -r -9 -qq --symlinks $rcpProductTempDir/papyrus-$papyrusVersion-win-$bitness.zip Papyrus
+
+	rm -rf $TMP
+done
+
+#Proceed with Linux
+for f in *linux*; do
+	echo "Repackaging $f"
+
+	if [[ $f == *x86_64* ]]; then
+		bitness=64
+	else
+		bitness=32
+	fi
+
+	mv $rcpProductDir/$f $rcpProductDir/papyrus-$papyrusVersion-linux-$bitness.tar.gz
+done
+
+#Proceed with MacOSX
+for f in *macosx*; do
+	echo "Repackaging $f"
+
+	if [[ $f == *x86_64* ]]; then
+		bitness=64
+	else
+		bitness=32
+	fi
+
+	rm -rf $TMP
+	mkdir $TMP
+	cd $TMP
+	
+	tar -xzf $rcpProductDir/$f
+
+	echo "  Signing $papyrusMacApp"
+	zip -r -q unsigned.zip $papyrusMacApp
+	rm -rf $papyrusMacApp
+	curl -o signed.zip -F file=@unsigned.zip http://build.eclipse.org:31338/macsign.php
+	unzip -qq signed.zip
+	rm -f unsigned.zip signed.zip
+
+	chmod a+x "$papyrusMacApp/Contents/MacOS/papyrus"
+	tar -czf $rcpProductTempDir/papyrus-$papyrusVersion-macosx-$bitness.tar.gz $papyrusMacApp
+
+	rm -rf $TMP
+done
+
+cd $rcpProductDir
+rm -f *linux* *macosx* *win32*
+
+mv $rcpProductTempDir/* .
+
+rm -rf $rcpProductTempDir
