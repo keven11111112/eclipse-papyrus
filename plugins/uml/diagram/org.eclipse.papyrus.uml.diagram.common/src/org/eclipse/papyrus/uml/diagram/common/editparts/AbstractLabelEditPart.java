d5e8b6f4ae02b2764000415ae9916675a8e7bf59
mgolubev <golubev@montages.com>
2015-08-28 15:52:37 +0300

    Bug 472023: Replaces default SNAP_FEEDBACK_ROLE policy with the one which does not respond to link creation requests. Thus overlapping external labels does not block link attachment to their "main" nodes.
    Change-Id: I2f9a82fe35248f1f3c342da1858bbaa158797ced
    Signed-off-by: mgolubev <golubev@montages.com>

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.common/src/org/eclipse/papyrus/uml/diagram/common/editparts/AbstractLabelEditPart.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.common/src/org/eclipse/papyrus/uml/diagram/common/editparts/AbstractLabelEditPart.java
@@ -15,6 +15,10 @@
 package org.eclipse.papyrus.uml.diagram.common.editparts;
 
 import org.eclipse.draw2d.IFigure;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.Request;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
+import org.eclipse.gmf.runtime.diagram.ui.internal.editpolicies.LabelSnapBackEditPolicy;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.papyrus.infra.gmfdiag.common.editpart.IPapyrusEditPart;
 import org.eclipse.papyrus.infra.gmfdiag.common.editpart.PapyrusLabelEditPart;
@@ -36,6 +40,37 @@ public abstract class AbstractLabelEditPart extends PapyrusLabelEditPart impleme
 	@Override
 	public IFigure getPrimaryShape() {
 		return getFigure();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void createDefaultEditPolicies() {
+		super.createDefaultEditPolicies();
+		installEditPolicy(EditPolicyRoles.SNAP_FEEDBACK_ROLE, new LabelSnapBackEditPolicyExt());
+	}
+
+	/**
+	 * Overrides default {@link LabelSnapBackEditPolicy} with correct coordination
+	 * between {@link #getCommand(Request)} and {@link #getTargetEditPart(Request)}.
+	 * 
+	 * @see bug 472023
+	 */
+	private static class LabelSnapBackEditPolicyExt extends LabelSnapBackEditPolicy {
+
+		/**
+		 * In contrast to the super-class, this edit-policy does NOT return the host EP as a target
+		 * for those requests it does NOT understand.
+		 *
+		 * @param request
+		 * @return
+		 */
+		@Override
+		public EditPart getTargetEditPart(Request request) {
+			return understandsRequest(request) ? getHost() : null;
+		}
+
 	}
 
 }
