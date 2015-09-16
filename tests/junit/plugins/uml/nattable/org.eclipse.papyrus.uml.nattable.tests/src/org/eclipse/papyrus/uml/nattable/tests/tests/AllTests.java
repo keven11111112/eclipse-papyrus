9eeb6fef6e7ce09751b5030395d99ae6fad57d0d
Benoit Maggi <benoit.maggi@cea.fr>
2015-09-16 16:22:46 +0200

    Bug 477582 - [Nattable][Tests] Add test to validate the models
     - add tests to validate model (tests are failing) 
    
    Change-Id: Ie431eb59ff2ba0ddbac52524a64788a640db8d9b
    Signed-off-by: Benoit Maggi <benoit.maggi@cea.fr>

--- tests/junit/plugins/uml/nattable/org.eclipse.papyrus.uml.nattable.tests/src/org/eclipse/papyrus/uml/nattable/tests/tests/AllTests.java
+++ tests/junit/plugins/uml/nattable/org.eclipse.papyrus.uml.nattable.tests/src/org/eclipse/papyrus/uml/nattable/tests/tests/AllTests.java
@@ -12,13 +12,16 @@
 package org.eclipse.papyrus.uml.nattable.tests.tests;
 
 
+import org.eclipse.papyrus.uml.nattable.tests.tests.configs.GenericTreeTableConfigurationTest;
+import org.eclipse.papyrus.uml.nattable.tests.tests.configs.GenericTreeTableNattableConfigurationTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ UMLRestrictedContentProviderTest.class, UMLRestrictedContentProviderTestCustomProfile.class
-
+@SuiteClasses({ UMLRestrictedContentProviderTest.class, UMLRestrictedContentProviderTestCustomProfile.class,
+	GenericTreeTableConfigurationTest.class,
+	GenericTreeTableNattableConfigurationTest.class
 })
 public class AllTests {
 
---
+++ tests/junit/plugins/uml/nattable/org.eclipse.papyrus.uml.nattable.tests/src/org/eclipse/papyrus/uml/nattable/tests/tests/configs/GenericTreeTableConfigurationTest.java
@@ -0,0 +1,45 @@
+/*****************************************************************************
+ * Copyright (c) 2015 CEA LIST and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Benoit Maggi (CEA LIST) benoit.maggi@cea.fr - Initial API and implementation
+ *   
+ *****************************************************************************/
+
+package org.eclipse.papyrus.uml.nattable.tests.tests.configs;
+
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.Diagnostician;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test the genericTreeTable model :
+ *  - validate the model
+ * 
+ * @author Benoit Maggi
+ */
+public class GenericTreeTableConfigurationTest {
+	
+	// path to the model
+	public static final String GENERIC_TREE_TABLE_CONFIGURATION_MODEL_PATH = org.eclipse.papyrus.uml.nattable.Activator.PLUGIN_ID+"/configs/genericTreeTable.configuration"; //$NON-NLS-0$
+	
+	/**
+	 * Validate the model with the rules defined in the meta-model tooling
+	 */
+	@Test
+	public void validateGenericTreeTableConfigurationModel() {
+		URI createPlatformPluginURI = URI.createPlatformPluginURI(GENERIC_TREE_TABLE_CONFIGURATION_MODEL_PATH, true);
+		Resource resource = new ResourceSetImpl().getResource(createPlatformPluginURI, true);
+		Diagnostic diagnostic = Diagnostician.INSTANCE.validate(resource.getContents().get(0));
+		Assert.assertEquals("The genericTreeTable model is not valid ", Diagnostic.OK, diagnostic.getSeverity());
+	}
+}
