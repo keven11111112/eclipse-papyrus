795b4df0bea78c2461043b347998f2e6619507ef
atischenko <tischenko@montages.com>
2015-09-18 11:23:12 +0300

    Bug 475371 - Copy paste Class do not copy all ownedElements
    Change-Id: I635e2b3bdd76ac22fe197f039db72b11b6e77c6d
    Signed-off-by: atischenko <tischenko@montages.com>

--- tests/junit/plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.composite.tests/test/org/eclipse/papyrus/uml/diagram/composite/test/AllTests.java
+++ tests/junit/plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.composite.tests/test/org/eclipse/papyrus/uml/diagram/composite/test/AllTests.java
@@ -18,6 +18,7 @@ import org.eclipse.papyrus.junit.framework.classification.ClassificationSuite;
 import org.eclipse.papyrus.junit.framework.classification.ClassificationSuite.DynamicClasses;
 import org.eclipse.papyrus.uml.diagram.composite.test.canonical.AllCanonicalTests;
 import org.eclipse.papyrus.uml.diagram.composite.test.model.AllModelTests;
+import org.eclipse.papyrus.uml.diagram.composite.tests.copyPaste.ConstraintCopyPasteTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
@@ -29,7 +30,8 @@ import org.junit.runners.Suite.SuiteClasses;
 		// canonical
 		AllCanonicalTests.class,
 		RoundedCompartmentTest.class,
-		AllModelTests.class
+		AllModelTests.class,
+		ConstraintCopyPasteTest.class
 })
 @DynamicClasses("org.eclipse.papyrus.uml.diagram.composite.test.AllGenTests")
 public class AllTests {
