2665cbb58250ee7d97817ed4ab8307f38ec3dcc7
ashatilov <shatilov@montages.com>
2015-09-23 10:22:28 +0300

    Bug 446054 - [All Diagrams] No graphical impact when changing Comment/Constraint link width
     - "Comment Link": Communication,Component,Composite,Deployment,
    Activity
     - "Constraint Link": StateMachine,Activity
    
    Change-Id: Ie123b2c3322846a7e484d9a5ddb837ebba9df034
    Signed-off-by: ashatilov <shatilov@montages.com>

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.activity/src/org/eclipse/papyrus/uml/diagram/activity/edit/parts/ConstraintConstrainedElementEditPart.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.activity/src/org/eclipse/papyrus/uml/diagram/activity/edit/parts/ConstraintConstrainedElementEditPart.java
@@ -4,14 +4,14 @@ import org.eclipse.draw2d.Connection;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ITreeBranchEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
 import org.eclipse.gmf.runtime.notation.View;
-import org.eclipse.papyrus.infra.gmfdiag.common.editpart.ConnectionEditPart;
 import org.eclipse.papyrus.infra.gmfdiag.common.editpolicies.DefaultSemanticEditPolicy;
 import org.eclipse.papyrus.uml.diagram.activity.figures.LinkFigure;
+import org.eclipse.papyrus.uml.diagram.common.editparts.UMLConnectionNodeEditPart;
 
 /**
  * @generated
  */
-public class ConstraintConstrainedElementEditPart extends ConnectionEditPart implements ITreeBranchEditPart {
+public class ConstraintConstrainedElementEditPart extends UMLConnectionNodeEditPart implements ITreeBranchEditPart {
 
 	/**
 	 * @generated
