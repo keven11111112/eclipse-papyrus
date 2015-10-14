29b7fa982f12e768669225af4e7a16d47ad81e38
Juan Cadavid <juan.cadavid@cea.fr>
2015-10-14 21:22:49 +0200

    BPMN lanes implementation
    Signed-off-by: Juan Cadavid <juan.cadavid@cea.fr>

--- extraplugins/bpmn/org.eclipse.papyrus.bpmn.diagram.common/src/org/eclipse/papyrus/bpmn/diagram/common/providers/CustomBpmnUMLEditPartFactory.java
+++ extraplugins/bpmn/org.eclipse.papyrus.bpmn.diagram.common/src/org/eclipse/papyrus/bpmn/diagram/common/providers/CustomBpmnUMLEditPartFactory.java
@@ -1,8 +1,20 @@
+/*****************************************************************************
+ * Copyright (c) 2015 CEA LIST.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Juan Cadavid (CEA LIST) juan.cadavid@cea.fr - Initial API and implementation
+ *****************************************************************************/
 package org.eclipse.papyrus.bpmn.diagram.common.providers;
 
 import org.eclipse.gef.EditPart;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.papyrus.bpmn.diagram.common.edit.part.LaneEditPart;
+import org.eclipse.papyrus.bpmn.diagram.common.edit.part.LaneLaneContentCompartmentEditPart;
 import org.eclipse.papyrus.bpmn.diagram.common.edit.part.LaneNameEditPart;
 import org.eclipse.papyrus.uml.diagram.activity.edit.part.CustomUMLEditPartFactory;
 import org.eclipse.papyrus.uml.diagram.activity.edit.parts.ActivityPartitionEditPart;
@@ -15,10 +27,12 @@ public class CustomBpmnUMLEditPartFactory extends CustomUMLEditPartFactory {
 		if (model instanceof View) {
 			View view = (View) model;
 			switch (UMLVisualIDRegistry.getVisualID(view)) {
-			case ActivityPartitionEditPart.VISUAL_ID:
+			case LaneEditPart.VISUAL_ID:
 				return new LaneEditPart(view);
-			case ActivityPartitionNameEditPart.VISUAL_ID:
+			case LaneNameEditPart.VISUAL_ID:
 				return new LaneNameEditPart(view);
+			case LaneLaneContentCompartmentEditPart.VISUAL_ID:
+				return new LaneLaneContentCompartmentEditPart(view);
 			}
 		}
 		return super.createEditPart(context, model);
