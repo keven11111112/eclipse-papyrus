2aa76feaa19e4b8270f6359bd2a120a14a899d09
Juan Cadavid <juan.cadavid@cea.fr>
2015-10-17 00:14:16 +0200

    Papyrus4Manufacturing and BPMN examples
    Signed-off-by: Juan Cadavid <juan.cadavid@cea.fr>

---
+++ extraplugins/manufacturing/org.eclipse.papyrus.manufacturing.ofbiz/src/org/eclipse/papyrus/manufacturing/ofbiz/createWorkEffort/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://ofbiz.apache.org/service/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.eclipse.papyrus.manufacturing.ofbiz.createWorkEffort;
