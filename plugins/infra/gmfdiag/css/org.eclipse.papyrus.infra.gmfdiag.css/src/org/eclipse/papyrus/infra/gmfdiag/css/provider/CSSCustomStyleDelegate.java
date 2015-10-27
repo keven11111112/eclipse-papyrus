1bda9384e45a79c0dcd273fe2c4ad127eae615e6
Christian W. Damus <give.a.damus@gmail.com>
2015-10-27 19:39:04 -0400

    Bug 480820: [CSS] Qualified name depth attribute does not work https://bugs.eclipse.org/bugs/show_bug.cgi?id=480820
    Stop always providing negative depth values from CSS, which result in
    fully qualified names in the diagram.
    
    Change-Id: I98440c7e026a0e5565fb9421caff1a9eebbe7cc7

--- plugins/infra/gmfdiag/css/org.eclipse.papyrus.infra.gmfdiag.css/src/org/eclipse/papyrus/infra/gmfdiag/css/provider/CSSCustomStyleDelegate.java
+++ plugins/infra/gmfdiag/css/org.eclipse.papyrus.infra.gmfdiag.css/src/org/eclipse/papyrus/infra/gmfdiag/css/provider/CSSCustomStyleDelegate.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2012 CEA LIST.
+ * Copyright (c) 2012, 2015 CEA LIST, Christian W. Damus, and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,6 +8,7 @@
  *
  * Contributors:
  *  Camille Letavernier (CEA LIST) camille.letavernier@cea.fr - Initial API and implementation
+ *  Christian W. Damus - bug 480820
  *****************************************************************************/
 package org.eclipse.papyrus.infra.gmfdiag.css.provider;
 
@@ -100,7 +101,7 @@ public class CSSCustomStyleDelegate implements CustomStyle {
 
 		try {
 			int value = Integer.parseInt(cssText);
-			return value > 0 ? -value : value;
+			return (value < 0) ? -value : value;
 		} catch (NumberFormatException ex) {
 			engine.handleExceptions(ex);
 			return NONE_VALUE;
