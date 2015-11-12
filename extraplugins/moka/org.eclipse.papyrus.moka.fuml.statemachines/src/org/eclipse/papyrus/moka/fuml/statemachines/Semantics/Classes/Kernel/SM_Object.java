76f1fe9dd993c2f191023fed31d36857212b38dd
jeremie.tatibouet <jeremie.tatibouet@cea.fr>
2015-11-12 19:00:56 +0100

    Completion events are handled according to the FIFO strategy. Tests concerning completion event handling were updated
    Change-Id: I68e088c5295a8a80789cd074985615a34cdf9ef3
    Signed-off-by: jeremie.tatibouet <jeremie.tatibouet@cea.fr>

--- extraplugins/moka/org.eclipse.papyrus.moka.fuml.statemachines/src/org/eclipse/papyrus/moka/fuml/statemachines/Semantics/Classes/Kernel/SM_Object.java
+++ extraplugins/moka/org.eclipse.papyrus.moka.fuml.statemachines/src/org/eclipse/papyrus/moka/fuml/statemachines/Semantics/Classes/Kernel/SM_Object.java
@@ -8,11 +8,21 @@ import org.eclipse.papyrus.moka.fuml.statemachines.Semantics.StateMachines.Behav
 public class SM_Object extends CS_Object {
 
 	public void sendCompletionEvent(StateActivation stateActivation){
-		// A completion event is always added in front off any other events
-		// placed in the event pool.
+		// A completion event does not have priority over other completion events.
+		// Therefore if completion events are already in the event pool then the currently
+		// posted completion event will be placed after these latter. In short the FIFO strategy
+		// is applied to completion events
 		CompletionEventOccurrence completionEventOccurrence = new CompletionEventOccurrence();
 		completionEventOccurrence.stateActivation = stateActivation;
-		this.objectActivation.eventPool.add(0, completionEventOccurrence);
+		int insertionIndex = 0;
+		boolean insertionPointFound = false;
+		while(!insertionPointFound && insertionIndex < this.objectActivation.eventPool.size()){
+			insertionPointFound = !(this.objectActivation.eventPool.get(insertionIndex) instanceof CompletionEventOccurrence);
+			if(!insertionPointFound){
+				insertionIndex++;
+			}
+		}
+		this.objectActivation.eventPool.add(insertionIndex, completionEventOccurrence);
 		this.objectActivation._send(new ArrivalSignal());
 	}
 	
