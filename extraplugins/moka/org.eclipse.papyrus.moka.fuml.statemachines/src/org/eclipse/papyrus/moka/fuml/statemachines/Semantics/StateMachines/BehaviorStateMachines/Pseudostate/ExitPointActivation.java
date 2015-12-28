1d7787e96e808badb255cdfa55cd5137ca5db4a5
jeremie.tatibouet <jeremie.tatibouet@cea.fr>
2015-12-28 18:47:33 +0100

    If multiple transitions originate from an ExitPoint and their guards are true then only one of them is fired. The one that is fired is selected based on the choice provided by the ChoiceStrategy.
    Change-Id: I561459dd17409b76cbb9ec0af991db5afaa3704c
    Signed-off-by: jeremie.tatibouet <jeremie.tatibouet@cea.fr>

--- extraplugins/moka/org.eclipse.papyrus.moka.fuml.statemachines/src/org/eclipse/papyrus/moka/fuml/statemachines/Semantics/StateMachines/BehaviorStateMachines/Pseudostate/ExitPointActivation.java
+++ extraplugins/moka/org.eclipse.papyrus.moka.fuml.statemachines/src/org/eclipse/papyrus/moka/fuml/statemachines/Semantics/StateMachines/BehaviorStateMachines/Pseudostate/ExitPointActivation.java
@@ -13,6 +13,10 @@
  *****************************************************************************/
 package org.eclipse.papyrus.moka.fuml.statemachines.Semantics.StateMachines.BehaviorStateMachines.Pseudostate;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.ChoiceStrategy;
 import org.eclipse.papyrus.moka.fuml.statemachines.Semantics.StateMachines.BehaviorStateMachines.RegionActivation;
 import org.eclipse.papyrus.moka.fuml.statemachines.Semantics.StateMachines.BehaviorStateMachines.TransitionActivation;
 import org.eclipse.papyrus.moka.fuml.statemachines.Semantics.StateMachines.BehaviorStateMachines.TransitionActivation.TransitionMetadata;
@@ -34,17 +38,32 @@ public class ExitPointActivation extends ConnectionPointActivation {
 		return enterable;
 	}
 
+	protected List<TransitionActivation> getFireableTransitions() {
+		// Determine the list of transitions originating from this pseudo-state that can be fired.
+		// A transition is considered as being fireable as soon as its guard evaluates to true.
+		List<TransitionActivation> fireableTransitions = new ArrayList<TransitionActivation>();
+		for (int i = 0; i < this.outgoingTransitionActivations.size(); i++) {
+			TransitionActivation transitionActivation = this.outgoingTransitionActivations.get(i);
+			if (transitionActivation.evaluateGuard()) {
+				fireableTransitions.add(transitionActivation);
+			}
+		}
+		return fireableTransitions;
+	}
+
 	public void enter(TransitionActivation enteringTransition, RegionActivation leastCommonAncestor) {
-		// Exit a state through an entry point.
-		if (this.isEnterable() && !this.outgoingTransitionActivations.isEmpty()) {
-			RegionActivation newLeastCommonAncestor = this.getLeastCommonAncestor(this.outgoingTransitionActivations.get(0).getTargetActivation());
+		List<TransitionActivation> fireableTransitions = this.getFireableTransitions();
+		if (this.isEnterable() && !fireableTransitions.isEmpty()) {
+			ChoiceStrategy choiceStrategy = (ChoiceStrategy) this.getExecutionLocus().factory.getStrategy("choice");
+			int chosenIndex = choiceStrategy.choose(fireableTransitions.size());
+			TransitionActivation selectedTransition = fireableTransitions.get(chosenIndex - 1);
+			RegionActivation newLeastCommonAncestor = this.getLeastCommonAncestor(selectedTransition.getTargetActivation());
 			super.enter(enteringTransition, leastCommonAncestor);
 			VertexActivation vertexActivation = this.getParentState();
 			if (vertexActivation != null) {
 				vertexActivation.exit(enteringTransition, newLeastCommonAncestor);
 			}
-			// FIXME: Can this transition have guards and triggers ?
-			this.outgoingTransitionActivations.get(0).fire();
+			selectedTransition.fire();
 		}
 	}
 }
