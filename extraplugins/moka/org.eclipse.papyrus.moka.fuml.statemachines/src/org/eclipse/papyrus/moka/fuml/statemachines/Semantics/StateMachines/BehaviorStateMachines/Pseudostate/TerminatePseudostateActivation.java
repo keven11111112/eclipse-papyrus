65b9415c69f0c337356a16954008c0519fbaa4e2
jeremie.tatibouet <jeremie.tatibouet@cea.fr>
2016-01-14 18:08:07 +0100

    Make sure Terminate behaves correctly when placed in a nested context. Verify this behavior with the new test Terminate_003
    Address Bug 485863 and Bug 485864
    
    Change-Id: I84cc9a15a7fd2e015a5771febda05e36d1663448
    Signed-off-by: jeremie.tatibouet <jeremie.tatibouet@cea.fr>

--- extraplugins/moka/org.eclipse.papyrus.moka.fuml.statemachines/src/org/eclipse/papyrus/moka/fuml/statemachines/Semantics/StateMachines/BehaviorStateMachines/Pseudostate/TerminatePseudostateActivation.java
+++ extraplugins/moka/org.eclipse.papyrus.moka.fuml.statemachines/src/org/eclipse/papyrus/moka/fuml/statemachines/Semantics/StateMachines/BehaviorStateMachines/Pseudostate/TerminatePseudostateActivation.java
@@ -3,6 +3,7 @@ package org.eclipse.papyrus.moka.fuml.statemachines.Semantics.StateMachines.Beha
 import org.eclipse.papyrus.moka.fuml.Semantics.CommonBehaviors.BasicBehaviors.Execution;
 import org.eclipse.papyrus.moka.fuml.statemachines.Semantics.StateMachines.BehaviorStateMachines.RegionActivation;
 import org.eclipse.papyrus.moka.fuml.statemachines.Semantics.StateMachines.BehaviorStateMachines.TransitionActivation;
+import org.eclipse.papyrus.moka.fuml.statemachines.Semantics.StateMachines.BehaviorStateMachines.VertexActivation;
 
 public class TerminatePseudostateActivation extends PseudostateActivation {
 
@@ -10,6 +11,12 @@ public class TerminatePseudostateActivation extends PseudostateActivation {
 		// When a terminate pseudo state is entered the state-machine executing terminates its execution.
 		// This termination occurs without exiting "exit" behaviors of state(s) that are currently
 		// registered in the state-machine configuration.
+		if (leastCommonAncestor != null && this.getParent() != leastCommonAncestor) {
+			VertexActivation parentVertexActivation = this.getParentState();
+			if (parentVertexActivation != null) {
+				parentVertexActivation.enter(enteringTransition, leastCommonAncestor);
+			}
+		}
 		super.enter(enteringTransition, leastCommonAncestor);
 		Execution stateMachineExecution = this.getStateMachineExecution();
 		stateMachineExecution.terminate();
