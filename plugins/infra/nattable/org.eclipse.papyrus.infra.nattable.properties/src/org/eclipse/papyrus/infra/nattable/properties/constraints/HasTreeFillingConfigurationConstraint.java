714e7c47fc6ba11406f345643290700ff16427dd
Nicolas FAUVERGUE <nicolas.fauvergue@all4tec.net>
2016-01-21 14:38:41 +0100

    Bug 479318: [Tree Table][property View] Papyrus should provide a specific Property View for Tree Table https://bugs.eclipse.org/bugs/show_bug.cgi?id=479318
    Bug 481014: [Table]Invert Axis is available in Property View for Tree
    Table
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=481014
    
    - The insert axis is not available when a tree table is selected (for
    table and appearance tabs)
    - The paste configuration is available for tree table only when the
    depth
    0 has no TreeFillingConfiguration
    
    On this patch, some problems of display are always available
    (particulary for invert axis) but this must be corrected with the bug
    481015.
    
    Change-Id: I087e20cab37fd6fb7266bb5e443ef8c7b2633166
    Signed-off-by: Nicolas FAUVERGUE <nicolas.fauvergue@all4tec.net>

--- plugins/infra/nattable/org.eclipse.papyrus.infra.nattable.properties/src/org/eclipse/papyrus/infra/nattable/properties/constraints/HasTreeFillingConfigurationConstraint.java
+++ plugins/infra/nattable/org.eclipse.papyrus.infra.nattable.properties/src/org/eclipse/papyrus/infra/nattable/properties/constraints/HasTreeFillingConfigurationConstraint.java
@@ -1,37 +1,39 @@
 /*****************************************************************************
- * Copyright (c) 2013 CEA LIST.
- *
+ * Copyright (c) 2015 CEA LIST and others.
+ * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *  Vincent Lorenzo (CEA LIST) vincent.lorenzo@cea.fr - Initial API and implementation
- *  Nicolas FAUVERGUE (ALL4TEC) nicolas.fauvergue@all4tec.net - Bug 476318
- *
+ *   Nicolas FAUVERGUE (ALL4TEC) nicolas.fauvergue@all4tec.net - Initial API and implementation
+ *   
  *****************************************************************************/
+
 package org.eclipse.papyrus.infra.nattable.properties.constraints;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.papyrus.infra.constraints.constraints.JavaQuery;
 import org.eclipse.papyrus.infra.emf.utils.EMFHelper;
 import org.eclipse.papyrus.infra.nattable.model.nattable.Table;
-import org.eclipse.papyrus.infra.nattable.utils.CreatableEObjectAxisUtils;
+import org.eclipse.papyrus.infra.nattable.utils.FillingConfigurationUtils;
+import org.eclipse.papyrus.infra.nattable.utils.TableHelper;
 
 /**
- * This allows to define the constraint to not create the paste properties.
+ * The constraint to define when no TreeFillingConfiguration is available in table.
  */
-public class CantCreateRowElementContraint extends HasTreeFillingConfigurationConstraint {
+public class HasTreeFillingConfigurationConstraint implements JavaQuery {
 
 	/**
 	 * {@inheritDoc}
 	 * 
-	 * @see org.eclipse.papyrus.infra.nattable.properties.constraints.HasTreeFillingConfigurationConstraint#match(java.lang.Object)
+	 * @see org.eclipse.papyrus.infra.constraints.constraints.JavaQuery#match(java.lang.Object)
 	 */
 	@Override
 	public boolean match(final Object selection) {
 		final EObject table = EMFHelper.getEObject(selection);
-		return !super.match(selection) && 0 == CreatableEObjectAxisUtils.getCreatableElementIds((Table) table, false).size();
+		return table instanceof Table && TableHelper.isTreeTable((Table) table) && FillingConfigurationUtils.hasTreeFillingConfigurationForDepth((Table) table, 0);
 	}
 
 }
