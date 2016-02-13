d72fa2795fb2cc3209fc92eb4eb0b7b5b4795b52
Christian W. Damus <give.a.damus@gmail.com>
2016-02-13 14:30:47 -0500

    Bug 485220: Fix simple metadata problems in new bundle https://bugs.eclipse.org/bugs/show_bug.cgi?id=485220

--- plugins/infra/emf/org.eclipse.papyrus.infra.emf.gmf/src/org/eclipse/papyrus/infra/emf/internal/gmf/Activator.java
+++ plugins/infra/emf/org.eclipse.papyrus.infra.emf.gmf/src/org/eclipse/papyrus/infra/emf/internal/gmf/Activator.java
@@ -25,7 +25,7 @@ public class Activator extends Plugin {
 	/**
 	 * The plug-in ID
 	 */
-	public static final String PLUGIN_ID = "org.eclipse.papyrus.infra.emf,gmf"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.eclipse.papyrus.infra.emf.gmf"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
