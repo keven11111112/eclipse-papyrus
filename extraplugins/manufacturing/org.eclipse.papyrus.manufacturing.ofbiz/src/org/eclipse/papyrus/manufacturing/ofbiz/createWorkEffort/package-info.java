0edefd99465a8d65b88c00da6fd559e14dec25d0
Juan Cadavid <juan.cadavid@cea.fr>
2016-02-26 14:57:01 +0100

    [Manufacturing] moving oep.manufacturing.ofbiz to a separate branch
    Change-Id: I9d4da4197f21ebaeb33933f18ba6c884f7017501
    Signed-off-by: Juan Cadavid <juan.cadavid@cea.fr>

--- extraplugins/manufacturing/org.eclipse.papyrus.manufacturing.ofbiz/src/org/eclipse/papyrus/manufacturing/ofbiz/createWorkEffort/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ofbiz.apache.org/service/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.eclipse.papyrus.manufacturing.ofbiz.createWorkEffort;
