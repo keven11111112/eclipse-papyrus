6fa573ace49d31b832ed095e4d7ca542a386bcf8
jeremie.tatibouet <jeremie.tatibouet@cea.fr>
2016-03-04 14:30:22 +0100

    Improvements of state-machine configuration analysis. - Selection is based on a recursive approach. - Selection makes sure event occurrence dispatching impact is evaluated on all regions.
    Change-Id: I072c3843913987f7c5cdef534374bd908b054173
    Signed-off-by: jeremie.tatibouet <jeremie.tatibouet@cea.fr>

--- extraplugins/moka/org.eclipse.papyrus.moka.fuml.statemachines/src/org/eclipse/papyrus/moka/fuml/statemachines/Semantics/StateMachines/DefaultTransitionSelectionStrategy.java
+++ extraplugins/moka/org.eclipse.papyrus.moka.fuml.statemachines/src/org/eclipse/papyrus/moka/fuml/statemachines/Semantics/StateMachines/DefaultTransitionSelectionStrategy.java
@@ -14,9 +14,7 @@
 package org.eclipse.papyrus.moka.fuml.statemachines.Semantics.StateMachines;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.papyrus.moka.fuml.Semantics.CommonBehaviors.Communications.EventOccurrence;
 import org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.ChoiceStrategy;
@@ -28,50 +26,54 @@ public class DefaultTransitionSelectionStrategy extends TransitionSelectionStrat
 	}
 	
 	public boolean isDeferred(EventOccurrence eventOccurrence){
-		// Returns true when at least one state within the configuration can defers the given
-		// event occurrence. The evaluation starts from the innermost part of the state-machine
-		// configuration. If no state can defer the given event occurrence false is returned.
-		boolean isDeferred = false;
-		boolean isTriggered = false;
-		Map<Integer, List<VertexActivation>> cartography = this.execution.getConfiguration().getCartography();
-		int i = cartography.size();
-		while(i >= 1 && (!isDeferred && !isTriggered)){
-			List<VertexActivation> verticesActivations = cartography.get(i);
-			int j = 0;
-			while(j < verticesActivations.size() && (!isDeferred && !isTriggered)){
-				StateActivation currentStateActivation = (StateActivation) verticesActivations.get(j);
-				isTriggered = !currentStateActivation.getFireableTransitions(eventOccurrence).isEmpty();
-				if(!isTriggered && currentStateActivation.canDefer(eventOccurrence)){
-					isDeferred = true;
-				}
-				j++;
-			}
-			i--;
+		// Determine if the dispatched event occurrence is deferred in the
+		// current state machine configuration. An event occurrence can only be deferred
+		// if the following conditions are fulfilled:
+		// 1 - One active state in the hierarchy declares the event types as being deferred.
+		// 2 - No transitions (ready to fire) with a higher priority than the deferring state
+		//     could be found.
+		return this._isDeferred(eventOccurrence, this.execution.getConfiguration().rootConfiguration);
+	}
+	
+	public boolean _isDeferred(EventOccurrence eventOccurrence, StateConfiguration stateConfiguration){
+		// Determine if the given state configuration is capable of deferring the given event occurrence.
+		int i = 0;
+		boolean deferred = false;
+		while(!deferred && i < stateConfiguration.children.size()){
+			deferred = this._isDeferred(eventOccurrence, stateConfiguration.children.get(i));
+			i++;
 		}
-		return isDeferred;
+		if(!deferred && 
+				stateConfiguration.vertexActivation != null && 
+				this._select(eventOccurrence, stateConfiguration).isEmpty() &&
+				((StateActivation)stateConfiguration.vertexActivation).canDefer(eventOccurrence)){
+				deferred = true;
+		}
+		return deferred;
 	}
 	
 	public void defer(EventOccurrence eventOccurrence){
-		// The state indicating that the given event occurrence is deferred defers the given event
-		// occurrence. When deferred the event occurrence is placed within the deferred event pool.
-		// This operation is intended to be called only and only if the "isDeferred" operation
-		// returned true.
-		boolean isDeferred = false;
-		Map<Integer, List<VertexActivation>> cartography = this.execution.getConfiguration().getCartography();
-		int i = cartography.size();
-		while(i >= 1 && !isDeferred){
-			int j = 0;
-			List<VertexActivation> verticesActivations = cartography.get(i);
-			while(j < verticesActivations.size() && !isDeferred){
-				StateActivation currentStateActivation = (StateActivation) verticesActivations.get(j);
-				if(currentStateActivation.canDefer(eventOccurrence)){
-					currentStateActivation.defer(eventOccurrence);
-					isDeferred = true;
-				}
-				j++;
-			}
-			i--;
+		// Defers the given event occurrence. A deferred event occurrence is registered in
+		// the deferred event pool. This latter refers to the deferred event as well as to the
+		// the deferring state.
+		this._defer(eventOccurrence, this.execution.getConfiguration().rootConfiguration);
+	}
+	
+	protected boolean _defer(EventOccurrence eventOccurrence, StateConfiguration stateConfiguration){
+		// Defers the given event occurrence in the context of the given state configuration.
+		int i = 0;
+		boolean deferred = false;
+		while(!deferred && i < stateConfiguration.children.size()){
+			deferred = this._defer(eventOccurrence, stateConfiguration.children.get(i));
+			i++;
 		}
+		if(!deferred &&
+				stateConfiguration.vertexActivation != null &&
+				((StateActivation)stateConfiguration.vertexActivation).canDefer(eventOccurrence)){
+			((StateActivation)stateConfiguration.vertexActivation).defer(eventOccurrence);
+			deferred = true;
+		}
+		return deferred;
 	}
 
 	public boolean isTriggering(EventOccurrence eventOccurrence){
@@ -81,126 +83,32 @@ public class DefaultTransitionSelectionStrategy extends TransitionSelectionStrat
 	}
 	
 	public List<TransitionActivation> select(EventOccurrence eventOccurrence) {
-		// Find for the given configuration the set of transition that can fire. The search
-		// starts from the deepest level. If no transition enabled are found at this level then
-		// the search continue in the upper level. The search stops when at given level a not empty
-		// set of enabled transition is calculated. Transition returned are only those that are not
-		// triggered by an event (i.e. Transitions that are automatic or only guarded).
+		// Find for the given configuration the set of transition that can fire.
+		return this._select(eventOccurrence, this.execution.getConfiguration().rootConfiguration);
+	}
+	
+	protected List<TransitionActivation> _select(EventOccurrence eventOccurrence, StateConfiguration stateConfiguration){
+		// Find for the given state configuration all transition that can actually fire.
+		// The set of transition only contains transitions with the highest priority. In addition
+		// no conflicting transitions are added to that set.
 		List<TransitionActivation> selectedTransitions = new ArrayList<TransitionActivation>();
-		Map<Integer, List<VertexActivation>> cartography = this.execution.getConfiguration().getCartography();
-		int i = cartography.size();
-		boolean nextLevel = true;
-		while(i >= 1 && nextLevel){
-			for(VertexActivation vertexActivation : cartography.get(i)){
-				StateActivation currentStateActivation = (StateActivation) vertexActivation;
-				List<TransitionActivation> fireableTransitions = new ArrayList<TransitionActivation>();
-				for(TransitionActivation transitionActivation : currentStateActivation.getOutgoingTransitions()){
-					if(transitionActivation.canFireOn(eventOccurrence)){
-						fireableTransitions.add(transitionActivation);
-					}
-				}
-				selectedTransitions.addAll(fireableTransitions);
-			}
-			if(!selectedTransitions.isEmpty()){
-				nextLevel = false;
-			}else{
-				i--;
-			}
+		for(int i = 0; i < stateConfiguration.children.size(); i++){
+			selectedTransitions.addAll(this._select(eventOccurrence, stateConfiguration.children.get(i)));
 		}
-		List<List<TransitionActivation>> groups = this.group(selectedTransitions);
-		ChoiceStrategy choiceStrategy = (ChoiceStrategy) this.execution.locus.factory.getStrategy("choice");
-		int choice = choiceStrategy.choose(groups.size()) - 1;
-		return groups.get(choice);
-	}
-	
-	protected List<List<TransitionActivation>> group(List<TransitionActivation> fireableTransitions) {
-		// The purpose here is to choose among a set of fireable transitions a subset that will
-		// effectively be fired. This subset can contain in the most trivial case a single
-		// transition. In the case of orthogonal region it can contain several transitions
-		// that will be fired using the same event occurrence
-		Map<VertexActivation, List<TransitionActivation>> transitionGroups = new HashMap<VertexActivation, List<TransitionActivation>>();
-		List<List<TransitionActivation>> groupingResults = new ArrayList<List<TransitionActivation>>();
-		if(fireableTransitions.isEmpty()){
-			groupingResults.add(new ArrayList<TransitionActivation>());
-		}else{
-			int i = 0;
-			while(i < fireableTransitions.size()){
-				TransitionActivation transitionActivation = fireableTransitions.get(i);
-				List<TransitionActivation> group = transitionGroups.get(transitionActivation.getSourceActivation().getParentState());
-				if(group==null){
-					group = new ArrayList<TransitionActivation>();
-					group.add(transitionActivation);
-					transitionGroups.put(transitionActivation.getSourceActivation().getParentState(), group);
-				}else{
-					group.add(transitionActivation);
-				}
-				i++;
-			}
-			for(VertexActivation vertexActivation: transitionGroups.keySet()){
-				List<TransitionActivation> group = transitionGroups.get(vertexActivation);
-				List<TransitionActivation> conflictingTransitions = this.getConflictingTransitions(group);
-				if(!conflictingTransitions.isEmpty()){
-					this.resolveConflitcs(group, conflictingTransitions);
-				}
-				groupingResults.add(group);
-				i++;
-			}
-		}
-		return groupingResults;
-	}
-
-	
-	protected List<TransitionActivation> getConflictingTransitions(List<TransitionActivation> group){
-		// Detect conflicting transitions located in a group of transitions. Conflicting transitions
-		// are transitions which are in the firing set but which originate from the same source vertex.
-		List<TransitionActivation> conflictingTransitions = new ArrayList<TransitionActivation>();
-		int i = 0;
-		while(i < group.size()){
-			TransitionActivation referenceTransition = group.get(i);
-			for(int j=0; j < group.size(); j++){
-				TransitionActivation currentTransition = group.get(j);
-				if(referenceTransition!=currentTransition 
-						&& !conflictingTransitions.contains(currentTransition)
-						&& referenceTransition.getSourceActivation()==currentTransition.getSourceActivation()){
-						if(!conflictingTransitions.contains(referenceTransition)){
-							conflictingTransitions.add(referenceTransition);
-						}
-						conflictingTransitions.add(currentTransition);
+		if(selectedTransitions.isEmpty() && stateConfiguration.vertexActivation != null){
+			for(int i = 0; i < stateConfiguration.vertexActivation.getOutgoingTransitions().size(); i++){
+				TransitionActivation transitionActivation = stateConfiguration.vertexActivation.getOutgoingTransitions().get(i);
+				if(transitionActivation.canFireOn(eventOccurrence)){
+					selectedTransitions.add(transitionActivation);
 				}
 			}
-			i++;
-		}
-		return conflictingTransitions;
-	}
-	
-	protected void resolveConflitcs(List<TransitionActivation> group, List<TransitionActivation> conflictingTransitions){
-		// The conflict resolution consist in the following set of actions:
-		// 1 - Conflicting transitions are grouped in different set. The grouping criteria is the origin of a transition
-		// 2 - For each set a transition is elected (the selection is a semantic variation point) to stay in the firing set.
-		//     Other transitions are  removed from the firing set.
-		Map<VertexActivation,List<TransitionActivation>> transitionsPerOrigin = new HashMap<VertexActivation, List<TransitionActivation>>();
-		// 1 - Grouping phase
-		for(int i=0; i < conflictingTransitions.size(); i++){
-			TransitionActivation currentTransitionActivation = conflictingTransitions.get(i);
-			List<TransitionActivation> transitionGroup = transitionsPerOrigin.get(currentTransitionActivation.getSourceActivation());
-			if(transitionGroup==null){
-				transitionGroup = new ArrayList<TransitionActivation>();
-				transitionGroup.add(currentTransitionActivation);
-				transitionsPerOrigin.put(currentTransitionActivation.getSourceActivation(), transitionGroup);
-			}else{
-				transitionGroup.add(currentTransitionActivation);
+			if(selectedTransitions.size() > 1){
+				ChoiceStrategy choiceStrategy = (ChoiceStrategy) this.execution.locus.factory.getStrategy("choice");
+				TransitionActivation electedTransition = selectedTransitions.get(choiceStrategy.choose(selectedTransitions.size()) - 1);
+				selectedTransitions.clear();
+				selectedTransitions.add(electedTransition);
 			}
 		}
-		// 2 - Conflict resolution
-		ChoiceStrategy choiceStrategy = (ChoiceStrategy) this.execution.locus.factory.getStrategy("choice");
-		for(VertexActivation origin: transitionsPerOrigin.keySet()){
-			List<TransitionActivation> transitionGroup = transitionsPerOrigin.get(origin);
-			int electedIndex = choiceStrategy.choose(transitionGroup.size()) - 1;
-			for(int i=0; i < transitionGroup.size(); i++){
-				if(i!=electedIndex){
-					group.remove(transitionGroup.get(i));
-				}
-			}
-		}
+		return selectedTransitions;
 	}
 }
