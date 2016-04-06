39507c0d322af147497a7b2d3fd375f1abc4afe3
Shuai Li <shuai.li@cea.fr>
2016-04-06 14:33:01 +0200

    Bug 407733 - [General] Papyrus shall support navigation history with back and next buttons
    - Update for post-refactoring Neon
    
    Change-Id: I23a0a26449c2bda1e162d07e659680bea218ebcd
    Signed-off-by: Shuai Li <shuai.li@cea.fr>

--- plugins/editor/org.eclipse.papyrus.editor/src/org/eclipse/papyrus/editor/PapyrusMultiDiagramEditor.java
+++ plugins/editor/org.eclipse.papyrus.editor/src/org/eclipse/papyrus/editor/PapyrusMultiDiagramEditor.java
@@ -16,6 +16,8 @@ package org.eclipse.papyrus.editor;
 
 import org.eclipse.papyrus.infra.core.sasheditor.editor.ISashWindowsContainer;
 import org.eclipse.papyrus.infra.ui.editor.CoreMultiDiagramEditor;
+import org.eclipse.ui.INavigationLocation;
+import org.eclipse.ui.INavigationLocationProvider;
 
 /**
  * Papyrus main MultiEditor. This class add GMF adaptation dedicated to Papyrus.
@@ -24,7 +26,7 @@ import org.eclipse.papyrus.infra.ui.editor.CoreMultiDiagramEditor;
  *
  * @author dumoulin
  */
-public class PapyrusMultiDiagramEditor extends CoreMultiDiagramEditor {
+public class PapyrusMultiDiagramEditor extends CoreMultiDiagramEditor implements INavigationLocationProvider  {
 
 	/**
 	 * The Papyrus Editor ID
@@ -32,12 +34,16 @@ public class PapyrusMultiDiagramEditor extends CoreMultiDiagramEditor {
 	public static final String EDITOR_ID = "org.eclipse.papyrus.infra.core.papyrusEditor"; //$NON-NLS-1$
 
 	private PapyrusPaletteSynchronizer myPaletteViewSynchronizer;
+	
+	private PapyrusNavigationHistorySynchronizer myNavigationHistorySynchronizer;
 
 	@Override
 	protected void activate() {
 		super.activate();
 		myPaletteViewSynchronizer = new PapyrusPaletteSynchronizer(this);
+		myNavigationHistorySynchronizer = new PapyrusNavigationHistorySynchronizer(this);
 		getISashWindowsContainer().addPageChangedListener(myPaletteViewSynchronizer);
+		getISashWindowsContainer().addPageChangedListener(myNavigationHistorySynchronizer);
 	}
 
 	@Override
@@ -46,6 +52,7 @@ public class PapyrusMultiDiagramEditor extends CoreMultiDiagramEditor {
 			ISashWindowsContainer sashContainer = getISashWindowsContainer();
 			if (sashContainer != null && !sashContainer.isDisposed()) {
 				sashContainer.removePageChangedListener(myPaletteViewSynchronizer);
+				sashContainer.removePageChangedListener(myNavigationHistorySynchronizer);
 			}
 			myPaletteViewSynchronizer.dispose();
 			myPaletteViewSynchronizer = null;
@@ -53,4 +60,23 @@ public class PapyrusMultiDiagramEditor extends CoreMultiDiagramEditor {
 		super.deactivate();
 	}
 
+	/**
+	 * @see org.eclipse.ui.INavigationLocationProvider#createEmptyNavigationLocation()
+	 *
+	 * @return
+	 */
+	@Override
+	public INavigationLocation createEmptyNavigationLocation() {
+		return null;
+	}
+
+	/**
+	 * @see org.eclipse.ui.INavigationLocationProvider#createNavigationLocation()
+	 *
+	 * @return
+	 */
+	@Override
+	public INavigationLocation createNavigationLocation() {
+		return new PapyrusNavigationLocation(this);
+	}
 }
