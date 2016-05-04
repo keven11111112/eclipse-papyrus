6e8c5e413d70a0e7e4827e56766aecd982c89021
Mickael ADAM <mickael.adam@ALL4TEC.net>
2016-05-04 18:07:29 +0200

    Bug 489118 - [UseCase Diagram] Extension Points cannot be shown
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=489118
    
    
    Change-Id: Ib205fa472fa6a37be9d9944a6e044db24e75e5ef
    Signed-off-by: Mickael ADAM <mickael.adam@ALL4TEC.net>

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.usecase/custom-src/org/eclipse/papyrus/uml/diagram/usecase/custom/edit/parts/UseCaseNodeEditPart.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.usecase/custom-src/org/eclipse/papyrus/uml/diagram/usecase/custom/edit/parts/UseCaseNodeEditPart.java
@@ -2,6 +2,7 @@ package org.eclipse.papyrus.uml.diagram.usecase.custom.edit.parts;
 
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.papyrus.uml.diagram.common.editparts.RoundedCompartmentEditPart;
+import org.eclipse.papyrus.uml.diagram.common.editpolicies.ShowHideClassifierContentsEditPolicy;
 
 /**
  * @author Mickael ADAM
@@ -16,6 +17,20 @@ abstract public class UseCaseNodeEditPart extends RoundedCompartmentEditPart {
 	 */
 	public UseCaseNodeEditPart(View view) {
 		super(view);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * Add missing edit policy to show/hide contents (Bug 489118)
+	 * 
+	 * @see org.eclipse.papyrus.uml.diagram.common.editparts.RoundedCompartmentEditPart#createDefaultEditPolicies()
+	 *
+	 */
+	@Override
+	protected void createDefaultEditPolicies() {
+		super.createDefaultEditPolicies();
+		installEditPolicy(ShowHideClassifierContentsEditPolicy.SHOW_HIDE_CLASSIFIER_CONTENTS_POLICY, new ShowHideClassifierContentsEditPolicy());
 	}
 
 	/**
