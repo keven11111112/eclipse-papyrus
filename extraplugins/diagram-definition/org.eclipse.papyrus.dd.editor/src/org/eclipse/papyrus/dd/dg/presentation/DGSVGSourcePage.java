bdd19048b40d549cdf341116efd953c97fdd8a16
Remi Schnekenburger <remi.schnekenburger@cea.fr>
2016-05-27 11:12:09 +0200

    Bug 494621: [IPlog] Remove batik.* jar from git repository
    - remove any external reference to orbit plugins
    
    Signed-off-by: Remi Schnekenburger <remi.schnekenburger@cea.fr>

--- extraplugins/diagram-definition/org.eclipse.papyrus.dd.editor/src/org/eclipse/papyrus/dd/dg/presentation/DGSVGSourcePage.java
+++ extraplugins/diagram-definition/org.eclipse.papyrus.dd.editor/src/org/eclipse/papyrus/dd/dg/presentation/DGSVGSourcePage.java
@@ -11,12 +11,8 @@
  */
 package org.eclipse.papyrus.dd.dg.presentation;
 
-import java.io.StringReader;
 import java.io.StringWriter;
 
-import org.apache.batik.dom.GenericDOMImplementation;
-import org.apache.batik.dom.util.DOMUtilities;
-import org.apache.batik.transcoder.svg2svg.PrettyPrinter;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.source.SourceViewer;
@@ -73,12 +69,12 @@ public class DGSVGSourcePage extends DDEditorPage {
 		String contents;
 		try {
 			StringWriter writer = new StringWriter();
-			DOMUtilities.writeDocument(svgDocument, writer);
+			// DOMUtilities.writeDocument(svgDocument, writer);
 			StringWriter formatted = new StringWriter();
-			PrettyPrinter printer = new PrettyPrinter();
-			printer.setTabulationWidth(4);
-			printer.setDocumentWidth(2000);
-			printer.print(new StringReader(writer.toString()), formatted);
+//			PrettyPrinter printer = new PrettyPrinter();
+//			pSrinter.setTabulationWidth(4);
+//			printer.setDocumentWidth(2000);
+//			printer.print(new StringReader(writer.toString()), formatted);
 			contents = formatted.toString().substring(1);
 			writer.close();
 		} catch (Exception e) {
@@ -101,7 +97,8 @@ public class DGSVGSourcePage extends DDEditorPage {
 
 			@Override
 			protected DOMImplementation getDOMImplementation() {
-				return GenericDOMImplementation.getDOMImplementation();
+				return null;
+				//return GenericDOMImplementation.getDOMImplementation();
 			}
 		};
 	}
