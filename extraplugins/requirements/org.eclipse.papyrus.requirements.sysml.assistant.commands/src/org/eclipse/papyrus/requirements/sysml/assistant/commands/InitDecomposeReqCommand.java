22b763be4cb214b1876c95196c807b500cb9e054
Mauricio Alferez <mauricio.alferez@cea.fr>
2016-06-28 13:25:27 +0200

    Bug 496654 - [Extra][Requirements] Id-Name creation strategies should be integrated
    Change-Id: I35759dc9cf72d23c15b34d1a700f10c1c57701c0
    Signed-off-by: Mauricio Alferez <mauricio.alferez@cea.fr>

--- extraplugins/requirements/org.eclipse.papyrus.requirements.sysml.assistant.commands/src/org/eclipse/papyrus/requirements/sysml/assistant/commands/InitDecomposeReqCommand.java
+++ extraplugins/requirements/org.eclipse.papyrus.requirements.sysml.assistant.commands/src/org/eclipse/papyrus/requirements/sysml/assistant/commands/InitDecomposeReqCommand.java
@@ -14,19 +14,14 @@
  *****************************************************************************/
 package org.eclipse.papyrus.requirements.sysml.assistant.commands;
 
-import java.text.DecimalFormat;
-import java.util.Iterator;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
-import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.papyrus.requirements.preferences.PreferenceConstants;
+import org.eclipse.papyrus.requirements.common.Utils;
 import org.eclipse.papyrus.requirements.sysml.common.I_SysMLStereotype;
 import org.eclipse.uml2.uml.Class;
 import org.eclipse.uml2.uml.Element;
 import org.eclipse.uml2.uml.Stereotype;
-import org.eclipse.uml2.uml.util.UMLUtil;
-
+import org.eclipse.uml2.uml.Package;
 /**
  * Creates a requirement and adds a decomposition link from the selected
  * requirement to the new requirement. The names are based on the Papyrus for Requirements
@@ -45,76 +40,19 @@ public class InitDecomposeReqCommand extends RecordingCommand {
 
 	@Override
 	protected void doExecute() {
-		String concatenedString = "";
-		org.eclipse.uml2.uml.Package owner = null;
+		Package owner = null;
 		if (selectedElement.getAppliedStereotype(I_SysMLStereotype.REQUIREMENT_STEREOTYPE) != null) {
-			Stereotype stereotype = selectedElement.getAppliedStereotype(I_SysMLStereotype.REQUIREMENT_STEREOTYPE);
-			concatenedString = concatenedString + selectedElement.getValue(stereotype, I_SysMLStereotype.REQUIREMENT_TEXT_ATT);
 			owner = selectedElement.getNearestPackage();
-			String id = getNewIDReq((Class) selectedElement, stereotype);
-			Class req = owner.createOwnedClass(id, false);
-			Stereotype reqStereotype = req.getApplicableStereotype(I_SysMLStereotype.REQUIREMENT_STEREOTYPE);
-			req.applyStereotype(reqStereotype);
-			req.setValue(reqStereotype, I_SysMLStereotype.REQUIREMENT_TEXT_ATT, concatenedString);
-			req.setValue(reqStereotype, I_SysMLStereotype.REQUIREMENT_ID_ATT, id);
-			DecomposeReqCommand decompositionReqCreateCommand = new DecomposeReqCommand(domain, req,
+			String id = Utils.getNewRequirementID((Class) selectedElement);
+			Class requirement = owner.createOwnedClass(id, false);
+			Stereotype reqStereotype = requirement.getApplicableStereotype(I_SysMLStereotype.REQUIREMENT_STEREOTYPE);
+			requirement.applyStereotype(reqStereotype);
+			requirement.setValue(reqStereotype, I_SysMLStereotype.REQUIREMENT_TEXT_ATT, "");
+			requirement.setValue(reqStereotype, I_SysMLStereotype.REQUIREMENT_ID_ATT, id);
+			DecomposeReqCommand decompositionReqCreateCommand = new DecomposeReqCommand(domain, requirement,
 					(Class) selectedElement);
 			decompositionReqCreateCommand.execute();
 		}
 	}
 
-	/**
-	 * get a new name of a Papyrus SysML child requirement based on the Papyrus
-	 * req preferences and the Id of the parent Requirement
-	 * 
-	 * @param parent
-	 *            the parent requirement
-	 * @return the name for a potential requirement
-	 */
-	public static String getNewIDReq(org.eclipse.uml2.uml.Class parent, Stereotype stereotype) {
-		IPreferenceStore store = org.eclipse.papyrus.requirements.preferences.Activator.getDefault().getPreferenceStore();
-
-		String prefix = store.getString(PreferenceConstants.REQUIREMENT_ID_PREFIX);// by
-																					// default
-																					// "R-"
-		String separator = store.getString(PreferenceConstants.CHILD_REQUIREMENTS_SEPARATOR); // by
-																								// default
-																								// "-"
-		boolean idUniqueInModel = store.getBoolean(PreferenceConstants.REQUIREMENT_ID_UNIQUE_IN_ENTIRE_MODEL); // by default true
-
-		String parentRequirementId = (String) parent.getValue(stereotype, I_SysMLStereotype.REQUIREMENT_ID_ATT);
-
-		String parentRequirementIdSuffix = parentRequirementId.replaceAll(prefix, "");
-		int digit = store.getInt(PreferenceConstants.REQUIREMENT_ID_DIGIT);// by
-																			// default 2
-		int i = 0;
-		DecimalFormat df = new DecimalFormat();
-		df.setMinimumIntegerDigits(digit);
-		String value = (df.format(i));
-		boolean IDexist = true;
-		while (IDexist) {
-			IDexist = false;
-			i++;
-			value = (df.format(i));
-			EList<Element> elements = null;
-			if (idUniqueInModel) {
-				elements = parent.getModel().allOwnedElements();
-			} else {
-				elements = parent.allOwnedElements();
-			}
-			for (Iterator<Element> iterator = elements.iterator(); iterator.hasNext() && (!IDexist);) {
-				Element element = (Element) iterator.next();
-				Object reqIdObject = UMLUtil.getTaggedValue(element, I_SysMLStereotype.REQUIREMENT_STEREOTYPE, I_SysMLStereotype.REQUIREMENT_ID_ATT);
-				if (reqIdObject != null) {
-					String existedID = (String) reqIdObject;
-					String newID = prefix + parentRequirementIdSuffix + separator + value;
-					// id already exists so increment suffix
-					if (newID.equals(existedID)) {
-						IDexist = true;
-					}
-				}
-			}
-		}
-		return prefix + parentRequirementIdSuffix + separator + value;
-	}
 }
