fb295a53dc165bcf364766df02b00acb2937e88c
Sebastien Gabel <sebastien.gabel@esterel-technologies.com>
2016-07-07 09:58:06 +0200

    Bug 497455: [SysML] Matchers improvements for Property sub-classes
    Change-Id: Ic2f165489d00629be061c483b9ed9fc0fcb46422
    Signed-off-by: Sebastien Gabel <sebastien.gabel@esterel-technologies.com>
    (cherry picked from commit 0d658d357627ad0c0e7b3267f76c8d20a38c7caa)

--- plugins/sysml/org.eclipse.papyrus.sysml.service.types/src/org/eclipse/papyrus/sysml/service/types/matcher/ConstraintPropertyMatcher.java
+++ plugins/sysml/org.eclipse.papyrus.sysml.service.types/src/org/eclipse/papyrus/sysml/service/types/matcher/ConstraintPropertyMatcher.java
@@ -1,10 +1,14 @@
 /*****************************************************************************
- * Copyright (c) 2010 CEA LIST.
+ * Copyright (c) 2010, 2016 CEA LIST, Esterel technologies SAS and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   CEA LIST - Initial API and implementation
+ *   Alain Le Guennec (Esterel technologies SAS) - Bug 497455
  *
  *****************************************************************************/
 package org.eclipse.papyrus.sysml.service.types.matcher;
@@ -12,7 +16,9 @@ package org.eclipse.papyrus.sysml.service.types.matcher;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.emf.type.core.IElementMatcher;
 import org.eclipse.papyrus.sysml.constraints.ConstraintProperty;
+import org.eclipse.uml2.uml.Class;
 import org.eclipse.uml2.uml.Property;
+import org.eclipse.uml2.uml.Type;
 import org.eclipse.uml2.uml.util.UMLUtil;
 
 /**
@@ -26,7 +32,9 @@ public class ConstraintPropertyMatcher implements IElementMatcher {
 		if (eObject instanceof Property) {
 
 			Property element = (Property) eObject;
-			if (UMLUtil.getStereotypeApplication(element, ConstraintProperty.class) != null) {
+			Type type = element.getType();
+			// A ConstraintProperty is necessarily typed by a ConstraintBlock which must be a Class.
+			if (type instanceof Class && UMLUtil.getStereotypeApplication(element, ConstraintProperty.class) != null) {
 				isMatch = true;
 			}
 		}
