879cacbe9c81c2c20ca34312e33b4ade4f9dae8b
Francois Le Fevre - CEA <francois.le-fevre@cea.fr>
2016-11-04 13:44:21 +0100

    Bug 507034 - [Toolsmiths] add the missing EPL header
    Change-Id: Ifaeb8bff08722ec8ad038c6b66ef7585ea5efc7b
    Signed-off-by: Francois Le Fevre - CEA <francois.le-fevre@cea.fr>

--- plugins/customization/org.eclipse.papyrus.customization.paletteconfiguration/src/org/eclipse/papyrus/customization/paletteconfiguration/queries/GetIconQuery.java
+++ plugins/customization/org.eclipse.papyrus.customization.paletteconfiguration/src/org/eclipse/papyrus/customization/paletteconfiguration/queries/GetIconQuery.java
@@ -1,3 +1,16 @@
+/*****************************************************************************
+ * Copyright (c) 2010 CEA LIST.
+ *
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Remi Schnekenburger (CEA LIST) remi.schnekenburger@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
 package org.eclipse.papyrus.customization.paletteconfiguration.queries;
 
 import org.eclipse.papyrus.emf.facet.efacet.core.IFacetManager;
