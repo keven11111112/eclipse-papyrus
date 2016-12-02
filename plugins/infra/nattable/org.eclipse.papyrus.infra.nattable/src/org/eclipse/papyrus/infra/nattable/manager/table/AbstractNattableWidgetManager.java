8c0dfdb1d3bd4dafbd9ae522a133107d96a347a1
Nicolas FAUVERGUE <nicolas.fauvergue@all4tec.net>
2016-12-02 13:57:49 +0100

    Bug 504077: [Table] Papyrus table to support percentage sizing offered by NatTable https://bugs.eclipse.org/bugs/show_bug.cgi?id=504077
    Improve percentage calculation to fill to exactly 100%.
    
    Change-Id: I6ae7b15bb9d82e897dac0e1213dee0412f242cbb
    Signed-off-by: Nicolas FAUVERGUE <nicolas.fauvergue@all4tec.net>

--- plugins/infra/nattable/org.eclipse.papyrus.infra.nattable/src/org/eclipse/papyrus/infra/nattable/manager/table/AbstractNattableWidgetManager.java
+++ plugins/infra/nattable/org.eclipse.papyrus.infra.nattable/src/org/eclipse/papyrus/infra/nattable/manager/table/AbstractNattableWidgetManager.java
@@ -142,6 +142,7 @@ import org.eclipse.papyrus.infra.nattable.utils.LocationValue;
 import org.eclipse.papyrus.infra.nattable.utils.NamedStyleConstants;
 import org.eclipse.papyrus.infra.nattable.utils.NattableConfigAttributes;
 import org.eclipse.papyrus.infra.nattable.utils.PapyrusTableSizeCalculation;
+import org.eclipse.papyrus.infra.nattable.utils.PercentageCalculationUtils;
 import org.eclipse.papyrus.infra.nattable.utils.TableEditingDomainUtils;
 import org.eclipse.papyrus.infra.nattable.utils.TableGridRegion;
 import org.eclipse.papyrus.infra.nattable.utils.TableSelectionWrapper;
@@ -1617,8 +1618,10 @@ public abstract class AbstractNattableWidgetManager implements INattableModelMan
 		}
 
 		// For the axis without 'axisWidth' named style and with the columns width percentage management, set the correct percentage
+		int numberIndex = 0;
 		for (int index : notManagedIndexAxisWidth) {
-			tableBodyLayer.setColumnWidthPercentageByPosition(index, Math.round(remainingPercentage / notManagedIndexAxisWidth.size()));
+			tableBodyLayer.setColumnWidthPercentageByPosition(index, PercentageCalculationUtils.calculatePercentageToSet(numberIndex, remainingPercentage, notManagedIndexAxisWidth.size()));
+			numberIndex++;
 		}
 
 		// this method is used to resize by default. In the actual state, only the rows, representing the table's core elements, are missing from the notation file
