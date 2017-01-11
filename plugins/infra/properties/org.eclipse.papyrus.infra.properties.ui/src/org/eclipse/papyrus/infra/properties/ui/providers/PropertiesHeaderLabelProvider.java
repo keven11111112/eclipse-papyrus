47b50b662fa9f3bfe14db3ac4f8743125e42df06
Christian W. Damus <give.a.damus@gmail.com>
2017-01-11 12:20:44 -0500

    Bug 510254: [Infra] Label provider changes should be propagated in Display thread
    Ensure that the various bits of the Properties View are notified of label
    updates on the UI thread because (a) observables can only interact on the UI thread
    and (b) the Eclipse Properties View framework doesn't account for title label
    provider updates triggered on any other thread.
    
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=510254
    (🍒-picked from branch streams/2.0-maintenance)
    
    Change-Id: Ibb47b03b48956e6063abb1929d82ecc098c0b1d8

--- plugins/infra/properties/org.eclipse.papyrus.infra.properties.ui/src/org/eclipse/papyrus/infra/properties/ui/providers/PropertiesHeaderLabelProvider.java
+++ plugins/infra/properties/org.eclipse.papyrus.infra.properties.ui/src/org/eclipse/papyrus/infra/properties/ui/providers/PropertiesHeaderLabelProvider.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2013, 2016 CEA LIST, Christian W. Damus, and others.
+ * Copyright (c) 2013, 2017 CEA LIST, Christian W. Damus, and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,7 +8,7 @@
  *
  * Contributors:
  *  Camille Letavernier (CEA LIST) camille.letavernier@cea.fr - Initial API and implementation
- *  Christian W. Damus - bug 508629
+ *  Christian W. Damus - bugs 508629, 510254
  *****************************************************************************/
 package org.eclipse.papyrus.infra.properties.ui.providers;
 
@@ -18,7 +18,9 @@ import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.LabelProviderChangedEvent;
 import org.eclipse.papyrus.infra.services.labelprovider.service.LabelProviderService;
 import org.eclipse.papyrus.infra.services.labelprovider.service.impl.LabelProviderServiceImpl;
+import org.eclipse.papyrus.infra.tools.util.CoreExecutors;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
 
 /**
  * An extensible LabelProvider for the Properties View header. It is based
@@ -57,7 +59,18 @@ public class PropertiesHeaderLabelProvider extends LabelProvider {
 	}
 
 	private void forwardLabelChange(LabelProviderChangedEvent event) {
-		fireLabelProviderChanged(new LabelProviderChangedEvent(this, event.getElements()));
+		LabelProviderChangedEvent forward = new LabelProviderChangedEvent(this, event.getElements());
+
+		// The TabbedPropertySheetPage does not account for label-provider events happening
+		// off the UI thread, so we have to synchronize with the UI to protect it
+
+		if (Display.getCurrent() != null) {
+			// Already on the UI thread
+			fireLabelProviderChanged(forward);
+		} else {
+			// Asynchronously on the UI thread, then
+			CoreExecutors.getUIExecutorService().execute(() -> fireLabelProviderChanged(forward));
+		}
 	}
 
 	@Override
