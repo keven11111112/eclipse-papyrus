976b7fd26224ab5593d26c75892c851b46363b00
Martin Fleck <mfleck@eclipsesource.com>
2017-01-11 15:18:39 +0100

    Bug 510268: [State Machine] Deleting a state does not delete transitions
    Add EditHelperAdvice to delete all incoming and outgoing transitions of
    a Vertex element if it gets deleted.
    
    Change-Id: I3e25be2d9812eb1c0184e7904c2bc06491eabb8a
    Signed-off-by: Martin Fleck <mfleck@eclipsesource.com>

---
+++ plugins/uml/org.eclipse.papyrus.uml.service.types/src/org/eclipse/papyrus/uml/service/types/helper/advice/VertexEditHelperAdvice.java
@@ -0,0 +1,47 @@
+/*****************************************************************************
+ * Copyright (c) 2017 EclipseSource Services GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Martin Fleck (EclipseSource) - Initial API and implementation
+ *****************************************************************************/
+package org.eclipse.papyrus.uml.service.types.helper.advice;
+
+import java.util.List;
+
+import org.eclipse.gmf.runtime.common.core.command.ICommand;
+import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
+import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyDependentsRequest;
+import org.eclipse.uml2.uml.Transition;
+import org.eclipse.uml2.uml.Vertex;
+
+import com.google.common.collect.Lists;
+
+/**
+ * Edit Helper Advice to delete incoming and outgoing transitions for deleted {@link Vertex} elements.
+ * 
+ * @author Martin Fleck <mfleck@eclipsesource.com>
+ */
+public class VertexEditHelperAdvice extends AbstractEditHelperAdvice {
+
+	/**
+	 * Default Constructor.
+	 */
+	public VertexEditHelperAdvice() {
+	}
+
+	@Override
+	protected ICommand getBeforeDestroyDependentsCommand(DestroyDependentsRequest request) {
+		if (!(request.getElementToDestroy() instanceof Vertex)) {
+			return super.getBeforeDestroyDependentsCommand(request);
+		}
+
+		Vertex vertexToDestroy = (Vertex) request.getElementToDestroy();
+		List<Transition> transitionsToDestroy = Lists.newArrayList(vertexToDestroy.getIncomings());
+		transitionsToDestroy.addAll(vertexToDestroy.getOutgoings());
+		return request.getDestroyDependentsCommand(transitionsToDestroy);
+	}
+}
--- tests/junit/plugins/uml/org.eclipse.papyrus.uml.service.types.tests/src/org/eclipse/papyrus/uml/service/types/tests/suites/AllTests.java
+++ tests/junit/plugins/uml/org.eclipse.papyrus.uml.service.types.tests/src/org/eclipse/papyrus/uml/service/types/tests/suites/AllTests.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2012, 2016 CEA LIST, Christian W. Damus, and others.
+ * Copyright (c) 2012, 2017 CEA LIST, Christian W. Damus, and others.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -10,6 +10,7 @@
  *   CEA LIST - Initial API and implementation
  *   Christian W. Damus - bugs 458685, 468071, 465899, 478314, 485220
  *   Vincent Lorenzo - bug 492522
+ *   Martin Fleck - bug 510268
  *****************************************************************************/
 
 package org.eclipse.papyrus.uml.service.types.tests.suites;
@@ -22,6 +23,7 @@ import org.eclipse.papyrus.uml.service.types.tests.deletion.DeleteCommentLinkTes
 import org.eclipse.papyrus.uml.service.types.tests.deletion.DeleteContainmentSubsetTest;
 import org.eclipse.papyrus.uml.service.types.tests.deletion.DeleteDependentWithStereotypeApplications458685;
 import org.eclipse.papyrus.uml.service.types.tests.deletion.DeletePureUMLElementTest;
+import org.eclipse.papyrus.uml.service.types.tests.deletion.DeleteTransitionsWithVertexTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
@@ -35,6 +37,7 @@ import org.junit.runners.Suite.SuiteClasses;
 		CreatePureUMLElementTest.class, // pure uml tests, only element edit service
 		DeletePureUMLElementTest.class,
 		DeleteDependentWithStereotypeApplications458685.class,
+		DeleteTransitionsWithVertexTest.class,
 		DeleteContainmentSubsetTest.class,
 		ConnectorReadOnlyTestBug465899.class,
 		DeleteCommentLinkTest.class
