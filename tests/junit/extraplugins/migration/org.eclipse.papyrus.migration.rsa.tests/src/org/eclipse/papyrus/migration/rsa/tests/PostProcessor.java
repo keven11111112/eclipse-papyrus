589571e9026792af615107993a2b8d025c58e756
Christian W. Damus <give.a.damus@gmail.com>
2017-01-20 16:25:17 -0500

    Bug 505330: Unable to post-process imported/migrated RSA-RTE model with proprietary profiles
    Add a test case for the QVTo transformation environment provided to
    post-processing extensions.
    
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=505330
    
    Change-Id: I50c7b05c07d097bae22c76aac8f6647648886a95

--- tests/junit/extraplugins/migration/org.eclipse.papyrus.migration.rsa.tests/src/org/eclipse/papyrus/migration/rsa/tests/PostProcessor.java
+++ tests/junit/extraplugins/migration/org.eclipse.papyrus.migration.rsa.tests/src/org/eclipse/papyrus/migration/rsa/tests/PostProcessor.java
@@ -19,9 +19,13 @@ import java.util.Set;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.m2m.qvt.oml.ExecutionContext;
+import org.eclipse.m2m.qvt.oml.ExecutionDiagnostic;
+import org.eclipse.m2m.qvt.oml.TransformationExecutor;
 import org.eclipse.papyrus.migration.rsa.concurrent.ExecutorsPool;
 import org.eclipse.papyrus.migration.rsa.internal.extension.PostProcessExtension;
 import org.eclipse.papyrus.migration.rsa.transformation.ImportTransformation;
@@ -35,7 +39,7 @@ import org.eclipse.uml2.uml.Package;
 public class PostProcessor implements PostProcessExtension {
 	public static boolean isEnabled = false;
 
-	private ImportTransformation transformation;
+	ImportTransformation transformation;
 
 	/**
 	 * Initializes me.
@@ -111,5 +115,69 @@ public class PostProcessor implements PostProcessExtension {
 	@Override
 	public void setExecutorsPool(ExecutorsPool executorsPool) {
 		// Pass
+	}
+
+	//
+	// Nested types
+	//
+
+	public static class WithQVTo extends PostProcessor {
+		public static boolean isEnabled = false;
+
+		private ExecutorsPool executorsPool;
+
+		/**
+		 * Initializes me.
+		 */
+		public WithQVTo() {
+			super();
+		}
+
+		@Override
+		public IStatus postProcess(ExecutionContext context, IProgressMonitor monitor) {
+			ImportTransformation transformation = this.transformation;
+			this.transformation = null;
+			ExecutorsPool pool = this.executorsPool;
+			this.executorsPool = null;
+
+			if (isEnabled) {
+				TransformationExecutor exec = pool.getExecutor(URI.createURI("platform:/plugin/org.eclipse.papyrus.migration.rsa.tests/resources/bug505330/postprocess.qvto"));
+				exec.loadTransformation(monitor);
+
+				ExecutionDiagnostic result = exec.execute(context, transformation.getInOutUMLModel(), transformation.getInoutNotationModel());
+
+				return BasicDiagnostic.toIStatus(result);
+			}
+
+			return Status.OK_STATUS;
+		}
+
+		@Override
+		public IStatus executeBefore(ExecutionContext context, IProgressMonitor monitor) {
+			return Status.OK_STATUS;
+		}
+
+		@Override
+		public IStatus executeAfter(ExecutionContext context, IProgressMonitor monitor) {
+			return Status.OK_STATUS;
+		}
+
+		@Override
+		public void setResourceSet(ResourceSet resourceSet) {
+			// Pass
+		}
+
+		@Override
+		public void setTransformation(ImportTransformation importTransformation) {
+			if (isEnabled) {
+				this.transformation = importTransformation;
+			}
+		}
+
+		@Override
+		public void setExecutorsPool(ExecutorsPool executorsPool) {
+			this.executorsPool = executorsPool;
+		}
+
 	}
 }
