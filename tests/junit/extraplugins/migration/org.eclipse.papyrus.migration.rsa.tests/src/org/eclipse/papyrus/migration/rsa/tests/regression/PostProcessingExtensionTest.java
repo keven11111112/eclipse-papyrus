568bc5018789a3de1a8228a60891c9db58a9f97d
Christian W. Damus <give.a.damus@gmail.com>
2017-01-20 13:07:12 -0500

    Bug 505330: Unable to post-process imported/migrated RSA-RTE model with proprietary profiles
    JUnit regression test for post-processing phase.
    
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=505330
    
    Change-Id: I314b18e949d6641c09bd8cf6563346528f74a425

--- tests/junit/extraplugins/migration/org.eclipse.papyrus.migration.rsa.tests/src/org/eclipse/papyrus/migration/rsa/tests/regression/PostProcessingExtensionTest.java
+++ tests/junit/extraplugins/migration/org.eclipse.papyrus.migration.rsa.tests/src/org/eclipse/papyrus/migration/rsa/tests/regression/PostProcessingExtensionTest.java
@@ -19,6 +19,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.eclipse.papyrus.junit.utils.rules.PluginResource;
 import org.eclipse.papyrus.migration.rsa.tests.PostProcessor;
+import org.eclipse.papyrus.migration.rsa.tests.regression.AbstractMigrationRegressionTest.BatchMigration;
 import org.eclipse.papyrus.migration.rsa.tests.regression.AbstractMigrationRegressionTest.TransformationExtensionClass;
 import org.eclipse.uml2.uml.Package;
 import org.eclipse.uml2.uml.Type;
@@ -28,6 +29,7 @@ import org.junit.Test;
  * Specific regression tests for bugs in the post-processing phase of import.
  */
 @TransformationExtensionClass(PostProcessor.class)
+@BatchMigration
 public class PostProcessingExtensionTest extends AbstractMigrationRegressionTest {
 
 	/**
@@ -41,8 +43,9 @@ public class PostProcessingExtensionTest extends AbstractMigrationRegressionTest
 	 * @see <a href="http://eclip.se/505330">bug 505330</a>
 	 */
 	@Test
-	@PluginResource("fragments/Blank Package.emx")
-	public void postProcessExtensionsInvoked() throws Exception {
+	@PluginResource({ "fragments/Blank Package.emx", "fragments/Class3.efx",
+			"fragments/Interface - Fragment.efx", "fragments/Package - Fragment.efx" })
+	public void postProcessExtensionsInvoked_bug505330() throws Exception {
 		assertThat(rootPackage.getName(), is("123 Blank Package"));
 
 		Package fragment = rootPackage.getNestedPackage("123 Package - Fragment");
