84a8d481df8a8bc742dd23e2d42280a2c16f76e8
Géry Deloge <gery.deloge@cea.fr>
2017-03-10 17:44:36 +0100

    Bug 513476: [BPMN] create diagram command
    Change-Id: I997ab8ee73b1836fe1c02fc9202e7eaad2209f7c
    Signed-off-by: Géry Deloge <gery.deloge@cea.fr>

---
+++ extraplugins/bpmn/org.eclipse.papyrus.bpmn.diagram/src/org/eclipse/papyrus/bpmn/diagram/BpmnViewpoint.java
@@ -0,0 +1,29 @@
+/*****************************************************************************
+ * Copyright (c) 2017 CEA LIST and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   CEA LIST - Initial API and implementation
+ *   
+ *****************************************************************************/
+
+package org.eclipse.papyrus.bpmn.diagram;
+
+
+public final class BpmnViewpoint {
+	
+	/*
+	 * These ID are the same than those defined in viewpoints/bpmn.configuration
+	 * 
+	 */
+	public static final String COLLABORATION_ID = "BPMN Collaboration Diagram";
+	public static final String PROCESS_ID = "BPMN Process Diagram";
+	
+	private BpmnViewpoint(){
+	}
+
+}
