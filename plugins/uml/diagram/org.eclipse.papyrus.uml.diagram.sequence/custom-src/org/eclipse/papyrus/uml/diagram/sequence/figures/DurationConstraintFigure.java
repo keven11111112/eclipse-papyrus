b7cc45f770631229ef38df19d86d6a9458a23a04
Céline Janssens <Celine.Janssens@all4tec.net>
2017-04-11 09:52:58 +0200

    Bug 489116: Migrate Duration Constraint to ElementTypes
    - Repaint the arrow figure 
    - Creation of the Duration interval in a EditHelperAdvice. 
    
    Change-Id: Ia56eaa82365de5abf193c8edb54409389cf13576
    Signed-off-by: Céline Janssens <Celine.Janssens@all4tec.net>

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.sequence/custom-src/org/eclipse/papyrus/uml/diagram/sequence/figures/DurationConstraintFigure.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.sequence/custom-src/org/eclipse/papyrus/uml/diagram/sequence/figures/DurationConstraintFigure.java
@@ -1,46 +1,118 @@
+/**
+ * Copyright (c) 2017 CEA LIST and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+  *  CEA LIST - Initial API and implementation
+ */
 package org.eclipse.papyrus.uml.diagram.sequence.figures;
 
 import org.eclipse.draw2d.Border;
+import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.PolylineShape;
 import org.eclipse.draw2d.PositionConstants;
 import org.eclipse.draw2d.RectangleFigure;
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Point;
-import org.eclipse.gmf.runtime.draw2d.ui.mapmode.IMapMode;
+import org.eclipse.draw2d.geometry.PointList;
+import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.papyrus.uml.diagram.common.draw2d.CenterLayout;
 import org.eclipse.papyrus.uml.diagram.common.draw2d.LinesBorder;
+import org.eclipse.swt.SWT;
 
 public class DurationConstraintFigure extends RectangleFigure {
 
-	private PolylineShape fDurationArrow;
 
-	private final IMapMode mapMode;
 
+	/**
+	 * The delta number of pixel to paint the Arrow.
+	 */
+	private static final int ARROW_SIZE = 10;
+
+	/**
+	 * 
+	 * Constructor.
+	 *
+	 */
 	public DurationConstraintFigure() {
-		mapMode = SequenceMapModeUtil.getMapModel(this);
 		CenterLayout layoutThis = new CenterLayout();
 		this.setLayoutManager(layoutThis);
-		this.setFill(false);
-		this.setOutline(false);
-		this.setPreferredSize(new Dimension(mapMode.DPtoLP(40), mapMode.DPtoLP(-1)));
-		this.setBorder(createBorder0());
-		createContents();
+
 	}
 
-	private void createContents() {
-		fDurationArrow = new PolylineShape();
-		fDurationArrow.addPoint(new Point(mapMode.DPtoLP(0), mapMode.DPtoLP(0)));
-		fDurationArrow.addPoint(new Point(mapMode.DPtoLP(10), mapMode.DPtoLP(0)));
-		this.add(fDurationArrow);
-	}
-
-	private Border createBorder0() {
+	/**
+	 * Create and display the top and bottom line of the figure.
+	 * 
+	 * @return the created Border
+	 */
+	private Border createBorder() {
 		LinesBorder result = new LinesBorder();
 		result.setSides(PositionConstants.TOP | PositionConstants.BOTTOM);
+		result.setStyle(SWT.BORDER_DASH);
 		return result;
 	}
 
-	public PolylineShape getDurationArrow() {
-		return fDurationArrow;
+
+	/**
+	 * @see org.eclipse.draw2d.Figure#paint(org.eclipse.draw2d.Graphics)
+	 *
+	 * @param graphics
+	 */
+	@Override
+	public void paint(Graphics graphics) {
+		super.paint(graphics);
+		Rectangle rect = this.getBounds();
+		graphics.pushState();
+		graphics.setForegroundColor(getForegroundColor());
+		Point top = new Point(rect.getTop());
+		Point bottom = new Point(rect.getBottom());
+		graphics.drawLine(top, bottom);
+		// draw arrows
+
+		// Top Arrow
+		Point left = new Point(top);
+		left = left.getTranslated(-ARROW_SIZE, ARROW_SIZE);
+		Point right = new Point(top);
+		right = right.getTranslated(ARROW_SIZE, ARROW_SIZE);
+		// Create list of point
+		PointList list = new PointList();
+		list.addPoint(right);
+		list.addPoint(top);
+		list.addPoint(left);
+		graphics.drawPolyline(list);
+
+		// Bottom Arrow
+		left = new Point(bottom);
+		left = left.getTranslated(-ARROW_SIZE, -ARROW_SIZE);
+		right = new Point(bottom);
+		right = right.getTranslated(ARROW_SIZE, -ARROW_SIZE);
+		// Create list of point
+		list = new PointList();
+		list.addPoint(right);
+		list.addPoint(bottom);
+		list.addPoint(left);
+		graphics.drawPolyline(list);
+
+		this.setFill(false);
+		this.setOutline(false);
+		this.setPreferredSize(new Dimension(40, 100));
+		this.setBorder(createBorder());
+		graphics.popState();
+
+	}
+
+	/**
+	 * Not used anymore (present for compilation purpose of CustomDurationConstraintFigure that is no more called )
+	 * 
+	 * @return null
+	 * @deprecated Use paint instead.
+	 * 
+	 */
+	protected PolylineShape getDurationArrow() {
+		return null;
 	}
 }
---
+++ plugins/uml/org.eclipse.papyrus.uml.service.types/src/org/eclipse/papyrus/uml/service/types/helper/advice/DurationConstraintEditHelperAdvice.java
@@ -0,0 +1,105 @@
+/*****************************************************************************
+ * Copyright (c) 2017 CEA LIST and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   CEA LIST - Initial API and implementation
+ *   
+ *****************************************************************************/
+
+package org.eclipse.papyrus.uml.service.types.helper.advice;
+
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.transaction.RecordingCommand;
+import org.eclipse.gmf.runtime.common.core.command.CompositeCommand;
+import org.eclipse.gmf.runtime.common.core.command.ICommand;
+import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
+import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
+import org.eclipse.gmf.runtime.emf.type.core.requests.IEditCommandRequest;
+import org.eclipse.papyrus.infra.emf.gmf.command.EMFtoGMFCommandWrapper;
+import org.eclipse.papyrus.uml.tools.utils.NamedElementUtil;
+import org.eclipse.uml2.uml.Duration;
+import org.eclipse.uml2.uml.DurationConstraint;
+import org.eclipse.uml2.uml.DurationInterval;
+import org.eclipse.uml2.uml.UMLFactory;
+
+/**
+ * @author Céline JANSSENS
+ *
+ */
+public class DurationConstraintEditHelperAdvice extends AbstractEditHelperAdvice {
+
+
+	/**
+	 * Add Duration value to the created Duration Interval
+	 * Set name with prefix
+	 * 
+	 * @param durationInterval
+	 */
+	private void initDurationInterval(final DurationInterval durationInterval) {
+		// create, add and set the min and max duration of the duration interval
+		org.eclipse.uml2.uml.Package parent = durationInterval.getNearestPackage();
+		Duration minDuration = UMLFactory.eINSTANCE.createDuration();
+		Duration maxDuration = UMLFactory.eINSTANCE.createDuration();
+
+		parent.getPackagedElements().add(minDuration);
+		parent.getPackagedElements().add(maxDuration);
+
+		String nameMin = NamedElementUtil.getDefaultNameWithIncrement("Min", minDuration, minDuration.getNearestPackage().getPackagedElements());// $NON-NLS-0$
+		String nameMax = NamedElementUtil.getDefaultNameWithIncrement("Max", maxDuration, maxDuration.getNearestPackage().getPackagedElements());// $NON-NLS-0$
+		minDuration.setName(nameMin);
+		maxDuration.setName(nameMax);
+
+		durationInterval.setMin(minDuration);
+		durationInterval.setMax(maxDuration);
+		minDuration.setExpr(UMLFactory.eINSTANCE.createLiteralInteger());
+		maxDuration.setExpr(UMLFactory.eINSTANCE.createLiteralInteger());
+	}
+
+
+	/**
+	 * @see org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice#getAfterEditCommand(org.eclipse.gmf.runtime.emf.type.core.requests.IEditCommandRequest)
+	 *
+	 * @param request
+	 * @return
+	 */
+	@Override
+	public ICommand getAfterEditCommand(final IEditCommandRequest request) {
+
+		ICommand composite = new CompositeCommand("After Edit Command of DurationConstraint");// $NON-NLS-0$
+
+		if (null != super.getAfterEditCommand(request) && super.getAfterEditCommand(request).canExecute()) {
+			composite.compose(super.getAfterEditCommand(request));
+		}
+
+		// Create the command to initialize Duration Interval in case of Duration Constraint
+		Command command = new RecordingCommand(request.getEditingDomain()) {
+
+			@Override
+			protected void doExecute() {
+				if (request instanceof ConfigureRequest) {
+					ConfigureRequest configRequest = (ConfigureRequest) request;
+					EObject element = configRequest.getElementToConfigure();
+					if (element instanceof DurationConstraint) {
+						if (((DurationConstraint) element).getSpecification() instanceof DurationInterval) {
+							// initialize Duration Interval
+							initDurationInterval((DurationInterval) ((DurationConstraint) element).getSpecification());
+						}
+					}
+				}
+			}
+		};
+
+
+		if (command.canExecute()) {
+			composite.compose(EMFtoGMFCommandWrapper.wrap(command));
+		}
+		return composite;
+	}
+
+}
