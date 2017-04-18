0ee84d66e32747c196f744702efec15830fc4990
jeremie.tatibouet <jeremie.tatibouet@cea.fr>
2017-04-18 10:24:07 +0200

    PSSM-1 Refactor DeferredEventOccurrence and CompletionEventOccurrence in order to implement new trigger matching API.
    Change-Id: Icd60a8b0197225700fb7de9ac8ee9aecffad7ce8
    Signed-off-by: jeremie.tatibouet <jeremie.tatibouet@cea.fr>

--- extraplugins/moka/org.eclipse.papyrus.moka.fuml.statemachines/src/org/eclipse/papyrus/moka/fuml/statemachines/Semantics/StateMachines/CompletionEventOccurrence.java
+++ extraplugins/moka/org.eclipse.papyrus.moka.fuml.statemachines/src/org/eclipse/papyrus/moka/fuml/statemachines/Semantics/StateMachines/CompletionEventOccurrence.java
@@ -13,7 +13,12 @@
 
 package org.eclipse.papyrus.moka.fuml.statemachines.Semantics.StateMachines;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.papyrus.moka.fuml.Semantics.CommonBehaviors.BasicBehaviors.ParameterValue;
 import org.eclipse.papyrus.moka.fuml.Semantics.CommonBehaviors.Communications.EventOccurrence;
+import org.eclipse.uml2.uml.Trigger;
 
 public class CompletionEventOccurrence extends EventOccurrence {
 	
@@ -32,5 +37,33 @@ public class CompletionEventOccurrence extends EventOccurrence {
 	
 	// A reference to the state from which the completion event was generated
 	public StateActivation stateActivation;
+
+	@Override
+	public boolean matchAny(List<Trigger> triggers) {
+		// A completion event can only trigger transition with no
+		// trigger. Hence if the list of trigger that is passed is
+		// empty then the completion event occurrence matches. It does match
+		// otherwise.
+		boolean match = false;
+		if(triggers.size() == 0){
+			match = true;
+		}
+		return match;
+	}
+	
+	@Override
+	public boolean match(Trigger trigger) {
+		// A completion event can only trigger transition with no
+		// trigger. Hence it cannot match a trigger so false is returned
+		// regardless the trigger provided as parameter.
+		return false;
+	}
+
+	@Override
+	public List<ParameterValue> getParameterValues() {
+		// A completion will never have associated values. Hence a
+		// empty list is returned.
+		return new ArrayList<ParameterValue>();
+	}
 
 }
--- extraplugins/moka/org.eclipse.papyrus.moka.fuml.statemachines/src/org/eclipse/papyrus/moka/fuml/statemachines/Semantics/StateMachines/DeferredEventOccurrence.java
+++ extraplugins/moka/org.eclipse.papyrus.moka.fuml.statemachines/src/org/eclipse/papyrus/moka/fuml/statemachines/Semantics/StateMachines/DeferredEventOccurrence.java
@@ -1,6 +1,11 @@
 package org.eclipse.papyrus.moka.fuml.statemachines.Semantics.StateMachines;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.papyrus.moka.fuml.Semantics.CommonBehaviors.BasicBehaviors.ParameterValue;
 import org.eclipse.papyrus.moka.fuml.Semantics.CommonBehaviors.Communications.EventOccurrence;
+import org.eclipse.uml2.uml.Trigger;
 
 public class DeferredEventOccurrence extends EventOccurrence {
 	
@@ -10,5 +15,27 @@ public class DeferredEventOccurrence extends EventOccurrence {
 	
 	// The event occurrence that is actually deferred
 	public EventOccurrence deferredEventOccurrence;
+
+	@Override
+	public boolean match(Trigger trigger) {
+		// Delegate to the match operation of the encapsulated event
+		// occurrence which is the one being deferred.
+		boolean match = false;
+		if(this.deferredEventOccurrence != null){
+			match = this.deferredEventOccurrence.match(trigger);
+		}
+		return match;
+	}
+
+	@Override
+	public List<ParameterValue> getParameterValues() {
+		// Delegate to the getParameterValues operation of the encapsulated event
+		// occurrence which is the one being deferred.
+		List<ParameterValue> parameterValues = new ArrayList<ParameterValue>();
+		if(this.deferredEventOccurrence != null){
+			parameterValues = this.deferredEventOccurrence.getParameterValues();
+		}
+		return parameterValues;
+	}
 
 }
