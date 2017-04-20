dd82bec9a46a6a8a48ac59e365fc15160a130905
Nicolas FAUVERGUE <nicolas.fauvergue@cea.fr>
2017-04-20 18:29:55 +0200

    Bug 515535: [Interoperability] Move common plugins into papyrus main git https://bugs.eclipse.org/bugs/show_bug.cgi?id=515535
    Add the interoperability common plugins into papyrus main git.
    Those ones are renamed as:
    - oep.uml.m2m.qvto.common
    - oep.uml.m2m.qvto.common.blackboxes
    - oep.uml.m2m.qvto.tests.tools
    
    Change-Id: Id17c4fd0e57a5c063c1b4a4e79d833c744f3f61e
    Signed-off-by: Nicolas FAUVERGUE <nicolas.fauvergue@cea.fr>

---
+++ plugins/uml/m2m/org.eclipse.papyrus.uml.m2m.qvto.common/src/org/eclipse/papyrus/uml/m2m/qvto/common/transformation/IImportTransformationLauncher.java
@@ -0,0 +1,39 @@
+/*****************************************************************************
+ * Copyright (c) 2014, 2016 CEA LIST, Christian W. Damus, and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Vincent Lorenzo (CEA LIST) vincent.lorenzo@cea.fr - Initial API and implementation
+ *****************************************************************************/
+
+package org.eclipse.papyrus.uml.m2m.qvto.common.transformation;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.common.util.URI;
+
+/**
+ * @author Vincent Lorenzo
+ *
+ */
+public interface IImportTransformationLauncher {
+
+	/**
+	 * Executes the transformation (Asynchronous)
+	 *
+	 * @param urisToImport
+	 */
+	public void run(List<URI> urisToImport);
+	
+	/** Mainly for test purpose */
+	public void waitForCompletion() throws Exception;
+	
+	/** Mainly for test purpose */
+	public IStatus getResult();
+
+}
