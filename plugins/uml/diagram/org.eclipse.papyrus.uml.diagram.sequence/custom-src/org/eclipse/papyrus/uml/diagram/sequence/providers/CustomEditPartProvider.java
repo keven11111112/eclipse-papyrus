e180f7d703f0eb77f857c023e81da0cac21411b8
Céline Janssens <Celine.Janssens@all4tec.net>
2017-04-20 11:44:02 +0200

    Bug 489116: [Sequence Diagram] Migrate Duration Observation for elementtypes
    - Activate Custom Duration Observation EditPart
    
    Know ISSUE : Parser passed is the composite parser instead of Sequence
    parser for DurationObservation
    
    Solution 1 : Have the same Parser for all the DurationObservation
    Issue 1 : Duration Observation Parser herit from MessageFormatParser
    that is generated for each diagram. And cannot be common
    
    Solution 2 : Modify the Element Type ID in order to identify if an
    element type belongs to a specific diagram to avoid to match a parser
    from another diagram.
    Issue2: No idea of the side effects 
    
    Solution 3 : Create a specific Parser Provider for DurationObservation
    of the sequence diagram and put it with a higher priority
    Issue 3:  I'm pretty sure, then this is the composite
    DurationObservation that will match the Sequence DurationObservation
    Parser. 
    
    
    
    Change-Id: I3da0abad30617c3e8243ce0c070d84b1fab073d9
    Signed-off-by: Céline Janssens <Celine.Janssens@all4tec.net>

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.sequence/custom-src/org/eclipse/papyrus/uml/diagram/sequence/providers/CustomEditPartProvider.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.sequence/custom-src/org/eclipse/papyrus/uml/diagram/sequence/providers/CustomEditPartProvider.java
@@ -14,10 +14,31 @@
 package org.eclipse.papyrus.uml.diagram.sequence.providers;
 
 import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
-import org.eclipse.gmf.runtime.diagram.ui.internal.editparts.DummyEditPart;
 import org.eclipse.gmf.runtime.notation.Connector;
 import org.eclipse.gmf.runtime.notation.View;
-import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.*;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.BehaviorExecutionSpecificationBehaviorEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.CCombinedFragmentCombinedFragmentCompartmentEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.CCombinedFragmentEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.CInteractionEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.CInteractionOperandEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.CLifeLineEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.CombinedFragmentCombinedFragmentCompartmentEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.CombinedFragmentEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.CustomDurationObservationAppliedStereotypeEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.CustomDurationObservationEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.CustomStateInvariantEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.CustomStateInvariantLabelEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.DurationObservationAppliedStereotypeEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.DurationObservationEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.GateEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.GateNameEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.InteractionEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.InteractionOperandEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.InteractionOperandGuardEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.LifelineEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.ObservationLinkEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.StateInvariantEditPart;
+import org.eclipse.papyrus.uml.diagram.sequence.edit.parts.StateInvariantLabelEditPart;
 import org.eclipse.papyrus.uml.diagram.sequence.part.UMLVisualIDRegistry;
 import org.eclipse.papyrus.uml.diagram.sequence.referencialgrilling.GrillingEditpart;
 import org.eclipse.papyrus.uml.diagram.sequence.util.SequenceUtil;
@@ -48,16 +69,16 @@ public class CustomEditPartProvider extends UMLEditPartProvider {
 			}
 		}
 		switch (UMLVisualIDRegistry.getVisualID(view)) {
-//		case SequenceDiagramEditPart.VISUAL_ID:
-//			return new OLDPackageEditPart(view);
+		// case SequenceDiagramEditPart.VISUAL_ID:
+		// return new OLDPackageEditPart(view);
 		case InteractionEditPart.VISUAL_ID:
 			return new CInteractionEditPart(view);
 		case GrillingEditpart.VISUAL_ID:
-			return new 	GrillingEditpart(view);
-//		case InteractionNameEditPart.VISUAL_ID:
-//			return new CustomInteractionNameEditPart(view);
-//		case ConsiderIgnoreFragmentEditPart.VISUAL_ID:
-//			return new CustomConsiderIgnoreFragmentEditPart(view);
+			return new GrillingEditpart(view);
+		// case InteractionNameEditPart.VISUAL_ID:
+		// return new CustomInteractionNameEditPart(view);
+		// case ConsiderIgnoreFragmentEditPart.VISUAL_ID:
+		// return new CustomConsiderIgnoreFragmentEditPart(view);
 		case CombinedFragmentEditPart.VISUAL_ID:
 			return new CCombinedFragmentEditPart(view);
 		case CombinedFragmentCombinedFragmentCompartmentEditPart.VISUAL_ID:
@@ -65,100 +86,100 @@ public class CustomEditPartProvider extends UMLEditPartProvider {
 
 		case InteractionOperandEditPart.VISUAL_ID:
 			return new CInteractionOperandEditPart(view);
-//		case InteractionUseEditPart.VISUAL_ID:
-//			return new CustomInteractionUseEditPart(view);
-//		case InteractionUseNameEditPart.VISUAL_ID:
-//			return new CustomInteractionUseNameEditPart(view);
-//		case InteractionUseName2EditPart.VISUAL_ID:
-//			return new CustomInteractionUseName2EditPart(view);
-//		case ContinuationEditPart.VISUAL_ID:
-//			return new CustomContinuationEditPart(view);
+		// case InteractionUseEditPart.VISUAL_ID:
+		// return new CustomInteractionUseEditPart(view);
+		// case InteractionUseNameEditPart.VISUAL_ID:
+		// return new CustomInteractionUseNameEditPart(view);
+		// case InteractionUseName2EditPart.VISUAL_ID:
+		// return new CustomInteractionUseName2EditPart(view);
+		// case ContinuationEditPart.VISUAL_ID:
+		// return new CustomContinuationEditPart(view);
 		case LifelineEditPart.VISUAL_ID:
 			return new CLifeLineEditPart(view);
-//		case LifelineNameEditPart.VISUAL_ID:
-//			return new CustomLifelineNameEditPart(view);
-//		case ActionExecutionSpecificationEditPart.VISUAL_ID:
-//			return new CustomActionExecutionSpecificationEditPart(view);
-//		case BehaviorExecutionSpecificationEditPart.VISUAL_ID:
-//			return new CustomBehaviorExecutionSpecificationEditPart(view);
+		// case LifelineNameEditPart.VISUAL_ID:
+		// return new CustomLifelineNameEditPart(view);
+		// case ActionExecutionSpecificationEditPart.VISUAL_ID:
+		// return new CustomActionExecutionSpecificationEditPart(view);
+		// case BehaviorExecutionSpecificationEditPart.VISUAL_ID:
+		// return new CustomBehaviorExecutionSpecificationEditPart(view);
 		case StateInvariantEditPart.VISUAL_ID:
 			return new CustomStateInvariantEditPart(view);
-//		case CombinedFragment2EditPart.VISUAL_ID:
-//			return new CustomCombinedFragment2EditPart(view);
-//		case TimeConstraintEditPart.VISUAL_ID:
-//			return new CustomTimeConstraintEditPart(view);
-//		case TimeConstraintAppliedStereotypeEditPart.VISUAL_ID:
-//			return new CustomTimeConstraintAppliedStereotypeEditPart(view);
-//		case TimeConstraintLabelEditPart.VISUAL_ID:
-//			return new CustomTimeConstraintLabelEditPart(view);
-//		case TimeObservationEditPart.VISUAL_ID:
-//			return new CustomTimeObservationEditPart(view);
-//		case TimeObservationLabelEditPart.VISUAL_ID:
-//			return new CustomTimeObservationLabelEditPart(view);
-//		case TimeObservationAppliedStereotypeEditPart.VISUAL_ID:
-//			return new CustomTimeObservationAppliedStereotypeEditPart(view);
-//		case DurationConstraintEditPart.VISUAL_ID:
-//			return new CustomDurationConstraintEditPart(view);
-//		case DurationConstraintAppliedStereotypeEditPart.VISUAL_ID:
-//			return new CustomDurationConstraintAppliedStereotypeEditPart(view);
-//		case DestructionOccurrenceSpecificationEditPart.VISUAL_ID:
-//			return new CustomDestructionOccurrenceSpecificationEditPart(view);
-//		case ConstraintEditPart.VISUAL_ID:
-//			return new CustomConstraintEditPart(view);
-//		case Constraint2EditPart.VISUAL_ID:
-//			return new CustomConstraint2EditPart(view);
-//		case CommentEditPart.VISUAL_ID:
-//			return new CustomCommentEditPart(view);
-//		case CommentBodyEditPart.VISUAL_ID:
-//			return new CustomCommentBodyEditPart(view);
-//		case DurationConstraintInMessageEditPart.VISUAL_ID:
-//			return new CustomDurationConstraintInMessageEditPart(view);
-//		case DurationConstraintInMessageAppliedStereotypeEditPart.VISUAL_ID:
-//			return new CustomDurationConstraintInMessageAppliedStereotypeEditPart(view);
-//		case DurationObservationEditPart.VISUAL_ID:
-//			return new CustomDurationObservationEditPart(view);
-//		case DurationObservationAppliedStereotypeEditPart.VISUAL_ID:
-//			return new CustomDurationObservationAppliedStereotypeEditPart(view);
-//		case InteractionInteractionCompartmentEditPart.VISUAL_ID:
-//			return new CustomInteractionInteractionCompartmentEditPart(view);
-//		case CombinedFragmentCombinedFragmentCompartmentEditPart.VISUAL_ID:
-//			return new CustomCombinedFragmentCombinedFragmentCompartmentEditPart(view);
-//		case MessageSyncEditPart.VISUAL_ID:
-//			return new CustomMessageSyncEditPart(view);
-//		case MessageSyncNameEditPart.VISUAL_ID:
-//			return new CustomMessageNameEditPart(view);
-//		case MessageAsyncEditPart.VISUAL_ID:
-//			return new CustomMessageAsyncEditPart(view);
-//		case MessageAsyncNameEditPart.VISUAL_ID:
-//			return new CustomMessageName2EditPart(view);
-//		case MessageReplyEditPart.VISUAL_ID:
-//			return new CustomMessageReplyEditPart(view);
-//		case MessageReplyNameEditPart.VISUAL_ID:
-//			return new CustomMessageName3EditPart(view);
-//		case MessageCreateEditPart.VISUAL_ID:
-//			return new CustomMessageCreateEditPart(view);
-//		case MessageCreateNameEditPart.VISUAL_ID:
-//			return new CustomMessageName4EditPart(view);
-//		case MessageDeleteEditPart.VISUAL_ID:
-//			return new CustomMessageDeleteEditPart(view);
-//		case MessageDeleteNameEditPart.VISUAL_ID:
-//			return new CustomMessageName5EditPart(view);
-//		case MessageLostEditPart.VISUAL_ID:
-//			return new CustomMessageLostEditPart(view);
-//		case MessageLostNameEditPart.VISUAL_ID:
-//			return new CustomMessageName6EditPart(view);
-//		case MessageFoundEditPart.VISUAL_ID:
-//			return new CustomMessageFoundEditPart(view);
-//		case MessageFoundNameEditPart.VISUAL_ID:
-//			return new CustomMessageName7EditPart(view);
-//		case CommentAnnotatedElementEditPart.VISUAL_ID:
-//			return new CustomCommentAnnotatedElementEditPart(view);
-//		case GeneralOrderingEditPart.VISUAL_ID:
-//			return new CustomGeneralOrderingEditPart(view);
-//		case ExecutionSpecificationEndEditPart.VISUAL_ID:
-//			return new ExecutionSpecificationEndEditPart(view);
-//		case MessageEndEditPart.VISUAL_ID:
-//			return new MessageEndEditPart(view);
+		// case CombinedFragment2EditPart.VISUAL_ID:
+		// return new CustomCombinedFragment2EditPart(view);
+		// case TimeConstraintEditPart.VISUAL_ID:
+		// return new CustomTimeConstraintEditPart(view);
+		// case TimeConstraintAppliedStereotypeEditPart.VISUAL_ID:
+		// return new CustomTimeConstraintAppliedStereotypeEditPart(view);
+		// case TimeConstraintLabelEditPart.VISUAL_ID:
+		// return new CustomTimeConstraintLabelEditPart(view);
+		// case TimeObservationEditPart.VISUAL_ID:
+		// return new CustomTimeObservationEditPart(view);
+		// case TimeObservationLabelEditPart.VISUAL_ID:
+		// return new CustomTimeObservationLabelEditPart(view);
+		// case TimeObservationAppliedStereotypeEditPart.VISUAL_ID:
+		// return new CustomTimeObservationAppliedStereotypeEditPart(view);
+		// case DurationConstraintEditPart.VISUAL_ID:
+		// return new CustomDurationConstraintEditPart(view);
+		// case DurationConstraintAppliedStereotypeEditPart.VISUAL_ID:
+		// return new CustomDurationConstraintAppliedStereotypeEditPart(view);
+		// case DestructionOccurrenceSpecificationEditPart.VISUAL_ID:
+		// return new CustomDestructionOccurrenceSpecificationEditPart(view);
+		// case ConstraintEditPart.VISUAL_ID:
+		// return new CustomConstraintEditPart(view);
+		// case Constraint2EditPart.VISUAL_ID:
+		// return new CustomConstraint2EditPart(view);
+		// case CommentEditPart.VISUAL_ID:
+		// return new CustomCommentEditPart(view);
+		// case CommentBodyEditPart.VISUAL_ID:
+		// return new CustomCommentBodyEditPart(view);
+		// case DurationConstraintInMessageEditPart.VISUAL_ID:
+		// return new CustomDurationConstraintInMessageEditPart(view);
+		// case DurationConstraintInMessageAppliedStereotypeEditPart.VISUAL_ID:
+		// return new CustomDurationConstraintInMessageAppliedStereotypeEditPart(view);
+		case DurationObservationEditPart.VISUAL_ID:
+			return new CustomDurationObservationEditPart(view);
+		case DurationObservationAppliedStereotypeEditPart.VISUAL_ID:
+			return new CustomDurationObservationAppliedStereotypeEditPart(view);
+		// case InteractionInteractionCompartmentEditPart.VISUAL_ID:
+		// return new CustomInteractionInteractionCompartmentEditPart(view);
+		// case CombinedFragmentCombinedFragmentCompartmentEditPart.VISUAL_ID:
+		// return new CustomCombinedFragmentCombinedFragmentCompartmentEditPart(view);
+		// case MessageSyncEditPart.VISUAL_ID:
+		// return new CustomMessageSyncEditPart(view);
+		// case MessageSyncNameEditPart.VISUAL_ID:
+		// return new CustomMessageNameEditPart(view);
+		// case MessageAsyncEditPart.VISUAL_ID:
+		// return new CustomMessageAsyncEditPart(view);
+		// case MessageAsyncNameEditPart.VISUAL_ID:
+		// return new CustomMessageName2EditPart(view);
+		// case MessageReplyEditPart.VISUAL_ID:
+		// return new CustomMessageReplyEditPart(view);
+		// case MessageReplyNameEditPart.VISUAL_ID:
+		// return new CustomMessageName3EditPart(view);
+		// case MessageCreateEditPart.VISUAL_ID:
+		// return new CustomMessageCreateEditPart(view);
+		// case MessageCreateNameEditPart.VISUAL_ID:
+		// return new CustomMessageName4EditPart(view);
+		// case MessageDeleteEditPart.VISUAL_ID:
+		// return new CustomMessageDeleteEditPart(view);
+		// case MessageDeleteNameEditPart.VISUAL_ID:
+		// return new CustomMessageName5EditPart(view);
+		// case MessageLostEditPart.VISUAL_ID:
+		// return new CustomMessageLostEditPart(view);
+		// case MessageLostNameEditPart.VISUAL_ID:
+		// return new CustomMessageName6EditPart(view);
+		// case MessageFoundEditPart.VISUAL_ID:
+		// return new CustomMessageFoundEditPart(view);
+		// case MessageFoundNameEditPart.VISUAL_ID:
+		// return new CustomMessageName7EditPart(view);
+		// case CommentAnnotatedElementEditPart.VISUAL_ID:
+		// return new CustomCommentAnnotatedElementEditPart(view);
+		// case GeneralOrderingEditPart.VISUAL_ID:
+		// return new CustomGeneralOrderingEditPart(view);
+		// case ExecutionSpecificationEndEditPart.VISUAL_ID:
+		// return new ExecutionSpecificationEndEditPart(view);
+		// case MessageEndEditPart.VISUAL_ID:
+		// return new MessageEndEditPart(view);
 		case StateInvariantLabelEditPart.VISUAL_ID:
 			return new CustomStateInvariantLabelEditPart(view);
 		}
