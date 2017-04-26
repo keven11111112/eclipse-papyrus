95e0b1eb02f84803f2a84a058eee342375660f1a
Nicolas FAUVERGUE <nicolas.fauvergue@cea.fr>
2017-04-26 13:39:07 +0200

    Bug 515535: [Interoperability] Move common plugins into papyrus main git https://bugs.eclipse.org/bugs/show_bug.cgi?id=515535
    Add the interoperability common plugins into papyrus main git.
    Those ones are renamed as:
    - oep.uml.m2m.qvto.common
    - oep.uml.m2m.qvto.common.blackboxes
    - oep.uml.m2m.qvto.tests.tools
    
    Change-Id: Icdd3090e0655fe8c2737ee6aa6d807a5a0e0772f
    Signed-off-by: Nicolas FAUVERGUE <nicolas.fauvergue@cea.fr>

---
+++ plugins/uml/m2m/org.eclipse.papyrus.uml.m2m.qvto.common/src-gen/org/eclipse/papyrus/uml/m2m/qvto/common/MigrationParameters/MigrationParametersFactory.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2014 CEA LIST.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Camille Letavernier (CEA LIST) camille.letavernier@cea.fr - Initial API and implementation
+ *   Vincent Lorenzo (CEA-LIST) vincent.lorenzo@cea.fr - bug 496176
+ * 
+ */
+package org.eclipse.papyrus.uml.m2m.qvto.common.MigrationParameters;
+
+import org.eclipse.emf.ecore.EFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Factory</b> for the model.
+ * It provides a create method for each non-abstract class of the model.
+ * <!-- end-user-doc -->
+ * @see org.eclipse.papyrus.uml.m2m.qvto.common.MigrationParameters.MigrationParametersPackage
+ * @generated
+ */
+public interface MigrationParametersFactory extends EFactory {
+	/**
+	 * The singleton instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	MigrationParametersFactory eINSTANCE = org.eclipse.papyrus.uml.m2m.qvto.common.MigrationParameters.impl.MigrationParametersFactoryImpl.init();
+
+	/**
+	 * Returns a new object of class '<em>Advanced Config</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Advanced Config</em>'.
+	 * @generated
+	 */
+	AdvancedConfig createAdvancedConfig();
+
+	/**
+	 * Returns a new object of class '<em>Thread Config</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Thread Config</em>'.
+	 * @generated
+	 */
+	ThreadConfig createThreadConfig();
+
+	/**
+	 * Returns a new object of class '<em>Mapping Parameters</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Mapping Parameters</em>'.
+	 * @generated
+	 */
+	MappingParameters createMappingParameters();
+
+	/**
+	 * Returns a new object of class '<em>URI Mapping</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>URI Mapping</em>'.
+	 * @generated
+	 */
+	URIMapping createURIMapping();
+
+	/**
+	 * Returns the package supported by this factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the package supported by this factory.
+	 * @generated
+	 */
+	MigrationParametersPackage getMigrationParametersPackage();
+
+} //MigrationParametersFactory
---
+++ plugins/uml/m2m/org.eclipse.papyrus.uml.m2m.qvto.common/src/org/eclipse/papyrus/uml/m2m/qvto/common/transformation/IImportTransformation.java
@@ -0,0 +1,68 @@
+/*****************************************************************************
+ * Copyright (c) 2014, 2016 CEA LIST, Christian W. Damus, and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Vincent Lorenzo (CEA LIST) vincent.lorenzo@cea.fr - Initial API and implementation
+ *****************************************************************************/
+
+package org.eclipse.papyrus.uml.m2m.qvto.common.transformation;
+
+import org.eclipse.core.runtime.IStatus;
+
+/**
+ * @author Vincent Lorenzo
+ *
+ */
+public interface IImportTransformation {
+
+	/**
+	 * @return
+	 */
+	long getLoadingTime();
+
+	/**
+	 * @return
+	 */
+	long getHandleDanglingRefTime();
+
+	/**
+	 * @return
+	 */
+	long getImportExtensionsTime();
+
+	/**
+	 * @return
+	 */
+	long getExecutionTime();
+
+	/**
+	 * @return
+	 */
+	String getModelName();
+
+	/**
+	 * @return
+	 */
+	IStatus getStatus();
+
+	/**
+	 * @return
+	 */
+	boolean isComplete();
+
+	/**
+	 * @param b
+	 */
+	void run(boolean b);
+
+	/**
+	 * 
+	 */
+	void cancel();
+
+}
