6df4255b7e8f143986cfb3261fbf308140694c17
Nicolas FAUVERGUE <nicolas.fauvergue@cea.fr>
2017-05-02 09:28:46 +0200

    [BundlesTests]
    Modify:
    - The PLUGIN_ID of plugin
    org.eclipse.papyrus.uml.m2m.qvto.common.blackboxes
    - The provider name of the plugin
    org.eclipse.papyrus.infra.emf.expressions
    
    Change-Id: Ia417ee4b037b4423fe9be884b448ded00dd3137f
    Signed-off-by: Nicolas FAUVERGUE <nicolas.fauvergue@cea.fr>

--- plugins/uml/m2m/org.eclipse.papyrus.uml.m2m.qvto.common.blackboxes/src/org/eclipse/papyrus/uml/m2m/qvto/common/blackboxes/Activator.java
+++ plugins/uml/m2m/org.eclipse.papyrus.uml.m2m.qvto.common.blackboxes/src/org/eclipse/papyrus/uml/m2m/qvto/common/blackboxes/Activator.java
@@ -26,7 +26,7 @@ public class Activator extends AbstractUIPlugin {
 	/**
 	 * The plug-in ID.
 	 */
-	public static final String PLUGIN_ID = "org.eclipse.papyrus.m2m.qvto.common.blackboxes"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.eclipse.papyrus.uml.m2m.qvto.common.blackboxes"; //$NON-NLS-1$
 
 	/**
 	 * The shared instance.
