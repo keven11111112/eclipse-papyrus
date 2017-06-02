6ff0d82e000de27b5e0825bff8d70e7206d13de8
Camille Letavernier <cletavernier@eclipsesource.com>
2017-06-02 14:21:04 +0200

    Bug 517688: Annotated tests should be restored https://bugs.eclipse.org/bugs/show_bug.cgi?id=517688
    - Remove the new factory "GMFUMLTypedElement...", and merge it into the
    existing "GMFUMLTypedElement..." one
    
    Change-Id: I16030822d68e280ec9a4b91ee66906b5e41e871a
    Signed-off-by: Camille Letavernier <cletavernier@eclipsesource.com>

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.css/src/org/eclipse/papyrus/uml/diagram/css/dom/GMFUMLTypedElementAdapter.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.css/src/org/eclipse/papyrus/uml/diagram/css/dom/GMFUMLTypedElementAdapter.java
@@ -11,16 +11,8 @@
  *****************************************************************************/
 package org.eclipse.papyrus.uml.diagram.css.dom;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.papyrus.infra.gmfdiag.css.engine.ExtendedCSSEngine;
-import org.eclipse.papyrus.infra.tools.util.ListHelper;
-import org.eclipse.papyrus.uml.diagram.css.dom.GMFUMLElementAdapter;
-import org.eclipse.uml2.uml.Stereotype;
-import org.eclipse.uml2.uml.Type;
-import org.eclipse.uml2.uml.TypedElement;
 
 /**
  * DOM Element Adapter for UML Elements
@@ -29,44 +21,15 @@ import org.eclipse.uml2.uml.TypedElement;
  *
  * @author Shuai Li
  * @since 2.0
- *
+ * @deprecated Use/Extend {@link GMFUMLElementAdapter} instead
  */
+@Deprecated
 public class GMFUMLTypedElementAdapter extends GMFUMLElementAdapter {
 
-	public static final String TYPE_APPLIED_STEREOTYPES_PROPERTY = "typeAppliedStereotypes"; //$NON-NLS-1$
-
+	//Deprecated, empty class
+	
 	public GMFUMLTypedElementAdapter(View view, ExtendedCSSEngine engine) {
 		super(view, engine);
 	}
-
-	/**
-	 * {@inheritDoc}
-	 *
-	 * Typed applied stereotypes are manipulated as DOM attributes
-	 */
-	@Override
-	protected String doGetAttribute(String attr) {
-		// Semantic properties
-		String parentValue = super.doGetAttribute(attr);
-		if (parentValue != null) {
-			return parentValue;
-		}
-
-		if (attr.equals(TYPE_APPLIED_STEREOTYPES_PROPERTY) && semanticElement instanceof TypedElement) {
-			Type type = ((TypedElement) semanticElement).getType();
-			if (type != null) {
-				List<String> appliedStereotypes = new LinkedList<>();
-				for (Stereotype stereotype : type.getAppliedStereotypes()) {
-					appliedStereotypes.add(stereotype.getName());
-					appliedStereotypes.add(stereotype.getQualifiedName());
-				}
-
-				if (!appliedStereotypes.isEmpty()) {
-					return ListHelper.deepToString(appliedStereotypes, CSS_VALUES_SEPARATOR);
-				}
-			}
-		}
-
-		return null;
-	}
+	
 }
