2ec48c3981d04d83fecdb92d71c4911dc552c84f
Fanch BONNABESSE <fanch.bonnabesse@all4tec.net>
2017-07-07 15:49:05 +0200

    Bug 515122: [Component Diagram] Contact lost when reroute Realization
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=515122
    
    Override "reorientTarget()" and "reorientSource()" to change "Contract"
    and "RealizingClassifier".
    
    Change-Id: If9f922cdc3b32b91c6e476e8de2d0c91fea0b7e9
    Signed-off-by: Fanch BONNABESSE <fanch.bonnabesse@all4tec.net>

--- plugins/uml/org.eclipse.papyrus.uml.service.types/src/org/eclipse/papyrus/uml/service/types/command/ComponentRealizationReorientCommand.java
+++ plugins/uml/org.eclipse.papyrus.uml.service.types/src/org/eclipse/papyrus/uml/service/types/command/ComponentRealizationReorientCommand.java
@@ -1,19 +1,22 @@
 /*****************************************************************************
- * Copyright (c) 2011 CEA LIST.
+ * Copyright (c) 2011, 2017 CEA LIST.
  *
- *    
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * 
+ *
  * 		Yann Tanguy (CEA LIST) yann.tanguy@cea.fr - Initial API and implementation
+ * 		Fanch Bonnabesse (ALL4TEC) fanch.bonnabesse@alltec.net - Bug 515122
  *
  *****************************************************************************/
 package org.eclipse.papyrus.uml.service.types.command;
 
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
 import org.eclipse.gmf.runtime.emf.type.core.requests.ReorientRelationshipRequest;
 import org.eclipse.uml2.uml.Classifier;
 import org.eclipse.uml2.uml.Component;
@@ -27,57 +30,70 @@ import org.eclipse.uml2.uml.ComponentRealization;
 public class ComponentRealizationReorientCommand extends DependencyReorientCommand {
 
 	/**
-	 * <pre>
 	 * Constructor.
-	 * 
-	 * @param request the re-orient relationship request.
-	 * </pre>
+	 *
+	 * @param request
+	 *            the re-orient relationship request.
 	 */
 	public ComponentRealizationReorientCommand(ReorientRelationshipRequest request) {
 		super(request);
 	}
 
 	/**
-	 * <pre>
 	 * @see org.eclipse.gmf.runtime.emf.type.core.commands.EditElementCommand#canExecute()
-	 * 
+	 *
 	 * @return true if the command is executable.
-	 * </pre>
 	 */
+	@Override
 	public boolean canExecute() {
-		if(! (getElementToEdit() instanceof ComponentRealization)) {
+		if (!(getElementToEdit() instanceof ComponentRealization)) {
 			return false;
 		}
-		
+
 		return super.canExecute();
 	}
 
+	@Override
 	protected boolean canReorientSource() {
-		if(!(newEnd instanceof Classifier)) {
+		if (!(newEnd instanceof Classifier)) {
 			return false;
 		}
 
 		if (newEnd == getLink().getAbstraction()) {
 			return false;
 		}
-		
+
 		return true;
 	}
 
+	@Override
 	protected boolean canReorientTarget() {
-		if(!(newEnd instanceof Component)) {
+		if (!(newEnd instanceof Component)) {
 			return false;
 		}
 
 		if (getLink().getRealizingClassifiers().contains(newEnd)) {
 			return false;
 		}
-		
+
 		return true;
 	}
 
+	@Override
 	protected ComponentRealization getLink() {
-		return (ComponentRealization)getElementToEdit();
+		return (ComponentRealization) getElementToEdit();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected CommandResult reorientSource() throws ExecutionException {
+		getLink().getRealizingClassifiers().remove(getOldSource());
+		if (getNewSource() instanceof Classifier) {
+			getLink().getRealizingClassifiers().add((Classifier) getNewSource());
+		}
+		return super.reorientSource();
 	}
 
 }
