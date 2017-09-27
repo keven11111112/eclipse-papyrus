73d973ff0f3d05ea87c9fae8de058c5dec3f91c2
Fanch BONNABESSE <fanch.bonnabesse@all4tec.net>
2017-09-27 15:50:49 +0200

    Bug 521908: [Property View][DataType] No editor for list of Real or list of UnlimitedNatural
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=521908
    
    Added management of Real and Unlimitednatural.
    
    Change-Id: Ie951079e463e17a7d8405450f36d6c9323429843
    Signed-off-by: Fanch BONNABESSE <fanch.bonnabesse@all4tec.net>

---
+++ plugins/infra/ui/org.eclipse.papyrus.infra.widgets/src/org/eclipse/papyrus/infra/widgets/selectors/UnlimitedNaturalSelector.java
@@ -0,0 +1,70 @@
+/*****************************************************************************
+ * Copyright (c) 2017 CEA LIST and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Fanch BONNABESSE (ALL4TEC) fanch.bonnabesse@all4tec.net - Initial API and implementation, Bug 521908
+ *
+ *****************************************************************************/
+
+package org.eclipse.papyrus.infra.widgets.selectors;
+
+import org.eclipse.papyrus.infra.widgets.util.Constants;
+
+/**
+ * Selector for the UnlimitedNaturalEditor. It accepts "-1", "*" and all integer >=0.
+ *
+ * @since 3.1
+ */
+public class UnlimitedNaturalSelector extends StringSelector {
+
+	/**
+	 * Constructs a Selector for Integer values
+	 */
+	public UnlimitedNaturalSelector() {
+		super();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Integer[] getSelectedElements() {
+		Integer[] result = new Integer[0];
+		String newText = text.getText();
+		if (isInteger(newText)) {
+			int value = Integer.parseInt(newText);
+			if (value >= -1) {
+				result = new Integer[] { value };
+				text.setText(Constants.EMPTY_STRING);
+			}
+		} else {
+			if (Constants.INFINITY_STAR.equals(newText)) {
+				result = new Integer[] { new Integer(-1) };
+				text.setText(Constants.EMPTY_STRING);
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * 
+	 * @param textToCheck
+	 * @return
+	 */
+	private boolean isInteger(final String textToCheck) {
+		boolean isInteger = true;
+		try {
+			Integer.parseInt(textToCheck);
+		} catch (NumberFormatException e) {
+			isInteger = false;
+		}
+		return isInteger;
+	}
+
+}
---
+++ plugins/infra/ui/org.eclipse.papyrus.infra.widgets/src/org/eclipse/papyrus/infra/widgets/util/Constants.java
@@ -0,0 +1,31 @@
+/*****************************************************************************
+ * Copyright (c) 2017 CEA LIST and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Fanch BONNABESSE (ALL4TEC) fanch.bonnabesse@all4tec.net - Initial API and implementation
+ *   
+ *****************************************************************************/
+
+package org.eclipse.papyrus.infra.widgets.util;
+
+/**
+ * The constants for the org.eclipse.papyrus.plugin infra.widgets.
+ * 
+ * @since 3.1
+ *
+ */
+public class Constants {
+	private Constants() {
+		// to prevent instanciation
+	}
+
+	public static final String INFINITY_STAR = "*"; //$NON-NLS-1$
+
+	public static final String EMPTY_STRING = ""; //$NON-NLS-1$
+
+}
