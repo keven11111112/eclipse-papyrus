18b714785f83b6b86d8101397e5a092fefae09dd
Vincent Lorenzo <vincent.lorenzo@cea.fr>
2017-09-29 10:23:08 +0200

    Bug 522721: [Table][Matrix] New Matrix Framework must provide JUnit Tests  - add the JUnit tests to the test suite
    Change-Id: Ia9d48105021e387d8b4042495b7baec621d8a87f
    Signed-off-by: Vincent Lorenzo <vincent.lorenzo@cea.fr>

--- tests/junit/plugins/uml/nattable/org.eclipse.papyrus.uml.nattable.suite.tests/test/org/eclipse/papyrus/uml/nattable/suite/tests/AllTests.java
+++ tests/junit/plugins/uml/nattable/org.eclipse.papyrus.uml.nattable.suite.tests/test/org/eclipse/papyrus/uml/nattable/suite/tests/AllTests.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2010, 2016 CEA LIST, Christian W. Damus, and others.
+ * Copyright (c) 2010, 2016, 2017 CEA LIST, Christian W. Damus, and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -10,7 +10,7 @@
  *  Remi Schnekenburger (CEA LIST) remi.schnekenburger@cea.fr - Initial API and implementation
  *  Christian W. Damus (CEA) - bugs 402525, 323802, 431953, 433310, 434993
  *  Christian W. Damus - bugs 399859, 451230, 433206, 463156, 474610, 469188, 485220, 488791, 496598, 508629
- *
+ *  Vincent Lorenzo CEA LIST) - bug 522721
  *****************************************************************************/
 package org.eclipse.papyrus.uml.nattable.suite.tests;
 
@@ -42,6 +42,7 @@ public class AllTests {
 		/* nattable tests */
 		suiteClasses.add(new PluginTestSuiteClass(org.eclipse.papyrus.uml.nattable.tests.tests.AllTests.class));
 		suiteClasses.add(new PluginTestSuiteClass(org.eclipse.papyrus.uml.nattable.generic.tests.tests.AllTests.class));
+		suiteClasses.add(new PluginTestSuiteClass(org.eclipse.papyrus.uml.nattable.matrix.tests.tests.AllTests.class));
 		suiteClasses.add(new PluginTestSuiteClass(org.eclipse.papyrus.uml.nattable.clazz.config.tests.tests.AllTests.class));
 		suiteClasses.add(new PluginTestSuiteClass(org.eclipse.papyrus.uml.nattable.stereotype.display.tests.tests.AllTests.class));
 
