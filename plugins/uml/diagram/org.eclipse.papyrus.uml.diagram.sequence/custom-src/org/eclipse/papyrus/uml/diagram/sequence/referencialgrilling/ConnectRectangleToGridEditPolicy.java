9b752e4752d26e554198d610cc2659ba754ae4c2
Nicolas FAUVERGUE <nicolas.fauvergue@cea.fr>
2018-03-28 18:04:33 +0200

    Bug 533004: [Sequence Diagram] When a MessageCreate or MessageDelete is created, other messages are moved
    - Change the old and new notification values if needed ('-1' need to be
    changed into the default size).
    - Change the move Y calculation: this is now done with the figure bounds
    (not updated) instead of the node bounds (already updated)
    
    Change-Id: I3aaa4cc02ad4d9a434ede6c749cfcc16c0c5fa63
    Signed-off-by: Nicolas FAUVERGUE <nicolas.fauvergue@cea.fr>

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.sequence/custom-src/org/eclipse/papyrus/uml/diagram/sequence/referencialgrilling/ConnectRectangleToGridEditPolicy.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.sequence/custom-src/org/eclipse/papyrus/uml/diagram/sequence/referencialgrilling/ConnectRectangleToGridEditPolicy.java
@@ -8,6 +8,7 @@
  *
  * Contributors:
  *   CEA LIST - Initial API and implementation
+ *   Nicolas FAUVERGUE (CEA LIST) nicolas.fauvergue@cea.fr - Bug 533004
  *   
  *****************************************************************************/
 
@@ -15,6 +16,7 @@ package org.eclipse.papyrus.uml.diagram.sequence.referencialgrilling;
 
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.PrecisionRectangle;
+import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.command.SetCommand;
@@ -31,7 +33,6 @@ import org.eclipse.gmf.runtime.notation.DecorationNode;
 import org.eclipse.gmf.runtime.notation.Edge;
 import org.eclipse.gmf.runtime.notation.IdentityAnchor;
 import org.eclipse.gmf.runtime.notation.LayoutConstraint;
-import org.eclipse.gmf.runtime.notation.Location;
 import org.eclipse.gmf.runtime.notation.Node;
 import org.eclipse.gmf.runtime.notation.NotationPackage;
 import org.eclipse.papyrus.infra.gmfdiag.common.editpolicies.AutomaticNotationEditPolicy;
@@ -214,31 +215,6 @@ public class ConnectRectangleToGridEditPolicy extends ConnectToGridEditPolicy im
 		}
 		Node nodeContainer = (Node) (((GraphicalEditPart) getHost()).getNotationView()).eContainer();
 		if (nodeContainer != null) {
-//			PrecisionRectangle originPosition = NotationHelper.getAbsoluteBounds(nodeContainer);
-//			Bounds currentBounds = (Bounds) ((Node) ((GraphicalEditPart) getHost()).getNotationView()).getLayoutConstraint();
-//
-//			// the ROW has changed so we must update the position of the combined fragment
-//			if (notification.getEventType() == Notification.SET && notification.getNotifier() instanceof Location && (((EObject) notification.getNotifier()).eContainer().equals(rowStart))) {
-//				// The grid is in absolute
-//				// compute next position for View.bound.y
-//				updateYFromAxisNotification(originPosition, currentBounds);
-//			}
-//			// the COLUMN has changed so we must update the postion of the combined fragment
-//			if (notification.getEventType() == Notification.SET && notification.getNotifier() instanceof Location && (((EObject) notification.getNotifier()).eContainer().equals(columnStart))) {
-//				// The grill is in absolute
-//				updateXFromAxisNotification(originPosition, currentBounds);
-//
-//			}
-//			if (notification.getEventType() == Notification.SET && notification.getNotifier() instanceof Location && (((EObject) notification.getNotifier()).eContainer().equals(rowFinish))) {
-//				// The grill is in absolute
-//				updateHeightFromAxisNotification(originPosition, currentBounds);
-//
-//			}
-//			if (notification.getEventType() == Notification.SET && notification.getNotifier() instanceof Location && (((EObject) notification.getNotifier()).eContainer().equals(columnFinish))) {
-//				// The grill is in absolute
-//				updateWidthFromAxisNotification(originPosition, currentBounds);
-//
-//			}
 
 			// UPDATE COLUM AND ROW of THE GRID
 			if (notification.getEventType() == Notification.SET && notification.getNotifier() instanceof Bounds) {
@@ -246,6 +222,11 @@ public class ConnectRectangleToGridEditPolicy extends ConnectToGridEditPolicy im
 				UMLDiagramEditorPlugin.log.trace(LogOptions.SEQUENCE_DEBUG_REFERENCEGRID, "+ EVENT: BOUNDS change " + notification.getNewValue());//$NON-NLS-1$
 
 				if (notification.getFeature().equals(NotationPackage.eINSTANCE.getSize_Height())) {
+
+					// Get the old and new values (but care about the -1 default value)
+					final int oldIntValue = notification.getOldIntValue() == -1 ? BoundForEditPart.getDefaultHeightFromView((Node) ((GraphicalEditPart) getHost()).getNotationView()) : notification.getOldIntValue();
+					final int newIntValue = notification.getNewIntValue() == -1 ? BoundForEditPart.getDefaultHeightFromView((Node) ((GraphicalEditPart) getHost()).getNotationView()) : notification.getNewIntValue();
+
 					updateRowFinishFromHeightNotification(bounds);
 					// update anchors
 					if ((((EObject) notification.getNotifier()).eContainer().equals(((EObject) getHost().getModel())))) {
@@ -254,14 +235,14 @@ public class ConnectRectangleToGridEditPolicy extends ConnectToGridEditPolicy im
 						for (Edge edge : sourceEdge) {
 							IdentityAnchor anchor = (IdentityAnchor) edge.getSourceAnchor();
 							if (anchor instanceof IdentityAnchor) {
-								updateAnchorFromHeight(anchor, ((Node) getHost().getModel()), notification.getNewIntValue() - notification.getOldIntValue());
+								updateAnchorFromHeight(anchor, ((Node) getHost().getModel()), newIntValue - oldIntValue);
 							}
 						}
 						java.util.List<Edge> targetEdge = node.getTargetEdges();
 						for (Edge edge : targetEdge) {
 							IdentityAnchor anchor = (IdentityAnchor) edge.getTargetAnchor();
 							if (anchor instanceof IdentityAnchor) {
-								updateAnchorFromHeight(anchor, ((Node) getHost().getModel()), notification.getNewIntValue() - notification.getOldIntValue());
+								updateAnchorFromHeight(anchor, ((Node) getHost().getModel()), newIntValue - oldIntValue);
 							}
 						}
 					}
@@ -292,14 +273,16 @@ public class ConnectRectangleToGridEditPolicy extends ConnectToGridEditPolicy im
 						for (Edge edge : sourceEdge) {
 							IdentityAnchor anchor = (IdentityAnchor) edge.getSourceAnchor();
 							if (anchor instanceof IdentityAnchor) {
-								updateAnchorFromY(anchor, ((Node) getHost().getModel()), notification.getOldIntValue(), notification.getNewIntValue());
+								// We need to manage the new Y position with the figure height instead of node height (that was already changed)
+								updateAnchorFromY(anchor, ((Node) getHost().getModel()), ((GraphicalEditPart) getHost()).getFigure().getBounds(), notification.getOldIntValue(), notification.getNewIntValue());
 							}
 						}
 						java.util.List<Edge> targetEdge = node.getTargetEdges();
 						for (Edge edge : targetEdge) {
 							IdentityAnchor anchor = (IdentityAnchor) edge.getTargetAnchor();
 							if (anchor instanceof IdentityAnchor) {
-								updateAnchorFromY(anchor, ((Node) getHost().getModel()), notification.getOldIntValue(), notification.getNewIntValue());
+								// We need to manage the new Y position with the figure height instead of node height (that was already changed)
+								updateAnchorFromY(anchor, ((Node) getHost().getModel()), ((GraphicalEditPart) getHost()).getFigure().getBounds(), notification.getOldIntValue(), notification.getNewIntValue());
 							}
 						}
 					}
@@ -473,10 +456,11 @@ public class ConnectRectangleToGridEditPolicy extends ConnectToGridEditPolicy im
 			double xPercent = IdentityAnchorHelper.getXPercentage(anchor);
 
 			// calculate bounds from notation
-			PrecisionRectangle bounds = NotationHelper.getAbsoluteBounds(node);
-			double oldHeight = BoundForEditPart.getHeightFromView(node) - deltaHeight;
+			int nodeHeight = BoundForEditPart.getHeightFromView(node);
+			double oldHeight = nodeHeight - deltaHeight;
+			double preciseHeight = nodeHeight;
 
-			double newPercentY = (yPercent * oldHeight) / (bounds.preciseHeight());
+			double newPercentY = (yPercent * oldHeight) / preciseHeight;
 			if (newPercentY <= 1 && newPercentY >= 0 && newPercentY <= 1 && newPercentY >= 0) {
 				final String newIdValue = IdentityAnchorHelper.createNewAnchorIdValue(xPercent, newPercentY);
 				execute(new SetCommand(getDiagramEditPart(getHost()).getEditingDomain(), anchor, NotationPackage.eINSTANCE.getIdentityAnchor_Id(), newIdValue));
@@ -485,18 +469,21 @@ public class ConnectRectangleToGridEditPolicy extends ConnectToGridEditPolicy im
 	}
 
 	/**
-	 * this class update the position of anchor after the move
+	 * This allows to update the position of anchor after the move.
 	 * 
-	 * @param sourceEdge
-	 * @param eObject
+	 * @param anchor The anchor to recalculate.
+	 * @param node The moved node.
+	 * @param nodeBounds The node bounds (from the figure (that was not already changed)).
+	 * @param oldY The old Y position.
+	 * @param newY The new Y position.
 	 */
-	protected void updateAnchorFromY(IdentityAnchor anchor, Node node, int oldY, int newY) {
+	protected void updateAnchorFromY(IdentityAnchor anchor, Node node, Rectangle nodeBounds, int oldY, int newY) {
 		if (null != anchor && !anchor.getId().trim().equals("")) { //$NON-NLS-1$
 			double yPercent = IdentityAnchorHelper.getYPercentage(anchor);
 			double xPercent = IdentityAnchorHelper.getXPercentage(anchor);
 
 			// calculate bounds from notation
-			double height = BoundForEditPart.getHeightFromView(node);
+			double height = nodeBounds.height == -1 ? BoundForEditPart.getDefaultHeightFromView(node) : nodeBounds.height;
 
 			double newPercentY = (oldY - newY) / (height) + yPercent;
 			if (newPercentY < 0) {
