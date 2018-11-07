de9194b1208e8a0077a122a0291d338fde4596be
Yupanqui Munoz <Yupanqui.MUNOZJULHO@cea.fr>
2018-11-07 08:52:35 +0100

    Bug 540864 - [SashEditor] Fix copyright in "org.eclipse.papyrus.infra.core.sasheditor" plug-in.
    Change-Id: I0039836b4277eeea1452f5d18be1afba43cda217
    Signed-off-by: Yupanqui Munoz <Yupanqui.MUNOZJULHO@cea.fr>

--- plugins/infra/core/org.eclipse.papyrus.infra.core.sasheditor/src/org/eclipse/papyrus/infra/core/sasheditor/contentprovider/IAbstractPanelModel.java
+++ plugins/infra/core/org.eclipse.papyrus.infra.core.sasheditor/src/org/eclipse/papyrus/infra/core/sasheditor/contentprovider/IAbstractPanelModel.java
@@ -1,6 +1,18 @@
-/**
+/*****************************************************************************
+ * Copyright (c) 2009 LIFL, CEA LIST and others.
  *
- */
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Cedric Dumoulin  Cedric.dumoulin@lifl.fr - Initial API and implementation
+ *
+ *****************************************************************************/
 package org.eclipse.papyrus.infra.core.sasheditor.contentprovider;
 
 /**
