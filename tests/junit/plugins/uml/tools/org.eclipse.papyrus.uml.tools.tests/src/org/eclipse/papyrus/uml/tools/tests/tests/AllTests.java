b682e1565457a874a4b3049332411bff176506da
Vincent Lorenzo <vincent.lorenzo@cea.fr>
2019-01-27 11:10:22 +0100

    Bug 541686 - Multiple clients and suppliers in edges after copy-paste 	- register the new tests in AllTests.class of the plugin
    Change-Id: I4e3535b62f8ee69b87728a5edc1331a2d39eb75d
    Signed-off-by: Vincent Lorenzo <vincent.lorenzo@cea.fr>

--- tests/junit/plugins/uml/tools/org.eclipse.papyrus.uml.tools.tests/src/org/eclipse/papyrus/uml/tools/tests/tests/AllTests.java
+++ tests/junit/plugins/uml/tools/org.eclipse.papyrus.uml.tools.tests/src/org/eclipse/papyrus/uml/tools/tests/tests/AllTests.java
@@ -1,6 +1,6 @@
 /*****************************************************************************
- * Copyright (c) 2012, 2016 CEA LIST, Christian W. Damus, and others.
- * 
+ * Copyright (c) 2012, 2016, 2019 CEA LIST, Christian W. Damus, and others.
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@
  *  Camille Letavernier (CEA LIST) camille.letavernier@cea.fr - Initial API and implementation
  *  Christian W. Damus (CEA) - bug 440108
  *  Christian W. Damus - bugs 458197, 468030, 485220, 502461, 508629
- *   
+ *  Ansgar Radermacher (CEA LIST) - bug 541686 (duplicated replationships)
  *****************************************************************************/
 package org.eclipse.papyrus.uml.tools.tests.tests;
 
@@ -38,6 +38,8 @@ import org.junit.runners.Suite.SuiteClasses;
 		CustomizableDelegatingItemLabelProviderTest.class,
 		DelegatingItemLabelProviderTest.class,
 		UMLCopyTest.class,
+		UMLCopyTestME.class,
+
 })
 public class AllTests {
 	// Test suite
