acd1d5a8e9be36ea4124203f744a56f94fc80651
Nicolas FAUVERGUE <nicolas.fauvergue@cea.fr>
2019-04-16 17:45:16 +0200

    Bug 546239: [Toolsmiths][Validation] Plug-ins to validate profile plug-in must be implemented
    - Create tests for Profile plug-in validation (actually 73% code
    coverage on oep.toolsmiths.validation.profile (handlers and testers not
    tested) and 69% on oep.toolsmiths.validation.common (all utils methods
    not all tested))
    
    Change-Id: I65902fafdf6599684eb4b0344fa34503326377e4
    Signed-off-by: Nicolas FAUVERGUE <nicolas.fauvergue@cea.fr>

--- tests/junit/framework/org.eclipse.papyrus.junit.utils/src/org/eclipse/papyrus/junit/utils/rules/ProjectFixture.java
+++ tests/junit/framework/org.eclipse.papyrus.junit.utils/src/org/eclipse/papyrus/junit/utils/rules/ProjectFixture.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2014, 2015 CEA, Christian W. Damus, and others.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -20,10 +20,15 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
 
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
@@ -32,6 +37,7 @@ import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.resources.ResourceAttributes;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
@@ -73,16 +79,16 @@ public class ProjectFixture implements TestRule {
 
 	/**
 	 * Creates a new file at the specified project-relative path with the contents of a bundle resource.
-	 * 
+	 *
 	 * @param relativeFilePath
 	 *            the project-relative path of the file to create
 	 * @param classFromBundle
 	 *            the bundle in which its content is to be found
 	 * @param resourcePath
 	 *            the path in the context bundle of the resource to copy
-	 * 
+	 *
 	 * @return the new file
-	 * 
+	 *
 	 * @throws IOException
 	 *             on any problem in creating the file
 	 */
@@ -125,21 +131,89 @@ public class ProjectFixture implements TestRule {
 
 	/**
 	 * Creates a new file in my project with the contents of a bundle resource.
-	 * 
+	 *
 	 * @param classFromBundle
 	 *            the bundle in which its content is to be found
 	 * @param resourcePath
 	 *            the path in the context bundle of the resource to copy
-	 * 
+	 *
 	 * @return the new file, which will have the same name as the bundle resource and will be at the top level of the project
-	 * 
+	 *
 	 * @throws IOException
 	 *             on any problem in creating the file
-	 * 
+	 *
 	 * @see #createFile(String, Class, String)
 	 */
 	public IFile createFile(Class<?> classFromBundle, String resourcePath) throws IOException {
 		return createFile(new Path(resourcePath).lastSegment(), classFromBundle, resourcePath);
+	}
+
+	/**
+	 * This allows to copy a entire folder into the current project.
+	 *
+	 * @param classFromBundle
+	 *            The bundle in which its content is to be found.
+	 * @param folderPath
+	 *            The path in the context bundle of the folder to copy.
+	 * @throws IOException
+	 *             On any problem while creating the files and folders.
+	 */
+	public void copyFolder(final Class<?> classFromBundle, final String folderPath) throws IOException {
+		// Get the bundle and check that the resource exists
+		final Bundle bundle = FrameworkUtil.getBundle(classFromBundle);
+		final URL resource = (bundle == null) ? null : bundle.getResource(folderPath);
+		if (resource == null) {
+			throw new IOException("No such bundle resource: " + folderPath); //$NON-NLS-1$
+		}
+
+		// Get the file corresponding to the resource
+		File pluginFile = null;
+		try {
+			pluginFile = new File(FileLocator.toFileURL(resource).toURI());
+		} catch (URISyntaxException e1) {
+			throw new IOException("Error while getting project files"); //$NON-NLS-1$
+		}
+		// Copy its content
+		try {
+			copyFiles(pluginFile, project);
+		} catch (Exception e) {
+			throw new IOException("Error while copying project files"); //$NON-NLS-1$
+		}
+	}
+
+	/**
+	 * This allows to copy recursively the files from sources to destination folder.
+	 *
+	 * @param srcFolder
+	 *            The source folder.
+	 * @param destFolder
+	 *            The destination folder.
+	 * @throws CoreException
+	 *             Possible core exception while manipulating files.
+	 * @throws FileNotFoundException
+	 *             In case where the file is not found.
+	 */
+	private void copyFiles(final File srcFolder, final IContainer destFolder) throws CoreException, FileNotFoundException {
+		if (null != srcFolder && srcFolder.isDirectory()) {
+			for (final File currentFile : srcFolder.listFiles()) {
+				if (currentFile.isDirectory()) {
+					// In the case of sub folders, create it (if needed) and copy files
+					final IFolder newFolder = destFolder.getFolder(new Path(currentFile.getName()));
+					if (!newFolder.exists()) {
+						newFolder.create(true, true, null);
+					}
+					copyFiles(currentFile, newFolder);
+				} else {
+					// In the case of files, create or replace it
+					final IFile newFile = destFolder.getFile(new Path(currentFile.getName()));
+					// If the file already exists, replace it
+					if (newFile.exists()) {
+						newFile.delete(true, null);
+					}
+					newFile.create(new FileInputStream(currentFile), true, null);
+				}
+			}
+		}
 	}
 
 	@Override
---
+++ tests/junit/plugins/toolsmiths/org.eclipse.papyrus.toolsmiths.validation.profile.tests/src/org/eclipse/papyrus/toolsmiths/validation/profile/tests/ProfilePluginValidationTest.java
@@ -0,0 +1,135 @@
+/*****************************************************************************
+ * Copyright (c) 2019 CEA LIST, and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Nicolas FAUVERGUE (CEA LIST) nicolas.fauvergue@cea.fr - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.toolsmiths.validation.profile.tests;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.papyrus.junit.framework.classification.tests.AbstractPapyrusTest;
+import org.eclipse.papyrus.junit.utils.rules.ProjectFixture;
+import org.eclipse.papyrus.toolsmiths.validation.profile.checkers.ProfilePluginCheckerService;
+import org.eclipse.papyrus.toolsmiths.validation.profile.constants.ProfilePluginValidationConstants;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * This class allows to test the profile plug-in validation.
+ * This will check the markers resulting from a failing profile plug-in definition.
+ */
+public class ProfilePluginValidationTest extends AbstractPapyrusTest {
+
+	/**
+	 * The plug-in path to copy into the project fixture
+	 */
+	private static final String PLUGIN_PATH = "resources/org.eclipse.papyrus.toolsmiths.validation.profile.example";//$NON-NLS-1$
+
+	/**
+	 * The severity attribute identifier.
+	 */
+	private static final String SEVERITY_ID = "severity"; //$NON-NLS-1$
+
+	/**
+	 * The project fixture to manage easily the project.
+	 */
+	@Rule
+	public final ProjectFixture fixture = new ProjectFixture();
+
+	/**
+	 * This allows to copy the project into the fixture project.
+	 */
+	@Before
+	public void init() {
+		try {
+			fixture.copyFolder(ProfilePluginValidationTest.class, PLUGIN_PATH);
+		} catch (IOException e) {
+			Assert.fail("Error while copying project"); //$NON-NLS-1$
+		}
+	}
+
+	/**
+	 * This allows to test the profile plug-in validation.
+	 */
+	@Test
+	public void testProfilePluginValidation() {
+		// First, run the validation
+		ProfilePluginCheckerService.checkProfilePlugin(fixture.getProject());
+
+		// Get the markers
+		List<IMarker> markers = null;
+		try {
+			markers = Arrays.asList(fixture.getProject().findMarkers(ProfilePluginValidationConstants.PROFILE_PLUGIN_VALIDATION_TYPE, true, IResource.DEPTH_INFINITE));
+		} catch (CoreException e) {
+			Assert.fail("Error with resource"); //$NON-NLS-1$
+		}
+
+		// Now check the markers
+		Assert.assertNotNull("The markers have to be found", markers); //$NON-NLS-1$
+		Assert.assertEquals("The number of markers is not correct", 5, markers.size()); //$NON-NLS-1$
+
+		// Check the profile.uml markers
+		final List<IMarker> profileFileMarkers = markers.stream().filter(marker -> marker.getResource().getFullPath().toString().endsWith("bookstore.profile.uml")).collect(Collectors.toList()); //$NON-NLS-1$
+		Assert.assertNotNull("Profile file markers are not found", profileFileMarkers); //$NON-NLS-1$
+		Assert.assertEquals("The number of markers for profile file is not correct", 1, profileFileMarkers.size()); //$NON-NLS-1$
+		Assert.assertTrue("The severity of profile marker is not correct", isMarkerSeverity(profileFileMarkers.get(0), IMarker.SEVERITY_ERROR)); //$NON-NLS-1$
+
+		// Check the dependencies markers
+		final List<IMarker> manifestMarkers = markers.stream().filter(marker -> marker.getResource().getFullPath().toString().endsWith("MANIFEST.MF")).collect(Collectors.toList()); //$NON-NLS-1$
+		Assert.assertNotNull("Dependencies markers are not found", manifestMarkers); //$NON-NLS-1$
+		Assert.assertEquals("The number of markers for dependencies is not correct", 1, manifestMarkers.size()); //$NON-NLS-1$
+		Assert.assertTrue("The severity of profile marker is not correct", isMarkerSeverity(manifestMarkers.get(0), IMarker.SEVERITY_WARNING)); //$NON-NLS-1$
+
+		// Check the build markers
+		final List<IMarker> buildMarkers = markers.stream().filter(marker -> marker.getResource().getFullPath().toString().endsWith("build.properties")).collect(Collectors.toList()); //$NON-NLS-1$
+		Assert.assertNotNull("Build markers are not found", buildMarkers); //$NON-NLS-1$
+		Assert.assertEquals("The number of markers for build is not correct", 1, buildMarkers.size()); //$NON-NLS-1$
+		Assert.assertTrue("The severity of profile marker is not correct", isMarkerSeverity(buildMarkers.get(0), IMarker.SEVERITY_ERROR)); //$NON-NLS-1$
+
+		// Check the extensions markers
+		final List<IMarker> extensionsMarkers = markers.stream().filter(marker -> marker.getResource().getFullPath().toString().endsWith("plugin.xml")).collect(Collectors.toList()); //$NON-NLS-1$
+		Assert.assertNotNull("Extensions markers are not found", extensionsMarkers); //$NON-NLS-1$
+		Assert.assertEquals("The number of markers for extensions is not correct", 2, extensionsMarkers.size()); //$NON-NLS-1$
+		final List<IMarker> warningExtensionsMarkers = extensionsMarkers.stream().filter(marker -> isMarkerSeverity(marker, IMarker.SEVERITY_WARNING)).collect(Collectors.toList());
+		Assert.assertEquals("The number of warning markers for extensions is not correct", 1, warningExtensionsMarkers.size()); //$NON-NLS-1$
+		final List<IMarker> errorExtensionsMarkers = extensionsMarkers.stream().filter(marker -> isMarkerSeverity(marker, IMarker.SEVERITY_ERROR)).collect(Collectors.toList());
+		Assert.assertEquals("The number of error markers for extensions is not correct", 1, errorExtensionsMarkers.size()); //$NON-NLS-1$
+
+	}
+
+	/**
+	 * This allows to determinate if a marker got the correct severity.
+	 *
+	 * @param marker
+	 *            the marker.
+	 * @param severity
+	 *            The severity to get.
+	 * @return <code>true</code> if the marker got the correct severity, <code>false</code> otherwise.
+	 */
+	private boolean isMarkerSeverity(final IMarker marker, final int severity) {
+		try {
+			return severity == (int) marker.getAttribute(SEVERITY_ID);
+		} catch (CoreException e) {
+			Assert.fail("Error while getting " + SEVERITY_ID); //$NON-NLS-1$
+			return false;
+		}
+	}
+
+}
