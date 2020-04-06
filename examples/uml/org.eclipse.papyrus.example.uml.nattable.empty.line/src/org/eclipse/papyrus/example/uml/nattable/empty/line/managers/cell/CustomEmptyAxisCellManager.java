49341edaa57ea8afb84b3a5e831bbe5c6efb5e5c
Vincent Lorenzo <vincent.lorenzo@cea.fr>
2020-04-06 17:36:33 +0200

    Bug 561409: [Table] The Papyrus row axis manager used by tree table must support empty rows. Bug 561278: [Table] Provide API for actions on single click on cells 	- provide examples for these 2 bugs
    Change-Id: I1079f1bc4879daf5a2af1391a3ab4326034531f2
    Signed-off-by: Vincent Lorenzo <vincent.lorenzo@cea.fr>

---
+++ examples/uml/org.eclipse.papyrus.example.uml.nattable.empty.line/src/org/eclipse/papyrus/example/uml/nattable/empty/line/managers/cell/CustomEmptyAxisCellManager.java
@@ -0,0 +1,35 @@
+/*****************************************************************************
+ * Copyright (c) 2020 CEA LIST
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Vincent Lorenzo (CEA LIST) vincent.lorenzo@cea.fr - Initial API and implementation
+  *****************************************************************************/
+
+package org.eclipse.papyrus.example.uml.nattable.empty.line.managers.cell;
+
+import org.eclipse.papyrus.example.uml.nattable.empty.line.Activator;
+import org.eclipse.papyrus.infra.nattable.manager.cell.AbstractEmptyAxisCellManager;
+
+/**
+ * @author Vincent Lorenzo (CEA LIST) <vincent.lorenzo@cea.fr>
+ *
+ */
+public class CustomEmptyAxisCellManager extends AbstractEmptyAxisCellManager {
+
+	/**
+	 * Constructor.
+	 *
+	 * @param tableKindId
+	 */
+	public CustomEmptyAxisCellManager() {
+		super(Activator.TABLE_KIND_ID);
+	}
+
+}
