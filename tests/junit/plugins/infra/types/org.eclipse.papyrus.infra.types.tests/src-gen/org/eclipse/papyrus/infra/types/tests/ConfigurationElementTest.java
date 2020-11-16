5fec483685ed834556c235e1d8b98604fb91b374
Christian W. Damus <give.a.damus@gmail.com>
2020-11-16 16:35:21 -0500

    Bug 568782: [Toolsmiths - ElementTypes] Allow specification of AdviceBindingConfiguration as children of ElementTypes
    Add JUnit test coverage, including:
    
    - generate tests for model operations and constraints
    - update test model to make advices owned that are used by tests
    
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=568782
    Signed-off-by: Christian W. Damus <give.a.damus@gmail.com>
    
    Change-Id: I656eb0ca8030023ed430391ef27097e0193053d3

---
+++ tests/junit/plugins/infra/types/org.eclipse.papyrus.infra.types.tests/src-gen/org/eclipse/papyrus/infra/types/tests/ConfigurationElementTest.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2020 Christian W. Damus, CEA LIST, and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ * Christian W. Damus - Initial API and implementation
+ */
+package org.eclipse.papyrus.infra.types.tests;
+
+import junit.framework.TestCase;
+
+import org.eclipse.papyrus.infra.types.ConfigurationElement;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Configuration Element</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public abstract class ConfigurationElementTest extends TestCase {
+
+	/**
+	 * The fixture for this Configuration Element test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ConfigurationElement fixture = null;
+
+	/**
+	 * Constructs a new Configuration Element test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ConfigurationElementTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Configuration Element test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(ConfigurationElement fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Configuration Element test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ConfigurationElement getFixture() {
+		return fixture;
+	}
+
+} //ConfigurationElementTest
